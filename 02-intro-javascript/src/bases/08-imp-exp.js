cadeb643402fee2015675cbd1f26124b2d092170
DanielGeek <daniel30081990@gmail.com>
2020-09-10 23:09:11 -0300

    22. Promesas

--- 02-intro-javascript/src/bases/08-imp-exp.js
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -1,15 +1,16 @@
 // import { hereos } from './data/heroes';
 // import { heroes } from './data/heroes';
-import heroes, { owners } from '../data/heroes';
+// import heroes, { owners } from '../data/heroes';
+import heroes from '../data/heroes';
 
-console.log( owners );
+// console.log( owners );
 
 // funcion para devolver el hereo que coincida con el id
-const getHeroeById = ( id ) => heroes.find( (heroe) => heroe.id === id);
+export const getHeroeById = ( id ) => heroes.find( (heroe) => heroe.id === id);
 
-console.log( getHeroeById(2) );
+// console.log( getHeroeById(2) );
 
 //buscar todos los hereos que coincidan con owner
-const getHeroesByOwner = ( owner ) => heroes.filter( (hereo) => hereo.owner === owner);
+export const getHeroesByOwner = ( owner ) => heroes.filter( (hereo) => hereo.owner === owner);
 
-console.log( getHeroesByOwner('DC') );
+// console.log( getHeroesByOwner('DC') );
