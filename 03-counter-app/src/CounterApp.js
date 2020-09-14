ef59409149a787854dc012068eabcbbe5e8b2131
DanielGeek <daniel30081990@gmail.com>
2020-09-14 13:36:53 -0300

    43. useState - Hook

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 // import React, { Fragment } from 'react';
 // comando para crear un functional component rafcp
 // Funtional component
 const CounterApp = ({ value }) => {
 
+    const [ counter, setCounter ] = useState(0);
+
     // handleAdd
-    const handleAdd = (e) => {
-        console.log(e)
+    const handleAdd = () => {
+        // setCounter( counter + 1);
+        setCounter( (c) => c + 1);
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button onClick={ handleAdd }>+1</button>
         </>
     );
