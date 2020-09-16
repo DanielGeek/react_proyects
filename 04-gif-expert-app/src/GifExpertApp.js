d43436e6aa36159fb466ec454f06b02740b9a079
DanielGeek <daniel30081990@gmail.com>
2020-09-15 23:15:36 -0300

    70. Creando una lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GitExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => {
+
+        // setCategories(['HunterXHunter', ...categories]);
+        setCategories(cats => [...cats, 'HunterXHunter']);
+    }
+
     return (
         <>
             <h2>GitExpertApp</h2>
             <hr />
+
+            <button onClick={handleAdd}>Agregar</button>
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}> {category} </li>
+                    })
+                }
+            </ol>
         </>
     )
 };
