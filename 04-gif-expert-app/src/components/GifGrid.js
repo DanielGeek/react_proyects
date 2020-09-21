380f001675b11c7d3b56f4e7f0400d77c3335245
DanielGeek <daniel30081990@gmail.com>
2020-09-21 11:07:57 -0300

    78. Custom Hook

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,23 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { GifGridItem } from './GifGridItem';
-import { getGifs } from '../helpers/getGifs';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+// import { GifGridItem } from './GifGridItem';
+// import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
+    // const [images, setImages] = useState([])
+    const { loading } = useFetchGifs();
 
-    const [images, setImages] = useState([])
-
-    useEffect( () => {
-        getGifs( category )
-            .then( setImages );
-    }, [ category ])
+    console.log(loading);
+    // useEffect( () => {
+    //     getGifs( category )
+    //         .then( setImages );
+    // }, [ category ])
 
     
-
     return (
         <>
             <h3>{ category }</h3>
-            <div className="card-grid">
+
+            { loading ? 'Cargando...' : 'Data cargada' }
+
+            {/* <div className="card-grid">
             
                 { 
                     images.map( img  => (
@@ -27,7 +31,7 @@ export const GifGrid = ({ category }) => {
                     ))
                 }
                 
-            </div>
+            </div> */}
         </>
         
     )
