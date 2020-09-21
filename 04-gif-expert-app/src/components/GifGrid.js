ae9815f63a75a283c1b228ff8d594407ac51705c
DanielGeek <daniel30081990@gmail.com>
2020-09-21 10:46:22 -0300

    77. Helpers - getGifs

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
@@ -7,27 +8,11 @@ export const GifGrid = ({ category }) => {
     const [images, setImages] = useState([])
 
     useEffect( () => {
-        getGifs();
-    }, [])
+        getGifs( category )
+            .then( setImages );
+    }, [ category ])
 
-    const getGifs = async () => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=bqumbdLo8Lrb1pXLE0Sl8YM3WLw5hPrv';
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-
-        console.log(gifs);
-        setImages( gifs );
-
-    }
+    
 
     return (
         <>
---
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+
+export const getGifs = async ( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=bqumbdLo8Lrb1pXLE0Sl8YM3WLw5hPrv`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
