af69bdedaa6c3012c5bf5d58de22ef66b56d65be
DanielGeek <daniel30081990@gmail.com>
2020-09-24 14:57:16 -0300

    asasa

---
+++ docs/precache-manifest.bbad4aaf3c031a5a3aa0fbb3fc09f5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd021c2a434704c9771e7b472a4efa0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aa1e8cf948efd44e9a8",
+    "url": "/static/css/main.6d2547d9.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa1e8cf948efd44e9a8",
+    "url": "/static/js/main.85cc6960.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
