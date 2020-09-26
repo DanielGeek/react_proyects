0ff0ffb682e3e2856db44ddd26f74d96e298279c
DanielGeek <daniel30081990@gmail.com>
2020-09-26 17:54:14 -0300

    99. Pruebas sobre customHooks

---
+++ 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,18 @@
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+import { renderHook } from '@testing-library/react-hooks';
+
+describe('Pruebas en el hook useFetchGifs', () => {
+
+    test('debe de retornar el estado inicial', () => {
+
+        const { result } = renderHook(() => useFetchGifs('One Punch'));
+        const { data, loading } = result.current;
+        // const { data, loading } = useFetchGifs('One Punch');
+
+        console.log(data, loading);
+
+        expect(data).toEqual([]);
+        expect(loading).toBe(true);
+    })
+
+})
