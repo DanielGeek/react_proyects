6b5f07f4bf4ace617fc612945fa41aab3da06253
DanielGeek <daniel30081990@gmail.com>
2020-10-01 20:51:52 -0300

    130. Toggle Todo - Marcar como completado o pendiente un TODO

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -32,6 +32,14 @@ export const TodoApp = () => {
         dispatch(action);
     }
 
+    const handleToggle = (todoId) => {
+
+        dispatch({
+            type: 'toggle',
+            payload: todoId
+        });
+    }
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -69,7 +77,12 @@ export const TodoApp = () => {
                                     key={todo.id}
                                     className="list-group-item"
                                 >
-                                    <p className="text-center ">{i + 1}. {todo.desc}</p>
+                                    <p
+                                        className={`${todo.done && 'complete'}`}
+                                        onClick={() => handleToggle(todo.id)}
+                                    >
+                                        {i + 1}. {todo.desc}
+                                    </p>
                                     <button
                                         className="btn btn-danger"
                                         onClick={() => handleDelete(todo.id)}
