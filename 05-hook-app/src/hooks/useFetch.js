9806c4f05d7abd181ce2b9b351cabed4a05dfe29
DanielGeek <daniel30081990@gmail.com>
2020-10-02 17:06:14 -0300

    149. Pruebas sobre useFetch - CustomHook

--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -27,7 +27,14 @@ export const useFetch = (url) => {
                     })
                 }
 
-            });
+            })
+            .catch(() => {
+                setState({
+                    data: null,
+                    loading: false,
+                    error: 'No se pudo cargar la info'
+                })
+            })
 
     }, [url])
 
