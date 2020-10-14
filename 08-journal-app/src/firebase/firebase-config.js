88cdb4b98decc1d73bb16188b4513edeedf7a18d
DanielGeek <daniel30081990@gmail.com>
2020-10-13 22:59:09 -0300

    280. Crear base de datos de desarrollo

--- 08-journal-app/src/firebase/firebase-config.js
+++ 08-journal-app/src/firebase/firebase-config.js
@@ -11,15 +11,33 @@ const firebaseConfig = {
     storageBucket: "react-app-cursos-d8cda.appspot.com",
     messagingSenderId: "72492644127",
     appId: "1:72492644127:web:6e9f9f5eeca9304abda22a"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
 
-  const db = firebase.firestore();
-  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const firebaseConfigTesting = {
+    apiKey: "AIzaSyDRdp6Xw9jpQbeEofzEVPlIXZk4IamuxZc",
+    authDomain: "redux-demo-c60e5.firebaseapp.com",
+    databaseURL: "https://redux-demo-c60e5.firebaseio.com",
+    projectId: "redux-demo-c60e5",
+    storageBucket: "redux-demo-c60e5.appspot.com",
+    messagingSenderId: "300165580143",
+    appId: "1:300165580143:web:43be8ab0adcc63ffeb149a"
+};
 
-  export {
-      db,
-      googleAuthProvider,
-      firebase
-  }
+if (process.env.NODE_ENV === 'test') {
+    // testing
+    firebase.initializeApp(firebaseConfigTesting);
+} else {
+    // dev/prod
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+}
+
+
+const db = firebase.firestore();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export {
+    db,
+    googleAuthProvider,
+    firebase
+}
--- 08-journal-app/src/tests/actions/notes.test.js
+++ 08-journal-app/src/tests/actions/notes.test.js
@@ -2,6 +2,8 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
 import { startNewNote } from '../../actions/notes';
+import { types } from '../../types/types';
+import { db } from '../../firebase/firebase-config';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -15,9 +17,37 @@ let store = mockStore({
 
 describe('Pruebas con las acciones de notes', () => {
 
-    test('debe de crear una nueva nota startNewNote', async() => {
-        
-        await store.dispatch( startNewNote() );
+    test('debe de crear una nueva nota startNewNote', async () => {
+
+        await store.dispatch(startNewNote());
+
+        const actions = store.getActions();
+        // console.log(actions);
+
+        expect(actions[0]).toEqual({
+            type: types.notesActive,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number)
+            }
+        });
+
+        expect(actions[1]).toEqual({
+            type: types.notesAddNew,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number)
+            }
+        });
+
+        // const docId .... action.... payload.... id
+        // await ..... db.... doc(``)..... .delete();
+        const docId = actions[0].payload.id;
+        await db.doc(`/TESTING/journal/notes/${docId}`).delete();
 
     })
 
