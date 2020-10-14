559a227b192ed59a7efcb79426fd4d40b6520c9e
DanielGeek <daniel30081990@gmail.com>
2020-10-13 22:26:29 -0300

    279. Pruebas de acciones asíncronas - startNewNote

--- 08-journal-app/src/actions/auth.js
+++ 08-journal-app/src/actions/auth.js
@@ -3,22 +3,22 @@ import Swal from 'sweetalert2';
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { types } from '../types/types';
 import { noteLogout } from './notes';
-import { StartLoading, FinishLoading } from './ui';
+import { startLoading, finishLoading } from './ui';
 
 export const startLoginEmailPassword = (email, password) => {
 
     return (dispatch) => {
 
-        dispatch(StartLoading());
+        dispatch(startLoading());
 
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(({ user }) => {
                 dispatch(login(user.uid, user.displayName));
 
-                dispatch(FinishLoading() );
+                dispatch(finishLoading());
             })
             .catch(e => {
-                dispatch(FinishLoading() );
+                dispatch(finishLoading());
                 Swal.fire('Error', e.message, 'error');
             })
     }
@@ -63,10 +63,10 @@ export const login = (uid, displayName) => ({
 });
 
 export const startLogout = () => {
-    return async( dispatch ) => {
+    return async (dispatch) => {
         await firebase.auth().signOut();
 
-        dispatch( logout() );
+        dispatch(logout());
         // purgo las notas del state
         dispatch(noteLogout());
     }
