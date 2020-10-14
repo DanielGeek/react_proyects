2324291ac926d332db7b266c0728b10d8ff51212
DanielGeek <daniel30081990@gmail.com>
2020-10-14 11:26:32 -0300

    287. Evaluar el login y el Google login

--- 08-journal-app/src/components/auth/LoginScreen.js
+++ 08-journal-app/src/components/auth/LoginScreen.js
@@ -12,8 +12,8 @@ export const LoginScreen = () => {
     const { loading } = useSelector(state => state.ui)
 
     const [ formValues, handleInputChange ] = useForm({
-        email: 'dangel@gmail.com',
-        password: '123123'
+        email: '',
+        password: ''
     });
 
     const { email, password } = formValues;
--- 08-journal-app/src/tests/components/auth/LoginScreen.test.js
+++ 08-journal-app/src/tests/components/auth/LoginScreen.test.js
@@ -9,6 +9,13 @@ import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 
 import { LoginScreen } from '../../../components/auth/LoginScreen';
+import { startGoogleLogin, startLoginEmailPassword } from '../../../actions/auth';
+
+// mock para saber si se llamo la funct
+jest.mock('../../../actions/auth', () => ({
+    startGoogleLogin: jest.fn(),
+    startLoginEmailPassword: jest.fn(),
+}))
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -22,6 +29,7 @@ const initState = {
 };
 
 let store = mockStore(initState);
+store.dispatch = jest.fn();
 
 const wrapper = mount( 
     <Provider store={ store }>
@@ -36,6 +44,7 @@ describe('Pruebas en <LoginScreen />', () => {
 
     beforeEach(()=> {
         store = mockStore(initState);
+        jest.clearAllMocks();
     })
 
     test('debe de mostrarse correctamente', () => {
@@ -43,4 +52,23 @@ describe('Pruebas en <LoginScreen />', () => {
         expect( wrapper ).toMatchSnapshot();
 
     });
+
+    test('debe de disparar la acción de startGoogleLogin', () => {
+        
+        wrapper.find('.google-btn').prop('onClick')();
+
+        expect( startGoogleLogin ).toHaveBeenCalled();
+    })
+
+    test('debe de disparar el startLogin con los respectivos argumentos', () => {
+        
+
+        wrapper.find('form').prop('onSubmit')({ 
+            preventDefault(){}
+        });
+
+        expect( startLoginEmailPassword ).toHaveBeenLastCalledWith('','');
+
+
+    })
 })
