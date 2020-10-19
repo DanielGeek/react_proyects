0264bda3a67fd52d23e8a78b549a67700b24b7b9
DanielGeek <daniel30081990@gmail.com>
2020-10-19 11:45:03 -0300

    324. Express Validator

--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -3,14 +3,28 @@
     host + /api/auth
 */
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { crearUsuario, loginusuario, revalidarToken } = require('../controllers/auth');
 
-router.post('/new', crearUsuario );
+router.post(
+    '/new',
+    [   // middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+    ], 
+    crearUsuario );
 
 
-router.post('/', loginusuario );
+router.post(
+    '/',
+    [   // middlewares
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+    ], 
+    loginusuario );
 
 router.get('/renew', revalidarToken );
 
