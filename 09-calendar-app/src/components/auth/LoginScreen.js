3dbc212431bffd4514ab660a36a71b3096f12ed7
DanielGeek <daniel30081990@gmail.com>
2020-10-23 23:59:33 -0300

    356. Disparar la acción de inicio del login de usuario

--- 09-calendar-app/src/components/auth/LoginScreen.js
+++ 09-calendar-app/src/components/auth/LoginScreen.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { startLogin } from '../../actions/auth';
+import { useForm } from '../../hooks/useForm';
 import './login.css';
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+
+    // custom hook para manejar formulario
+    const [formLoginValues, handleLoginInputChange] = useForm({
+        // inicializo los inputos con estos valores
+        lEmail: 'dangel@gmail.com',
+        lPassword: '123456'
+    });
+
+    const { lEmail, lPassword } = formLoginValues;
+
+    const handleLogin = (e) => {
+
+        e.preventDefault();
+        dispatch(startLogin(lEmail, lPassword));
+
+    }
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={handleLogin}>
                         <div className="form-group">
-                            <input 
+                            <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name="lEmail"
+                                value={lEmail}
+                                onChange={handleLoginInputChange}
                             />
                         </div>
                         <div className="form-group">
@@ -20,13 +45,16 @@ export const LoginScreen = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name="lPassword"
+                                value={lPassword}
+                                onChange={handleLoginInputChange}
                             />
                         </div>
                         <div className="form-group">
-                            <input 
+                            <input
                                 type="submit"
                                 className="btnSubmit"
-                                value="Login" 
+                                value="Login"
                             />
                         </div>
                     </form>
@@ -53,7 +81,7 @@ export const LoginScreen = () => {
                             <input
                                 type="password"
                                 className="form-control"
-                                placeholder="Contraseña" 
+                                placeholder="Contraseña"
                             />
                         </div>
 
@@ -61,14 +89,14 @@ export const LoginScreen = () => {
                             <input
                                 type="password"
                                 className="form-control"
-                                placeholder="Repita la contraseña" 
+                                placeholder="Repita la contraseña"
                             />
                         </div>
 
                         <div className="form-group">
-                            <input 
-                                type="submit" 
-                                className="btnSubmit" 
+                            <input
+                                type="submit"
+                                className="btnSubmit"
                                 value="Crear cuenta" />
                         </div>
                     </form>
