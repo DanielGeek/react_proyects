5d386dbcbf08a12ba1561b1a17007954854da11d
DanielGeek <daniel30081990@gmail.com>
2020-10-24 00:39:32 -0300

    357. Realizar la petición HTTP para autenticarnos

--- 09-calendar-app/src/actions/auth.js
+++ 09-calendar-app/src/actions/auth.js
@@ -1,7 +1,27 @@
+import { fetchSinToken } from "../helpers/fetch"
+import { types } from "../types/types";
 
 
 export const startLogin = (email, password) => {
-    return async () => {
-        console.log(email, password);
+    return async (dispatch) => {
+
+        const resp = await fetchSinToken('auth', { email, password }, 'POST');
+        const body = await resp.json();
+
+        if (body.ok) {
+            localStorage.setItem('token', body.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            // agrega al state en mi storage de mi reducer el uid y name
+            dispatch(login({
+                uid: body.uid,
+                name: body.name
+            }))
+        }
+
     }
 }
+
+const login = (user) => ({
+    type: types.authLogin,
+    payload: user
+})
