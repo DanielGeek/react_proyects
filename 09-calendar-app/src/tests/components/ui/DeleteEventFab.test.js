5ba0e1ce9dc7e54870aecff6251d80855794c093
DanielGeek <daniel30081990@gmail.com>
2020-10-25 22:23:33 -0300

    387. Pruebas en el componente DeleteEventFab

---
+++ 09-calendar-app/src/tests/components/ui/DeleteEventFab.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import '@testing-library/jest-dom';
+
+import { DeleteEventFab } from '../../../components/ui/DeleteEventFab';
+
+jest.mock('../../../actions/events', () => ({
+    eventStartDelete: jest.fn()
+}))
+
+
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {};
+const store = mockStore(initState);
+store.dispatch = jest.fn();
+
+
+const wrapper = mount(
+    <Provider store={store} >
+        <DeleteEventFab />
+    </Provider>
+)
+
+
+
+describe('Pruebas en <DeleteEventFab />', () => {
+
+
+    test('debe de mostrarse correctamente', () => {
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+})
+
