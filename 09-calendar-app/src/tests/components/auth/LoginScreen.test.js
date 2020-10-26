9c24734f3bd0cb04531336226890c38cbd7772df
DanielGeek <daniel30081990@gmail.com>
2020-10-26 00:47:46 -0300

    392. Pruebas cuando las contraseñas no son iguales

--- 09-calendar-app/src/tests/components/auth/LoginScreen.test.js
+++ 09-calendar-app/src/tests/components/auth/LoginScreen.test.js
@@ -4,14 +4,20 @@ import { Provider } from 'react-redux';
 
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import Swal from 'sweetalert2';
 
 import '@testing-library/jest-dom';
 import { LoginScreen } from '../../../components/auth/LoginScreen';
-import { startLogin } from '../../../actions/auth';
+import { startLogin, startRegister } from '../../../actions/auth';
 
 
 jest.mock('../../../actions/auth', () => ({
-    startLogin: jest.fn()
+    startLogin: jest.fn(),
+    startRegister: jest.fn()
+}));
+
+jest.mock('sweetalert2', () => ({
+    fire: jest.fn(),
 }));
 
 const middlewares = [thunk];
@@ -66,6 +72,62 @@ describe('Pruebas en <LoginScreen />', () => {
 
         expect(startLogin).toHaveBeenCalledWith('juan@gmail.com', '123456');
 
-    });
+    })
+
+    test('No hay registro si las contraseñas son diferentes', () => {
+
+        wrapper.find('input[name="rPassword1"]').simulate('change', {
+            target: {
+                name: 'rPassword1',
+                value: '123456',
+            }
+        });
+
+        wrapper.find('input[name="rPassword2"]').simulate('change', {
+            target: {
+                name: 'rPassword2',
+                value: '1234567',
+            }
+        });
+
+
+        wrapper.find('form').at(1).prop('onSubmit')({
+            preventDefault() { }
+        });
+
+        expect(startRegister).not.toHaveBeenCalled();
+        expect(Swal.fire).toHaveBeenCalledWith('Error', 'Las contraseñas deben de ser iguales', 'error');
+
+    })
+
+
+    test('Registro con contraseñas iguales', () => {
+
+        wrapper.find('input[name="rPassword1"]').simulate('change', {
+            target: {
+                name: 'rPassword1',
+                value: 'hola mundo',
+            }
+        });
+
+        wrapper.find('input[name="rPassword2"]').simulate('change', {
+            target: {
+                name: 'rPassword2',
+                value: 'hola mundo',
+            }
+        });
+
+
+        wrapper.find('form').at(1).prop('onSubmit')({
+            preventDefault() { }
+        });
+
+        expect(Swal.fire).not.toHaveBeenCalled();
+        expect(startRegister).toHaveBeenCalledWith('dangel@gmail.com', 'hola mundo', 'Daniel');
+
+    })
+
+
+
 
 })
