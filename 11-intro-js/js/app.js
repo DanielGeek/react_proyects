c88cdb4b04f865d08dc26afdf1d962a92a7caf84
DanielGeek <daniel30081990@gmail.com>
2020-10-27 00:26:48 -0300

    13. Template Strings en JavaScript

--- 11-intro-js/js/app.js
+++ 11-intro-js/js/app.js
@@ -1,8 +1,28 @@
-const musica = 'Rock';
+// scope
+// const musica = 'Rock';
 
-if (musica) {
-    const musica = 'Grunge';
-    console.log('dentro del if:', musica);
-}
+// if (musica) {
+//     const musica = 'Grunge';
+//     console.log('dentro del if:', musica);
+// }
 
-console.log('Fuera del IF', musica);
+// console.log('Fuera del IF', musica);
+
+// Template Strings
+const nombre = 'Daniel';
+const trabajo = 'Desarrollador Web';
+
+// Concatenar js
+console.log('Nombre: ' + nombre + ', Trabajo: ' + trabajo);
+console.log(`Nombre: ${nombre}, Trabajo: ${trabajo}`);
+
+// Concatenar con múltiples líneas
+const contenedorApp = document.querySelector('#app');
+let html = `
+        <ul>
+            <li>Nombre: ${nombre} </li>
+            <li>Trabajo: ${trabajo} </li>
+        </ul>
+    `;
+
+contenedorApp.innerHTML = html; 
