89f4de3fc45bcff5575aa24265b78df06083c335
DanielGeek <daniel30081990@gmail.com>
2020-10-27 00:36:16 -0300

    14. Funciones y los distintos tipos de funciones

--- 11-intro-js/js/app.js
+++ 11-intro-js/js/app.js
@@ -1,3 +1,36 @@
+// Function Declaretion
+function saludar(nombre) {
+
+    console.log('Bienvenido ' + nombre);
+}
+saludar('Daniel');
+
+// Function Expresion
+
+const cliente = function (nombreCliente) {
+    console.log(`Mostrando datos del cliente: ${nombreCliente}`);
+}
+cliente('Daniel');
+
+// Template Strings
+// const nombre = 'Daniel';
+// const trabajo = 'Desarrollador Web';
+
+// Concatenar js
+// console.log('Nombre: ' + nombre + ', Trabajo: ' + trabajo);
+// console.log(`Nombre: ${nombre}, Trabajo: ${trabajo}`);
+
+// Concatenar con múltiples líneas
+// const contenedorApp = document.querySelector('#app');
+// let html = `
+//         <ul>
+//             <li>Nombre: ${nombre} </li>
+//             <li>Trabajo: ${trabajo} </li>
+//         </ul>
+//     `;
+
+// contenedorApp.innerHTML = html; 
+
 // scope
 // const musica = 'Rock';
 
@@ -8,21 +41,3 @@
 
 // console.log('Fuera del IF', musica);
 
-// Template Strings
-const nombre = 'Daniel';
-const trabajo = 'Desarrollador Web';
-
-// Concatenar js
-console.log('Nombre: ' + nombre + ', Trabajo: ' + trabajo);
-console.log(`Nombre: ${nombre}, Trabajo: ${trabajo}`);
-
-// Concatenar con múltiples líneas
-const contenedorApp = document.querySelector('#app');
-let html = `
-        <ul>
-            <li>Nombre: ${nombre} </li>
-            <li>Trabajo: ${trabajo} </li>
-        </ul>
-    `;
-
-contenedorApp.innerHTML = html; 
