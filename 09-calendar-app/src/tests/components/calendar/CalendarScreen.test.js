5dcf1c3e00105f66b8821739bae24ff75244fed6
DanielGeek <daniel30081990@gmail.com>
2020-10-26 21:13:41 -0300

    393. Pruebas en CalendarScreen

---
+++ 09-calendar-app/src/tests/components/calendar/CalendarScreen.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import '@testing-library/jest-dom';
+
+import { CalendarScreen } from '../../../components/calendar/CalendarScreen';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {
+    calendar: {
+        events: []
+    },
+    auth: {
+        uid: '123',
+        name: 'Daniel'
+    },
+    ui: {
+        openModal: false
+    }
+};
+
+const store = mockStore(initState);
+store.dispatch = jest.fn();
+
+
+const wrapper = mount(
+    <Provider store={store} >
+        <CalendarScreen />
+    </Provider>
+)
+
+
+
+describe('Pruebas en <CalendarScreen />', () => {
+
+    test('debe de mostrarse correctamente', () => {
+
+        expect(wrapper).toMatchSnapshot();
+
+    });
+
+    test('pruebas con las interacciones del calendario', () => {
+
+    })
+
+})
