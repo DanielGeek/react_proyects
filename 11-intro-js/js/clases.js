387fbb4856d3a827f8ef183d7ba254de67afb08e
DanielGeek <daniel30081990@gmail.com>
2020-10-31 01:41:40 -0300

    30. Programación Orientada a Objetos - Clases - Heredando de una Clase

--- 11-intro-js/js/clases.js
+++ 11-intro-js/js/clases.js
@@ -6,7 +6,21 @@ class Tarea {
         this.prioridad = prioridad
     }
     mostrar() {
-        return `${this.nombre} tiene un prioridad de ${this.prioridad}`;
+        console.log(`${this.nombre} tiene un prioridad ${this.prioridad}`);
+    }
+    hola() {
+        return 'hola';
+    }
+}
+
+class ComprasPendientes extends Tarea {
+    constructor(nombre, prioridad, cantidad) {
+        super(nombre, prioridad);
+        this.cantidad = cantidad;
+    }
+    mostrar() {
+        super.mostrar();
+        console.log(`y la cantidad de ${this.cantidad}`);
     }
 }
 
@@ -16,7 +30,12 @@ let tarea2 = new Tarea('Aprender React', 'Alta');
 let tarea3 = new Tarea('Aprender Mongo', 'Alta');
 let tarea4 = new Tarea('Aprender Css', 'Baja');
 
-console.log(tarea1.mostrar());
-console.log(tarea2.mostrar());
-console.log(tarea3.mostrar());
-console.log(tarea4.mostrar());
+// console.log(tarea1.mostrar());
+// console.log(tarea2.mostrar());
+// console.log(tarea3.mostrar());
+// console.log(tarea4.mostrar());
+
+
+let compra1 = new ComprasPendientes('Arroz', 'Media', 3);
+compra1.mostrar();
+console.log(compra1.hola());
