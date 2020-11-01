f08056851719ac447ad883fc0888843822d34da0
DanielGeek <daniel30081990@gmail.com>
2020-11-01 20:43:10 -0300

    45. Eventos en React

--- 12-basicos/src/components/Producto.js
+++ 12-basicos/src/components/Producto.js
@@ -4,10 +4,17 @@ const Producto = ({ producto }) => {
 
     const { id, nombre, precio } = producto;
 
+    const seleccionarProducto = id => {
+        console.log('comprando... ', id);
+    }
     return (
         <div>
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            <button
+                type="button"
+                onClick={() => seleccionarProducto(id)}
+            >Comprar</button>
         </div>);
 }
 export default Producto;
