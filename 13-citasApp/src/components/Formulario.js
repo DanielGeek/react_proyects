ed842ca9da6aa65519e26d2eed6c12629338887c
DanielGeek <daniel30081990@gmail.com>
2020-11-02 23:29:49 -0300

    59. Creando las Citas

--- 13-citasApp/src/components/Formulario.js
+++ 13-citasApp/src/components/Formulario.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import uuid from 'uuid/v4';
 
-const Formulario = () => {
+const Formulario = ({ crearCita }) => {
 
     // Crear State de Citas
     const [cita, actualizarCita] = useState({
@@ -41,6 +41,7 @@ const Formulario = () => {
         cita.id = uuid();
 
         // Crear la cita
+        crearCita(cita);
 
         // Reiniciar el form
     }
