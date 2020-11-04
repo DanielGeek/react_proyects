bb48ec21a00641c5b8931d073ce5db69d81d1d40
DanielGeek <daniel30081990@gmail.com>
2020-11-03 21:00:44 -0300

    71. Leyendo el Presupuesto del Usuario

--- 14-presupuesto-app/src/components/Pregunta.js
+++ 14-presupuesto-app/src/components/Pregunta.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Pregunta = () => {
+
+    // definir el state
+    const [cantidad, guardarCantidad] = useState(0);
+
+    // Funcicón que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+
+    // Submit para definit el presupuesto
+    const agregrarPresupuesto = e => {
+        e.preventDefault();
+
+        // Validar
+
+        // Si se pasa la validación
+
+    }
+
     return (
         <>
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            <form onSubmit={agregrarPresupuesto}>
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
 
                 <input
