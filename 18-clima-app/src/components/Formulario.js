bd1a6c79056be88bb489ddf3188729ddd93df58f
DanielGeek <daniel30081990@gmail.com>
2020-11-15 15:20:22 -0300

    117. Creando el State y Leyendo lo que el usuario selecciona

--- 18-clima-app/src/components/Formulario.js
+++ 18-clima-app/src/components/Formulario.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Formulario = () => {
+
+    const [search, setSearch] = useState({
+        city: '',
+        country: ''
+    });
+
+    // extract city and country
+    const { city, country } = search;
+
+    // function put the elements in the state
+    const handleChange = e => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
         <form>
             <div className="input-field col s12">
                 <input
                     type="text"
-                    name="ciudad"
-                    id="ciudad"
+                    name="city"
+                    id="city"
+                    value={city}
+                    onChange={handleChange}
                 />
-                <label htmlFor="ciudad">Ciudad: </label>
+                <label htmlFor="city">City: </label>
             </div>
 
             <div className="input-field col s12">
                 <select
-                    name="pais"
-                    id="pais"
+                    name="country"
+                    id="country"
+                    value={country}
+                    onChange={handleChange}
                 >
-                    <option value="">-- Seleccione un país --</option>
+                    <option value="">-- Select Country --</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CH">Chile</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="PE">Perú</option>
+                    <option value="VE">Venezuela</option>
 
                 </select>
-                <label htmlFor="pais">País: </label>
+                <label htmlFor="country">País: </label>
             </div>
         </form>
     )
