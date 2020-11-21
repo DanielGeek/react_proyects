5a29b50cbbc0c69fae9de7d5100594a5be2cb92c
DanielGeek <daniel30081990@gmail.com>
2020-11-21 00:10:38 -0300

    168. Mostrando la letra de la canción

--- 22-canciones-app/src/App.js
+++ 22-canciones-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { Formulario } from './components/Formulario';
 
 import axios from 'axios';
+import { Cancion } from './components/Cancion';
 
 function App() {
 
@@ -21,8 +22,8 @@ function App() {
       const resultado = await axios(url);
 
       guardarLetra(resultado.data.lyrics);
-      console.log(letra);
     }
+
     consultarApiLetra();
 
   }, [busquedaletra]);
@@ -32,6 +33,19 @@ function App() {
       <Formulario
         guardarBusquedaLetra={guardarBusquedaLetra}
       />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+
+          </div>
+          <div className="col-md-6">
+            <Cancion
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
