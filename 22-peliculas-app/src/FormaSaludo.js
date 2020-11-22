5b52146ae53e7887e155f24b02c9771c4c6b3a1f
DanielGeek <daniel30081990@gmail.com>
2020-11-22 19:06:52 -0300

    12. Ejercicio: Conectando los componentes de React

--- 22-peliculas-app/src/FormaSaludo.js
+++ 22-peliculas-app/src/FormaSaludo.js
@@ -4,12 +4,23 @@ import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
 export class FormaSaludo extends Component {
 
     alEnviarForma = e => {
+        let datos = {};
         const nombre = e.target.nombre.value;
+        const mensaje = e.target.mensaje.value;
+
         e.preventDefault();
+
         if (typeof nombre === "string" && nombre.length > 0) {
-            this.props.alObtenerNuevoNombre(nombre);
+            datos.nombre = nombre;
             e.target.nombre.value = "";
         }
+
+        if (typeof mensaje === "string" && mensaje.length > 0) {
+            datos.mensaje = mensaje;
+            e.target.mensaje.value = "";
+        }
+
+        this.props.alObtenerNuevoSaludo(datos);
     };
 
     render() {
@@ -20,6 +31,10 @@ export class FormaSaludo extends Component {
                         <ControlLabel>Nombre</ControlLabel>
                         <FormControl type="text" id="nombre" name="nombre" />
                         <br />
+                        <ControlLabel>Mensaje</ControlLabel>
+                        <FormControl componentClass="textarea" id="mensaje" name="mensaje" />
+                        <br />
+
                         <Button type="submit">Saludar</Button>
                     </FormGroup>
                 </form>
