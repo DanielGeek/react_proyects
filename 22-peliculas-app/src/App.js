330774e6b84f7ff639462fa0471398745700a857
DanielGeek <daniel30081990@gmail.com>
2020-11-22 18:14:56 -0300

    9. Eventos de Usuario y llamado a Funciones en React

--- 22-peliculas-app/src/App.js
+++ 22-peliculas-app/src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
-import { Grid } from 'react-bootstrap';
+import { Grid, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
 
 export class App extends Component {
+    state = {
+        nombre: this.props.nombre
+    };
+
+    alOprimirBoton = e => {
+        const nombre = e.target.nombre.value;
+        e.preventDefault();
+        if (typeof nombre === "string" && nombre.length > 0) {
+            this.setState({
+                nombre: e.target.nombre.value
+            });
+            e.target.nombre.value = "";
+        }
+    };
+
     render() {
-        const { nombre, mensaje } = this.props;
+        const { nombre } = this.state;
+        const { mensaje } = this.props;
         return (
             <Grid>
                 <h1>Hola {nombre}!</h1>
                 <p>{mensaje}</p>
+                <form onSubmit={this.alOprimirBoton}>
+                    <FormGroup>
+                        <ControlLabel>Nombre</ControlLabel>
+                        <FormControl type="text" id="nombre" name="nombre" />
+                        <br />
+                        <Button type="submit">Saludar</Button>
+                    </FormGroup>
+                </form>
             </Grid>
         );
     }
