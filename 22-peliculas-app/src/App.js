89578fd97ef5222c9378d01af8a9d0619e4a35bf
DanielGeek <daniel30081990@gmail.com>
2020-11-22 19:15:10 -0300

    14. Creando el componente Saludo.js

--- 22-peliculas-app/src/App.js
+++ 22-peliculas-app/src/App.js
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
-import { Grid } from 'react-bootstrap';
-import FormaSaludo from './FormaSaludo';
-import MensajeSaludo from './MensajeSaludo';
+import Saludo from './Saludo';
 
 export class App extends Component {
-    state = {
-        nombre: this.props.nombre,
-        mensaje: this.props.mensaje
-    };
-
-    manejarNuevoSaludo = datos => {
-        this.setState(datos);
-    }
-
     render() {
-        const { nombre, mensaje } = this.state;
-        return (
-            <Grid>
 
-                <MensajeSaludo nombre={nombre} mensaje={mensaje} />
-                <FormaSaludo alObtenerNuevoSaludo={this.manejarNuevoSaludo} />
-            </Grid>
+        return (
+            <Saludo />
         );
     }
 }
-
-App.defaultProps = {
-    nombre: "React",
-    mensaje: "Mensaje por default"
-};
 
 export default App;
