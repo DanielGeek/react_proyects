fd84a6e9f6535bf640dee2cb41c4ffa834b53ba0
DanielGeek <daniel30081990@gmail.com>
2020-11-23 21:46:58 -0300

    17. Crear componente PeliculasDisponibles.js

--- 22-peliculas-app/src/App.js
+++ 22-peliculas-app/src/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Saludo } from './Saludo';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Pelicula from "./Pelicula";
+import { Saludo } from "./Saludo";
 
 export class App extends Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/" component={Saludo} />
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" component={Saludo} exact />
+            <Route path="/peliculas" component={Pelicula} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
