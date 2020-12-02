01b6e31ec6dba25c8c6aabe3e98aa8b51552ebda
DanielGeek <daniel30081990@gmail.com>
2020-12-02 00:09:02 -0300

    178. Leer lo que el usuario selecciona

--- 23-bebidas-app/src/components/Formulario.js
+++ 23-bebidas-app/src/components/Formulario.js
@@ -1,9 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CategoriasContext } from "../context/CategoriasContext";
 
 export const Formulario = () => {
-  // obtengo el state de mi context creado
+  // obtengo el state global de mi context creado
   const { categorias } = useContext(CategoriasContext);
+
+  const [busqueda, guardarBusqueda] = useState({
+    nombre: "",
+    categoria: ""
+  });
+
+  // función para leer los contenidos
+  const obtenerDatosReceta = e => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value
+    });
+  };
 
   return (
     <form className="col-12">
@@ -17,10 +30,15 @@ export const Formulario = () => {
             className="form-control"
             type="text"
             placeholder="Buscar por Ingrediente"
+            onChange={obtenerDatosReceta}
           />
         </div>
         <div className="col-md-4">
-          <select className="form-control" name="categoria">
+          <select
+            className="form-control"
+            name="categoria"
+            onChange={obtenerDatosReceta}
+          >
             <option value="">-- Selecciona Categoría --</option>
             {categorias.map(categoria => (
               <option key={categoria.strCategory} value={categoria.strCategory}>
