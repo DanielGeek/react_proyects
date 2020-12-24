9507adf9295203371a597983c518dbe7c7163b1b
DanielZerviz <daniel.angel@zerviz.cl>
2020-12-24 04:05:02 -0300

    216. Creando una función que obtenga las tareas del Proyecto

--- 24-MERN_Task/cliente/src/components/proyectos/Proyecto.js
+++ 24-MERN_Task/cliente/src/components/proyectos/Proyecto.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import proyectoContext from "../../context/proyectos/proyectoContext";
+import tareaContexT from '../../context/tareas/tareaContext';
 
 export const Proyecto = ({ proyecto }) => {
   // Obtener el state de proyectos
@@ -7,12 +8,22 @@ export const Proyecto = ({ proyecto }) => {
   // funcion para despachar y buscar el proyecto seleccionado por su id
   const { proyectoActual } = proyectosContext;
 
+  // Obtener la función del context de tarea
+  const tareasContext = useContext(tareaContexT);
+  const { obtenerTareas } = tareasContext;
+
+  // Función para agregar el proyecto actual
+  const seleccionarProyecto = id => {
+    proyectoActual(id); // Fijar un proyecto actual
+    obtenerTareas(id); // Filtrar las tareas cuando se de click
+  }
+
   return (
     <li>
       <button
         type="button"
         className="btn btn-blank"
-        onClick={() => proyectoActual(proyecto.id)}
+        onClick={() => seleccionarProyecto(proyecto.id)}
       >
         {proyecto.nombre}
       </button>
--- 24-MERN_Task/cliente/src/context/tareas/tareaState.js
+++ 24-MERN_Task/cliente/src/context/tareas/tareaState.js
@@ -2,33 +2,45 @@ import React, { useReducer } from "react";
 import TareaContext from "./tareaContext";
 import TareaReducer from "./tareaReducer"
 
+import { TAREAS_PROYECTO } from "../../types";
 
 const TareaState = props => {
     const initialState = {
         tareas: [
-            { id:1 ,nombre: "Elegir Plataforma", estado: true, proyectoId:1 },
-            { id:2 ,nombre: "Elegir Colores", estado: false, proyectoId:2 },
-            { id:3 ,nombre: "Elegir Plataformas de pago", estado: false, proyectoId:3 },
-            { id:4 ,nombre: "Elegir Hosting", estado: true, proyectoId:4 },
-            { id:1 ,nombre: "Elegir Plataforma", estado: true, proyectoId:2 },
-            { id:2 ,nombre: "Elegir Colores", estado: false, proyectoId:3 },
-            { id:3 ,nombre: "Elegir Plataformas de pago", estado: false, proyectoId:4 },
-            { id:4 ,nombre: "Elegir Hosting", estado: true, proyectoId:1 },
-            { id:1 ,nombre: "Elegir Plataforma", estado: true, proyectoId:2 },
-            { id:2 ,nombre: "Elegir Colores", estado: false, proyectoId:3 },
-            { id:3 ,nombre: "Elegir Plataformas de pago", estado: false, proyectoId:4 },
-            { id:4 ,nombre: "Elegir Hosting", estado: true, proyectoId:3 },
-            { id:4 ,nombre: "Elegir Hosting", estado: true, proyectoId:4 }
+            { id: 1, nombre: "Elegir Plataforma", estado: true, proyectoId: 1 },
+            { id: 2, nombre: "Elegir Colores", estado: false, proyectoId: 2 },
+            { id: 3, nombre: "Elegir Plataformas de pago", estado: false, proyectoId: 3 },
+            { id: 4, nombre: "Elegir Hosting", estado: true, proyectoId: 4 },
+            { id: 1, nombre: "Elegir Plataforma", estado: true, proyectoId: 2 },
+            { id: 2, nombre: "Elegir Colores", estado: false, proyectoId: 3 },
+            { id: 3, nombre: "Elegir Plataformas de pago", estado: false, proyectoId: 4 },
+            { id: 4, nombre: "Elegir Hosting", estado: true, proyectoId: 1 },
+            { id: 1, nombre: "Elegir Plataforma", estado: true, proyectoId: 2 },
+            { id: 2, nombre: "Elegir Colores", estado: false, proyectoId: 3 },
+            { id: 3, nombre: "Elegir Plataformas de pago", estado: false, proyectoId: 4 },
+            { id: 4, nombre: "Elegir Hosting", estado: true, proyectoId: 3 },
+            { id: 4, nombre: "Elegir Hosting", estado: true, proyectoId: 4 }
         ],
     }
 
     // Crear dispatch y state
-    const [state, dispatch ] = useReducer(TareaReducer, initialState);
+    const [state, dispatch] = useReducer(TareaReducer, initialState);
+
+    // Crear las funciones
+
+    //Obtener las tareas de un proyecto
+    const obtenerTareas = proyectoId => {
+        dispatch({
+            type: TAREAS_PROYECTO,
+            payload: proyectoId
+        })
+    }
 
     return (
         <TareaContext.Provider
             value={{
-                tareas: state.tareas
+                tareas: state.tareas,
+                obtenerTareas
             }}
         >
             {props.children}
