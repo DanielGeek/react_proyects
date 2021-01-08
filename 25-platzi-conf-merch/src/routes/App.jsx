e5cf2a348c28550452f9a15a64d332ebb84e60ce
DanielGeek <daniel30081990@gmail.com>
2021-01-08 01:40:41 -0300

    Maquetación y estilos del home

--- 25-platzi-conf-merch/src/routes/App.jsx
+++ 25-platzi-conf-merch/src/routes/App.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Checkout from '../containers/Checkout';
 import Home from '../containers/Home';
+import Checkout from '../containers/Checkout';
 import Information from '../containers/Information';
-import NotFound from '../containers/NotFound';
 import Payment from '../containers/Payment';
 import Success from '../containers/Success';
+import NotFound from '../containers/NotFound';
+import Layout from '../components/Layout';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/checkout/information" component={Information} />
-        <Route exact path="/checkout/payment" component={Payment} />
-        <Route exact path="/ckeckout/success" component={Success} />
-
-        <Route component={NotFound} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/checkout/information" component={Information} />
+          <Route exact path="/checkout/payment" component={Payment} />
+          <Route exact path="/checkout/success" component={Success} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 };
