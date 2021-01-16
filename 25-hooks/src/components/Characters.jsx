95b3e42253b34338ba1161b89cea37ef71dc67f4
DanielGeek <daniel30081990@gmail.com>
2021-01-15 23:08:55 -0300

    useCallback: evita cálculos innecesarios en funciones

--- 25-hooks/src/components/Characters.jsx
+++ 25-hooks/src/components/Characters.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useReducer, useMemo, useRef } from 'react';
+import React, { useEffect, useState, useReducer, useMemo, useRef, useCallback } from 'react';
+import Search from './Search';
 
 
 const initialState = {
@@ -33,9 +34,14 @@ const Characters = () => {
         dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
     }
 
-    const handleSearch = () => {
+    // const handleSearch = () => {
+    //     setSearch(searchInput.current.value);
+    // }
+
+    const handleSearch = useCallback(() => {
         setSearch(searchInput.current.value);
-    }
+    }, [])
+
     // trae todos lo que coincida en la busqueda de search
     // const filteredUsers = characters.filter((user) => {
     //     return user.name.toLowerCase().includes(search.toLowerCase());
@@ -57,14 +63,7 @@ const Characters = () => {
                 </li>
             ))}
 
-            <div className="Search">
-                <input
-                    type="text"
-                    value={search}
-                    ref={searchInput}
-                    onChange={handleSearch}
-                />
-            </div>
+            <Search search={search} searchInput={searchInput} handleSearch={handleSearch} />
 
             {filteredUsers.map(character => (
                 <div className="item" key={character.id}>
