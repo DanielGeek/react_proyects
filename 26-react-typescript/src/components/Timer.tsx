ed5e10997063b52066e3f12c1daa7f651cc288ea
DanielGeek <daniel30081990@gmail.com>
2021-01-28 23:56:26 -0300

    React + TypeScript: useEffect + useRef + Custom Props parte 1

---
+++ 26-react-typescript/src/components/Timer.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+export const Timer = () => {
+
+  const [segundos, setSegundos] = useState(0)
+
+  useEffect(() => {
+    setInterval(() => setSegundos(s => s + 1), 1000);
+  }, [])
+  return (
+    <>
+      <h4>Timer: <small>{segundos}</small></h4>
+    </>
+  )
+}
---
+++ 26-react-typescript/src/components/TimerPadre.tsx
@@ -0,0 +1,14 @@
+import { Timer } from './Timer'
+
+export const TimerPadre = () => {
+  return (
+    <>
+      <span>Milisegundos {1000}</span>
+      <br />
+      <button className="btn btn-outline-success">
+        1000
+      </button>
+      <Timer />
+    </>
+  )
+}
