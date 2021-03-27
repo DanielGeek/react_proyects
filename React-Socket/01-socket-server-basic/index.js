7bffbc6a5127bf43f0f5db0f08b50ea147a6941b
DanielGeek <daniel30081990@gmail.com>
2021-03-27 00:27:05 -0300

    15. Backend basado en clases

--- React-Socket/01-socket-server-basic/index.js
+++ React-Socket/01-socket-server-basic/index.js
@@ -1,35 +1,25 @@
-// Servidor de Express
-const express = require('express');
-const app = express();
+const Server = require('./models/server');
 
-// Servidor de sockets
-const server = require('http').createServer(app);
+const server = new Server();
 
-// Configuración del socket server
-const io = require('socket.io')(server);
+server.execute();
 
-// Desplegar el directorio público
-app.use( express.static(__dirname + '/public') );
+// io.on('connection', (socket) => {
 
-io.on('connection', (socket) => {
+//   // socket.emit('mensaje-bienvenida', {
+//   //   msg: 'Bienvenido al server',
+//   //   fecha: new Date()
+//   // });
 
-  // socket.emit('mensaje-bienvenida', {
-  //   msg: 'Bienvenido al server',
-  //   fecha: new Date()
-  // });
+//   // Escuchar el evento del cliente
+//   socket.on('mensaje-to-server', (data) => {
+//     console.log(data);
 
-  // Escuchar el evento del cliente
-  socket.on('mensaje-to-server', (data) => {
-    console.log(data);
-
-    // io para emitir a todos los clientes
-    io.emit('mensaje-from-server', data);
-  });
-});
+//     // io para emitir a todos los clientes
+//     io.emit('mensaje-from-server', data);
+//   });
+// });
 
 
 
 
-server.listen(8080, () => {
-  console.log('Server corriendo en puerto :8080')
-});
