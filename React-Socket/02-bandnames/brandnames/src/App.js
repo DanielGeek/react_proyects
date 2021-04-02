761d600a3846aca7cf3273a91a0299d609e372c8
DanielGeek <daniel30081990@gmail.com>
2021-04-01 23:59:03 -0300

    30. Configurar socket.io en React

--- React-Socket/02-bandnames/brandnames/src/App.js
+++ React-Socket/02-bandnames/brandnames/src/App.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import io from 'socket.io-client';
+
 import { BandAdd } from "./components/BandAdd";
 import { BandList } from "./components/BandList";
 
+const connectSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', {
+    transport: ['websocket']
+  });
+  return socket;
+}
+
 function App() {
+
+  const [ socket ] = useState(connectSocketServer())
+  const [ online, setOnline ] = useState(false);
+
   return (
     <div className="container">
       <div className="alert">
