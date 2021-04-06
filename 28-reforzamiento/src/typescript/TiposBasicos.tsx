fc13e56f163ed6408ebe504df01b71a945464660
DanielGeek <daniel30081990@gmail.com>
2021-04-06 03:26:33 -0400

    11. Tipos Básicos - TypeScript

--- 28-reforzamiento/src/typescript/TiposBasicos.tsx
+++ 28-reforzamiento/src/typescript/TiposBasicos.tsx
@@ -1,8 +1,18 @@
 
 export const TiposBasicos = () => {
+
+  const nombre: string = 'Daniel';
+  const edad: number = 30;
+  const estaActivo: boolean = false;
+
+  const poderes: string[] = []; //['Velocidad', 'Volar', 'Respirar en el agua'];
+
   return (
     <>
       <h3>Tipos Básicos</h3>
+      { nombre }, { edad }, { (estaActivo) ? 'activo': 'no activo' }
+      <br />
+      { poderes.join(', ')}
     </>
   )
 }
