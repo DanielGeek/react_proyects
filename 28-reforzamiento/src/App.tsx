0c8b941b6affeaa534c0bc6e16073ad6cbf27ab9
DanielZerviZ <daniel30081990@gmail.com>
2021-04-07 23:56:52 -0400

    Revert "12. Objetos literales e interfaces"
    This reverts commit 89273d65940f312019bb554e475ae3b7ec5d3f4b.

--- 28-reforzamiento/src/App.tsx
+++ 28-reforzamiento/src/App.tsx
@@ -1,13 +1,11 @@
-// import { TiposBasicos } from "./typescript/TiposBasicos"
-import { ObjetosLiterales } from './typescript/ObjetosLiterales';
+import { TiposBasicos } from "./typescript/TiposBasicos"
 
 const App = () => {
   return (
     <div className="mt-2">
       <h1>Introducción a TS - React </h1>
       <hr />
-      {/* <TiposBasicos /> */}
-      <ObjetosLiterales />
+      <TiposBasicos />
     </div>
   )
 }
