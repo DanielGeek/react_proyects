f78b0da64ddc5799435cbfdae93e63104d3ff640
DanielGeek <daniel30081990@gmail.com>
2021-04-15 00:14:17 -0400

    22. Mostrar usuarios en pantalla

--- 28-reforzamiento/src/components/Usuarios.tsx
+++ 28-reforzamiento/src/components/Usuarios.tsx
@@ -10,10 +10,30 @@ export const Usuarios = () => {
     // llamado al API
     reqResApi.get<ReqResListado>('/users')
       .then( resp => {
-        console.log(resp.data.data);
+        setUsuarios( resp.data.data);
       })
       .catch( console.log );
   }, [])
+
+  const renderItem = ( { id, first_name, last_name, email, avatar }: Usuario ) => {
+    return (
+        <tr key={ id.toString() }>
+          <td>
+            <img
+              src={ avatar }
+              alt={ first_name }
+              style={{
+                width: 35,
+                borderRadius: 100
+              }}
+              />
+          </td>
+          <td>Avatar</td>
+          <td> { first_name } { last_name } </td>
+          <td> { email } </td>
+        </tr>
+    )
+  }
 
   return (
     <>
@@ -26,7 +46,18 @@ export const Usuarios = () => {
             <th>Email</th>
           </tr>
         </thead>
+        <tbody>
+          {
+            usuarios.map( renderItem )
+          }
+        </tbody>
       </table>
+
+      <button
+        className="btn btn-primary"
+      >
+        Siguientes
+      </button>
     </>
   )
 }
