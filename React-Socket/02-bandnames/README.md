8e66c55a333c373e464fe8abf506b5ff5e21756e
DanielGeek <daniel30081990@gmail.com>
2021-05-04 01:34:23 -0400

    48. Conectar gráfica con la data del servidor

--- React-Socket/02-bandnames/README.md
+++ React-Socket/02-bandnames/README.md
@@ -1,3 +1,4 @@
 # App with React and Socket.io
 
 ![Screenshot](assets/img/bandsapp.png)
+![Screenshot](assets/img/bands_charts.png)
--- React-Socket/02-bandnames/bandnames/src/components/BandChart.js
+++ React-Socket/02-bandnames/bandnames/src/components/BandChart.js
@@ -1,43 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Chart from 'chart.js/auto';
+import { SocketContext } from '../context/SocketContext';
 
 export const BandChart = () => {
 
-  useEffect(() => {
+  const { socket } = useContext(SocketContext);
 
-    const ctx = document.getElementById('myChart');
-    var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-        indexAxis: 'y',
-        beginAtZero: true
+  useEffect(() => {
+    let chart = '';
+		// recibo la data de mi emisor server
+    socket.on('current-bands', (bands) => {
+      if(chart) {
+        chart.destroy()
       }
-  });
-  }, [])
+      chart = crearGrafica(bands);
+    })
+		return () => socket.off('current-bands');
+	}, [ socket ])
+
+  const crearGrafica = (bands = []) => {
+      const ctx = document.getElementById('myChart');
+      return new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: bands.map(band => band.name),
+            datasets: [{
+                label: '# of Votes',
+                data: bands.map(band => band.votes),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+          animation: false,
+          indexAxis: 'y',
+          beginAtZero: true
+        }
+    });
+  }
 
   return (
     <>
