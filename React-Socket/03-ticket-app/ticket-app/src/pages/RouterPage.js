45a80ceace5385e7ec69bd6d4a2cce8816a44e45
DanielGeek <daniel30081990@gmail.com>
2021-05-05 01:45:12 -0400

    55. Router entre pantallas

--- React-Socket/03-ticket-app/ticket-app/src/pages/RouterPage.js
+++ React-Socket/03-ticket-app/ticket-app/src/pages/RouterPage.js
@@ -7,22 +7,42 @@ import {
   UploadOutlined,
 } from '@ant-design/icons';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+import { Ingresar } from './Ingresar';
+import { Cola } from './Cola';
+import { CrearTicket } from './CrearTicket';
+import { Escritorio } from './Escritorio';
+
 const { Sider, Content } = Layout;
 
 export const RouterPage = () => {
   return (
-    <Layout style={{ height: '100vh'}}>
-        <Sider>
+    <Router>
+      <Layout style={{ height: '100vh'}}>
+        <Sider hidden={false}>
           <div className="logo" />
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1" icon={<UserOutlined />}>
-              Ingresar
+              <Link to="/ingresar">
+                Ingresar
+              </Link>
             </Menu.Item>
             <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              Cola
+              <Link to="/cola">
+                Cola de tickets
+              </Link>
             </Menu.Item>
             <Menu.Item key="3" icon={<UploadOutlined />}>
-              Crear Ticket
+              <Link to="/crear">
+                Crear Ticket
+              </Link>
             </Menu.Item>
           </Menu>
         </Sider>
@@ -35,9 +55,17 @@ export const RouterPage = () => {
               minHeight: 280,
             }}
           >
-            Content
+            <Switch>
+              <Route path="/ingresar" component={Ingresar}/>
+              <Route path="/cola" component={Cola}/>
+              <Route path="/crear" component={CrearTicket}/>
+              <Route path="/escritorio" component={Escritorio}/>
+
+              <Redirect to="/ingresar" />
+            </Switch>
           </Content>
         </Layout>
       </Layout>
+    </Router>
   )
 }
