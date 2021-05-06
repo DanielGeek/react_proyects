d24340b8cd4d20a599190f3e888686f0e900a8b1
DanielGeek <daniel30081990@gmail.com>
2021-05-05 23:43:48 -0400

    56. Pantalla de Ingresar

--- React-Socket/03-ticket-app/ticket-app/src/pages/Ingresar.js
+++ React-Socket/03-ticket-app/ticket-app/src/pages/Ingresar.js
@@ -1,9 +1,60 @@
 import React from 'react'
 
+import { Form, Input, Button, InputNumber } from 'antd';
+import { SaveOutlined } from '@ant-design/icons';
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 14 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 14 },
+};
+
 export const Ingresar = () => {
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div>
-      Ingresar Page
-    </div>
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="User name"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Desktop number"
+        name="desktop"
+        rules={[{ required: true, message: 'Please input your desktop number!' }]}
+      >
+        <InputNumber min={1} max={99} />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button
+            type="primary"
+            htmlType="submit"
+            shape="round"
+        >
+          <SaveOutlined />
+          Ingresar
+        </Button>
+      </Form.Item>
+    </Form>
   )
 }
