2a3d7476ac798cdf24143b64fed7fa1f33c3f3fd
DanielGeek <daniel30081990@gmail.com>
2021-05-06 00:19:53 -0400

    57. Pantalla de Escritorio

--- React-Socket/03-ticket-app/ticket-app/src/pages/Escritorio.js
+++ React-Socket/03-ticket-app/ticket-app/src/pages/Escritorio.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons';
+import { Row, Col, Typography, Button, Divider } from 'antd';
+
+const { Title, Text } = Typography;
 
 export const Escritorio = () => {
+
+  const logout = () => {
+    console.log('logout');
+  }
+
+  const nextTicket = () => {
+    console.log('nextTicket');
+  }
+
   return (
-    <div>
-      Escritorio Page
-    </div>
+    <>
+      <Row>
+        <Col span={ 20 }>
+            <Title level={ 2 }>Daniel</Title>
+            <Text>You are working in desktop number: </Text>
+            <Text type="success"> 5 </Text>
+        </Col>
+
+        <Col span={ 4 } align="right">
+            <Button
+                shape="round"
+                type="danger"
+                onClick={ logout }
+            >
+                <CloseCircleOutlined />
+                Logout
+            </Button>
+        </Col>
+
+    </Row>
+
+    <Divider />
+
+    <Row>
+        <Col>
+            <Text>You are attending the ticket number: </Text>
+            <Text
+                style={{ fontSize: 30 }}
+                type="danger"
+            >
+            55
+            </Text>
+        </Col>
+    </Row>
+
+
+    <Row>
+        <Col offset={ 18 } span={ 6 } align="right">
+            <Button
+                onClick={ nextTicket }
+                shape="round"
+                type="primary"
+            >
+                <RightOutlined />
+                Next
+            </Button>
+        </Col>
+      </Row>
+    </>
   )
 }
--- React-Socket/03-ticket-app/ticket-app/src/pages/Ingresar.js
+++ React-Socket/03-ticket-app/ticket-app/src/pages/Ingresar.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-import { Form, Input, Button, InputNumber } from 'antd';
+import { Form, Input, Button, InputNumber, Typography, Divider } from 'antd';
 import { SaveOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router';
+
+const { Title, Text } = Typography;
 
 const layout = {
   labelCol: { span: 8 },
@@ -13,8 +16,12 @@ const tailLayout = {
 
 export const Ingresar = () => {
 
+  const history = useHistory();
+
   const onFinish = (values) => {
     console.log('Success:', values);
+
+    history.push('/escritorio');
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -22,39 +29,45 @@ export const Ingresar = () => {
   };
 
   return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="User name"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input />
-      </Form.Item>
+    <>
+      <Title level={2}>Login</Title>
+      <Text>Input your name and desktop number</Text>
+      <Divider />
 
-      <Form.Item
-        label="Desktop number"
-        name="desktop"
-        rules={[{ required: true, message: 'Please input your desktop number!' }]}
+      <Form
+          {...layout}
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
       >
-        <InputNumber min={1} max={99} />
-      </Form.Item>
-
-      <Form.Item {...tailLayout}>
-        <Button
-            type="primary"
-            htmlType="submit"
-            shape="round"
+        <Form.Item
+          label="User name"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
         >
-          <SaveOutlined />
-          Ingresar
-        </Button>
-      </Form.Item>
-    </Form>
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Desktop number"
+          name="desktop"
+          rules={[{ required: true, message: 'Please input your desktop number!' }]}
+        >
+          <InputNumber min={1} max={99} />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button
+              type="primary"
+              htmlType="submit"
+              shape="round"
+          >
+            <SaveOutlined />
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   )
 }
