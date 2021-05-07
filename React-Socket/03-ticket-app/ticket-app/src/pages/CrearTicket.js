03ae454d0d01d9d270fc0db16f49a53c9d9a5580
DanielGeek <daniel30081990@gmail.com>
2021-05-07 00:13:28 -0400

    58. Pantalla para crear un ticket

--- React-Socket/03-ticket-app/ticket-app/src/pages/CrearTicket.js
+++ React-Socket/03-ticket-app/ticket-app/src/pages/CrearTicket.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Button, Col, Row, Typography } from 'antd';
+import {DownloadOutlined} from '@ant-design/icons';
+
+const { Title, Text } = Typography;
 
 export const CrearTicket = () => {
+
+  const newTicket = () => {
+    console.log('newTicket');
+  }
+
   return (
-    <div>
-      CrearTicket
-    </div>
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={3}>
+            Press the boton for a new ticket
+          </Title>
+
+          <Button
+            type="primary"
+            shape="round"
+            icon={<DownloadOutlined />}
+            size="large"
+            onClick={newTicket}
+          >
+            New Ticket
+          </Button>
+        </Col>
+      </Row>
+
+      <Row style={{marginTop: 100}}>
+        <Col span={14} offset={6} align="center">
+            <Text level={2}>
+                Your number
+            </Text>
+            <br />
+            <Text type="success" style={{fontSize: 55}}>
+                55
+            </Text>
+        </Col>
+      </Row>
+    </>
   )
 }
