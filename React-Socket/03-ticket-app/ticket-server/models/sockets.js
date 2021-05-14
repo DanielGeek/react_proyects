382f99f7fd271487c54d71d664a535db6fd5a2af
DanielGeek <daniel30081990@gmail.com>
2021-05-14 00:08:36 -0400

    67. Conectar la aplicación de React con nuestro socket server

--- React-Socket/03-ticket-app/ticket-server/models/sockets.js
+++ React-Socket/03-ticket-app/ticket-server/models/sockets.js
@@ -13,6 +13,8 @@ class Sockets {
         // On connection
         this.io.on('connection', ( socket ) => {
 
+            console.log('cliente conectado');
+
             // Escuchar evento: mensaje-to-server
             socket.on('mensaje-to-server', ( data ) => {
                 console.log( data );
