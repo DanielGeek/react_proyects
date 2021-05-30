e8a16cf42650d303d84cfdf7173d7dbd9484df5c
DanielGeek <daniel30081990@gmail.com>
2021-05-30 00:55:11 -0400

    91. Models needed in the backend

---
+++ React-Socket/05-maps/maps-server/models/markers.js
@@ -0,0 +1,24 @@
+
+class Markers {
+
+  contructor() {
+    this.actives = {};
+  }
+
+  addMarker(marker) {
+    this.actives[marker.id] = marker;
+    return marker;
+  }
+
+  removeMaker( id ) {
+    // delete property in obj
+    delete this.actives[id];
+  }
+
+  updateMarker( marker ) {
+    this.actives[marker.id] = marker;
+  }
+
+}
+
+module.exports = Markers;
