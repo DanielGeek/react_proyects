3dc7f444adc8d7d4fdf10548da77013412dbae2f
DanielGeek <daniel30081990@gmail.com>
2021-06-09 22:49:14 -0400

    110. Custom Middleware - campsValidator

--- React-Socket/06-chat/chat-backend/router/auth.js
+++ React-Socket/06-chat/chat-backend/router/auth.js
@@ -5,16 +5,23 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
+const { campsValidator } = require('../middlewares/camps-validator');
 
 const router = Router();
 
 // Create new users
-router.post('/new', createUser);
+router.post('/new', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    campsValidator
+], createUser);
 
 // Login
 router.post('/', [
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password es obligatorio').not().isEmpty()
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    campsValidator
 ], loginUser);
 
 // Revalidate token
