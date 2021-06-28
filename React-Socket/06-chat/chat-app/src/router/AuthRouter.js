7be63a4fc4666e60fa2057c3c29539ebeeae3a9c
DanielGeek <daniel30081990@gmail.com>
2021-06-27 21:58:59 -0400

    126. Login and Registration Design

--- React-Socket/06-chat/chat-app/src/router/AuthRouter.js
+++ React-Socket/06-chat/chat-app/src/router/AuthRouter.js
@@ -4,13 +4,21 @@ import { Route, Switch } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage';
 import { RegisterPage } from '../pages/RegisterPage';
 
+import '../css/login-register.css';
+
 export const AuthRouter = () => {
   return (
-    <Switch>
-      <Route exact path="/auth/login" component={ LoginPage } />
-      <Route exact path="/auth/register" component={ RegisterPage } />
+    <div className="limiter">
+		  <div className="container-login100">
+			  <div class="wrap-login100 p-t-50 p-b-90">
+          <Switch>
+            <Route exact path="/auth/login" component={ LoginPage } />
+            <Route exact path="/auth/register" component={ RegisterPage } />
 
-      <Redirect to="/auth/login" />
-    </Switch>
+            <Redirect to="/auth/login" />
+          </Switch>
+        </div>
+      </div>
+    </div>
   )
 }
