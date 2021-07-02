53c71bb95abf12100ed0d4c92814ca69bb3fdb15
DanielGeek <daniel30081990@gmail.com>
2021-07-02 00:18:43 -0400

    134. AuthProvider - AuthContext

--- React-Socket/06-chat/chat-app/src/router/AuthRouter.js
+++ React-Socket/06-chat/chat-app/src/router/AuthRouter.js
@@ -10,7 +10,7 @@ export const AuthRouter = () => {
   return (
     <div className="limiter">
 		  <div className="container-login100">
-			  <div class="wrap-login100 p-t-50 p-b-90">
+			  <div className="wrap-login100 p-t-50 p-b-90">
           <Switch>
             <Route exact path="/auth/login" component={ LoginPage } />
             <Route exact path="/auth/register" component={ RegisterPage } />
