a426a2b9fa9639cf7ee08fbe775ff5c06797e0de
DanielGeek <daniel30081990@gmail.com>
2021-07-05 23:48:55 -0400

    140. Task: Registration Screen

--- React-Socket/06-chat/chat-app/src/auth/AuthContext.js
+++ React-Socket/06-chat/chat-app/src/auth/AuthContext.js
@@ -38,7 +38,27 @@ export const AuthProvider = ({ children }) => {
 
   }
 
-  const register = (name, email, password) => {
+  const register = async(name, email, password) => {
+
+    const resp = await fetchSinToken('login/new', { name, email, password }, 'POST');
+
+    if( resp.ok ) {
+      localStorage.setItem('token', resp.token );
+      const { user } = resp;
+
+      setAuth({
+        uid: user.uid,
+        checking: false,
+        logged: true,
+        name: user.name,
+        email: user.email,
+      });
+
+      console.log('Autenticate!');
+      return true;
+    }
+
+    return resp.msg;
 
   }
 
