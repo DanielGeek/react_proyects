0dd7b6b7eba21a9db786ff918503a2da6dca227e
DangelGlobant <daniel30081990@gmail.com>
2021-12-06 20:54:37 -0300

    homeWork

---
+++ 02-intro-javascript/src/bases/01-homeWork.js
@@ -0,0 +1,16 @@
+const n1 = 10;
+const n2 = 10;
+
+let suma = n1 + n2;
+
+console.log(`La suma es ${suma}`);
+
+if(true) {
+  const n1 = 20;
+  const n2 = 10;
+
+  let suma = n1 + n2;
+
+  console.log(`La suma es ${suma}`);
+
+}
