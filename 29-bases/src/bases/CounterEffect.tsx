bf92a31c0e4bd3aa5ac9587166c5177cd922c6f5
DangelGlobant <daniel30081990@gmail.com>
2021-12-18 01:13:22 -0300

    16. useRef and TimeLines

--- 29-bases/src/bases/CounterEffect.tsx
+++ 29-bases/src/bases/CounterEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
@@ -6,26 +6,29 @@ const MAXIMUN_COUNT = 10;
 export const CounterEffect = () => {
 
   const [counter, setCounter] = useState(5);
+  const counterElement = useRef<HTMLHeadingElement>(null);
 
   const handleClick = () => {
       setCounter( prev => Math.min( prev + 1, MAXIMUN_COUNT ) );
   }
 
-  useEffect(() => {
+  useLayoutEffect(() => {
 
     if ( counter < 10 ) return;
 
     console.log('%cLimit Max Value', 'color: red; background-color: black;');
 
-    gsap.to('h1', { y: -10, duration: 0.2, ease:'ease.out'}).then( () => {
-      gsap.to('h1', { y: 0, duration: 1, ease:'bounce.out'});
-    });
+    const tl = gsap.timeline();
+
+    tl.to( counterElement.current, { y: -10, duration: 0.2, ease:'ease.out'})
+      .to( counterElement.current, { y: 0, duration: 1, ease:'bounce.out'})
+
   }, [counter])
 
   return (
     <>
       <h1>CounterEffect</h1>
-      <h1>{ counter }</h1>
+      <h2 ref={ counterElement }>{ counter }</h2>
 
       <button onClick={ handleClick }>
         +1
