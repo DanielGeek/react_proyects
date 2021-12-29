8595a0d66687eb8d9953155113503e88c2a50956
DangelGlobant <daniel30081990@gmail.com>
2021-12-29 18:46:10 -0300

    61. Composite Component Pattern - Getting Started

--- 30-react-app/src/02-component-patterns/components/ProductCard.tsx
+++ 30-react-app/src/02-component-patterns/components/ProductCard.tsx
@@ -14,16 +14,26 @@ interface Product {
   img?: string;
 }
 
-export const ProductCard = ({ product }: Props) => {
-
-  const { counter, increaseBy } = useProduct();
-
+export const ProductImage = ({ img = '' }) => {
   return (
-    <div className={ styles.productCard }>
-      <img className={ styles.productImg } src={ product.img ? product.img : noImage } alt="Coffee Mug" />
+    <img className={ styles.productImg } src={ img ? img : noImage } alt="Product Image" />
+  );
+}
 
-      <span className={ styles.productDescription}>{ product.title }</span>
 
+export const ProductTitle = ({ title }: { title: string }) => {
+  return (
+    <span className={ styles.productDescription}>{ title }</span>
+    );
+  }
+
+interface ProductButtonsProps {
+    increaseBy: ( value: number ) => void;
+    counter: number;
+}
+
+export const ProductButtons = ({ increaseBy, counter }: ProductButtonsProps ) => {
+  return (
       <div className={ styles.buttonsContainer }>
         <button
             className={ styles.buttonMinus}
@@ -35,6 +45,25 @@ export const ProductCard = ({ product }: Props) => {
             className={ styles.buttonAdd }
             onClick={ () => increaseBy( +1 )}> + </button>
       </div>
+  );
+}
+
+export const ProductCard = ({ product }: Props) => {
+
+  const { counter, increaseBy } = useProduct();
+
+  return (
+    <div className={ styles.productCard }>
+
+      <ProductImage img={ product.img} />
+
+      <ProductTitle title={ product.title} />
+
+      <ProductButtons
+          increaseBy={ increaseBy }
+          counter={ counter }
+      />
+
     </div>
   )
 }
