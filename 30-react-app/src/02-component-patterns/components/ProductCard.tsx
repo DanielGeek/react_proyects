ca6885e89365847f673ac846232bdfe2000a4193
DangelGlobant <daniel30081990@gmail.com>
2021-12-30 21:45:26 -0300

    [Fixed error] 66. Assign components to another component

--- 30-react-app/src/02-component-patterns/components/ProductCard.tsx
+++ 30-react-app/src/02-component-patterns/components/ProductCard.tsx
@@ -1,37 +1,27 @@
 import { createContext } from 'react';
 
 import { useProduct } from '../hooks/useProduct';
-import { ProductCardProps, ProductContextProps } from '../interfaces/interfaces';
+import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces';
 
-import styles from '../styles/styles.module.css';
-import coffeeMug from '../assets/coffee-mug.png';
-
+import styles from '../styles/styles.module.css'
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ children, product }: ProductCardProps) => {
 
-  const { counter, increaseBy } = useProduct();
+export const ProductCard = ({ children, product }: ProductCardProps ) => {
 
-  return (
-    <Provider value={{
-        counter,
-        increaseBy,
-        product
-    }}>
-      <div className={ styles.productCard }>
-        { children }
-        {/* <ProductImage img={ product.img} />
+    const { counter, increaseBy } = useProduct();
 
-        <ProductTitle title={ product.title} />
-
-        <ProductButtons
-            increaseBy={ increaseBy }
-            counter={ counter }
-        /> */}
-
-      </div>
-    </Provider>
-  )
+    return (
+        <Provider value={{
+            counter,
+            increaseBy,
+            product
+        }}>
+            <div className={ styles.productCard }>
+                { children }
+            </div>
+        </Provider>
+    )
 }
