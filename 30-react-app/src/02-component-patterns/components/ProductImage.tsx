5579fb1ac0faae67b3c3447a6bc95bcec4ac49c8
DangelGlobant <daniel30081990@gmail.com>
2022-01-01 20:45:46 -0300

    73. className in the ProductTitle and ProductImage

--- 30-react-app/src/02-component-patterns/components/ProductImage.tsx
+++ 30-react-app/src/02-component-patterns/components/ProductImage.tsx
@@ -4,8 +4,12 @@ import { ProductContext } from './ProductCard';
 import styles from '../styles/styles.module.css'
 import noImage from '../assets/no-image.jpg';
 
+interface Props {
+    img?: string;
+    className?: string;
+}
 
-export const ProductImage = ({ img = '' }) => {
+export const ProductImage = ({ img, className }: Props ) => {
 
     const { product } = useContext( ProductContext );
     let imgToShow: string;
@@ -20,6 +24,10 @@ export const ProductImage = ({ img = '' }) => {
 
 
     return (
-        <img className={ styles.productImg } src={ imgToShow } alt="Product" />
+        <img
+            className={ `${ styles.productImg } ${ className }` }
+            src={ imgToShow }
+            alt="Product"
+        />
     );
 }
