eda26f9787bca164bc839c09db582cf3ef90094b
DangelGlobant <daniel30081990@gmail.com>
2022-01-03 22:41:07 -0300

    82. Problem and need for property control

--- 30-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 30-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,7 +1,9 @@
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
+
+import { Product } from '../interfaces/interfaces';
 import '../styles/custom-styles.css';
 
-const product = {
+const product1 = {
     id: '1',
     title: 'Coffee Mug - Card',
     img: './coffee-mug.png'
@@ -12,6 +14,8 @@ const product2 = {
     title: 'Coffee Mug - Meme',
     img: './coffee-mug2.png'
 }
+
+const products: Product[] = [ product1, product2 ];
 
 
 
@@ -26,25 +30,39 @@ export const ShoppingPage = () => {
                 flexDirection: 'row',
                 flexWrap: 'wrap'
             }}>
+                {
+                    products.map( product => (
+                        <ProductCard
+                            key={ product.id }
+                            product={ product }
+                            className="bg-dark text-white"
+                        >
+                            <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
+                            <ProductTitle className="text-bold" />
+                            <ProductButtons className="custom-buttons" />
+                        </ProductCard>
+                    ))
+                }
 
-                <ProductCard
-                    product={ product }
-                    className="bg-dark text-white "
-                >
-                    <ProductCard.Image className="custom-image" />
-                    <ProductCard.Title className="text-bold" />
-                    <ProductCard.Buttons className="custom-buttons" />
-                </ProductCard>
+            </div>
 
+            <div className="shopping-cart">
                 <ProductCard
                     product={ product2 }
-                    className="bg-dark text-white "
+                    className="bg-dark text-white"
+                    style={{ width: '100px' }}
                 >
                     <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
-                    <ProductTitle className="text-bold" />
                     <ProductButtons className="custom-buttons" />
                 </ProductCard>
-
+                <ProductCard
+                    product={ product1 }
+                    className="bg-dark text-white"
+                    style={{ width: '100px' }}
+                >
+                    <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
+                    <ProductButtons className="custom-buttons" />
+                </ProductCard>
             </div>
         </div>
     )
