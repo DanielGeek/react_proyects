2561c250908b2dda04d7512d9f2cbb829d431afd
DangelGlobant <daniel30081990@gmail.com>
2022-01-14 10:06:56 -0300

    102. Task: isMaxCountReaced

--- 30-react-app/src/02-component-patterns/components/ProductCard.tsx
+++ 30-react-app/src/02-component-patterns/components/ProductCard.tsx
@@ -21,12 +21,13 @@ export interface Props {
 
 export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props ) => {
 
-    const { counter, increaseBy } = useProduct({ onChange, product, value, initialValues });
+    const { counter, increaseBy, maxCount } = useProduct({ onChange, product, value, initialValues });
 
     return (
         <Provider value={{
             counter,
             increaseBy,
+            maxCount,
             product
         }}>
             <div
--- 30-react-app/src/02-component-patterns/hooks/useProduct.ts
+++ 30-react-app/src/02-component-patterns/hooks/useProduct.ts
@@ -38,7 +38,8 @@ export const useProduct = ( { onChange, product, value = 0, initialValues }: use
 
     return {
         counter,
-        increaseBy
+        increaseBy,
+        maxCount: initialValues?.maxCount,
     }
 
 }
