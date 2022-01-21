18539bafbae82f1166ae7fe10baed8c8bacb35e0
DangelGlobant <daniel30081990@gmail.com>
2022-01-21 17:24:22 -0300

    116. Tests on ProductTitle

---
+++ dab-product-card/test/data/products.ts
@@ -0,0 +1,11 @@
+export const product1 = {
+  id: '1',
+  title: 'Coffe Mug - Withou image',
+  // img: './coffe-mug.png
+}
+
+export const product2 = {
+  id: '1',
+  title: 'Coffe Mug - With Image',
+  img: './coffee-mug.png'
+}
