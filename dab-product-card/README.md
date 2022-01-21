f635dac7fb434308315f7ab9ae7955d920937704
DangelGlobant <daniel30081990@gmail.com>
2022-01-20 23:01:23 -0300

    113. Step #4 - Build

--- dab-product-card/README.md
+++ dab-product-card/README.md
@@ -11,7 +11,6 @@ import { ProductCard, ProductImage, ProductTitle, ProductButtons } from 'dab-pro
 
 ```
 <ProductCard
-    key={ product.id }
     product={ product }
     initialValues={{
         count: 6,
--- dab-product-card/example/index.tsx
+++ dab-product-card/example/index.tsx
@@ -1,13 +1,35 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../src';
+
+const product = {
+  id: '1',
+  title: 'Coffee Mug - Card',
+  // img: './coffe-mug.png'
+}
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <>
+      <ProductCard
+        product={ product }
+        initialValues={{
+            count: 6,
+            // maxCount: 10,
+        }}
+      >
+        {
+            ({ reset, count, isMaxCountReached, maxCount, increaseBy }) => (
+                <>
+                    <ProductImage />
+                    <ProductTitle />
+                    <ProductButtons />
+                </>
+            )
+        }
+      </ProductCard>
+    </>
   );
 };
 
