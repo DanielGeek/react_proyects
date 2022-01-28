86f96b79a8177fd6d1477a2e091d565ee63f77ef
DangelGlobant <daniel30081990@gmail.com>
2022-01-28 15:07:34 -0300

    125.start proyect - Forms

--- 30-react-app/src/routes/Navigation.tsx
+++ 30-react-app/src/routes/Navigation.tsx
@@ -6,7 +6,6 @@ import {
 } from 'react-router-dom';
 
 import logo from '../logo.svg';
-import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 export const Navigation = () => {
   return (
@@ -16,7 +15,7 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Shopping</NavLink>
+              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
             </li>
             <li>
               <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
@@ -30,14 +29,14 @@ export const Navigation = () => {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
           <Route path="/about">
             <h1>About</h1>
           </Route>
           <Route path="/users">
             <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <ShoppingPage />
           </Route>
         </Switch>
       </div>
