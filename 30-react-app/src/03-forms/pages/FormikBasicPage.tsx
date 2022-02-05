21d6bed2725433b30829c0a57b091d94d4e04b59
DangelGlobant <daniel30081990@gmail.com>
2022-02-04 23:13:48 -0300

    133. Formik - Manual validations

--- 30-react-app/src/03-forms/pages/FormikBasicPage.tsx
+++ 30-react-app/src/03-forms/pages/FormikBasicPage.tsx
@@ -1,8 +1,40 @@
-import { useFormik } from 'formik';
+import { FormikErrors, useFormik } from 'formik';
 
 import '../styles/styles.css';
 
+interface FormValues {
+  firstName: string,
+  lastName: string,
+  email: string,
+}
+
 export const FormikBasePage = () => {
+
+  const validate = ( { firstName, lastName, email }: FormValues ) => {
+
+    const errors: FormikErrors<FormValues> = {};
+
+    if( !firstName ) {
+      errors.firstName = 'Required';
+    } else if ( firstName.length > 15 ) {
+      errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if( !lastName ) {
+      errors.lastName = 'Required';
+    } else if ( lastName.length > 15 ) {
+      errors.lastName = 'Must be 15 characters or less';
+    }
+
+    if (!email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    return errors;
+
+  }
 
   const { handleChange, values, handleSubmit } = useFormik({
     initialValues: {
@@ -12,7 +44,8 @@ export const FormikBasePage = () => {
     },
     onSubmit: (values) => {
       console.log(values);
-    }
+    },
+    validate
   });
 
   return (
