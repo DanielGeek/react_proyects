c72ad2d931bda5a3124cecbb668039dce089b070
DangelGlobant <daniel30081990@gmail.com>
2022-02-06 14:56:28 -0300

    12. How to make queries to get UI elements

---
+++ 32-react-testing-library-examples/src/components/hello-world.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable testing-library/no-debugging-utils */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import HelloWorld from './hello-world';
+
+test('renders hello world', () => {
+  render(<HelloWorld />)
+  // screen.debug();
+
+  const title = screen.getByText(/hello world/i);
+  // screen.debug(title);
+  expect(title).toBeInTheDocument();
+});
