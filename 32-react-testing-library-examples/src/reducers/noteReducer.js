1a6743851ca9c974f8fed6b21dbffc459e61b341
DangelGlobant <daniel30081990@gmail.com>
2022-02-12 00:25:57 -0300

    Creating a Redux Reducer with TDD and best practices

---
+++ 32-react-testing-library-examples/src/reducers/noteReducer.js
@@ -0,0 +1,21 @@
+export const noteReducer = (state = [], action) => {
+  if(action.type === '@notes/created') {
+    // return state.concat(action.payload);
+    return [ ...state, action.payload];
+  }
+
+  if(action.type === '@notes/toogle_important') {
+    const { id } = action.payload;
+    return state.map(note => {
+      if ( note.id === id ) {
+        return {
+          ...note,
+          important: !note.important
+        }
+      }
+      return note;
+    });
+  };
+
+  return state;
+}
