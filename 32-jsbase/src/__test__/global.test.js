566f3c0252c13910361ebbf24d48b85e926ddbe0
DangelGlobant <daniel30081990@gmail.com>
2022-02-13 03:40:00 -0300

    Implementing tests for Boolean and Array

--- 32-jsbase/src/__test__/global.test.js
+++ 32-jsbase/src/__test__/global.test.js
@@ -1,5 +1,28 @@
 const text = "Hello World";
+const fruits = ['Apple', 'Melon', 'Bannana'];
 
 test('Should have a text', () => {
   expect(text).toMatch(/World/);
+});
+
+test('Should have a Bannana', () => {
+  expect(fruits).toContain('Bannana');
+});
+
+test('Should have a number Greater', () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test('Should have true', () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""));
+};
+
+test('Should have a reverse string', () => {
+  reverseString('Hello', (str) => {
+    expect(str).toBe('olleH');
+  });
 });
