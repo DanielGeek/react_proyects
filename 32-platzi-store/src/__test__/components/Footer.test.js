73cc65fb05e492e4e94bd2ae2b9eb04d063a4451
DangelGlobant <daniel30081990@gmail.com>
2022-02-18 09:46:49 -0300

    Create mocks

--- 32-platzi-store/src/__test__/components/Footer.test.js
+++ 32-platzi-store/src/__test__/components/Footer.test.js
@@ -7,8 +7,12 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+
   test('Render the Footer component', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
   });
+  test('Render of title', () => {
+    expect(footer.find(".Footer-title").text()).toEqual("Platzi Store");
+  })
 });
