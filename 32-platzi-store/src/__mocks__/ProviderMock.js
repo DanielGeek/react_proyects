2b46c91cc7ac27ec703a408de509aa2eff7a0e0c
DangelGlobant <daniel30081990@gmail.com>
2022-02-19 01:01:06 -0300

    Deployment Provider Mockup

--- 32-platzi-store/src/__mocks__/ProviderMock.js
+++ 32-platzi-store/src/__mocks__/ProviderMock.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createBrowserHistory } from 'history';
+import { createMemoryHistory } from 'history';
 import initialState from '../initialState';
 import reducer from '../reducers';
 
 const store = createStore(reducer, initialState);
-const history = createBrowserHistory();
+const history = createMemoryHistory();
 
 const ProviderMock = props => (
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       {props.children}
     </Router>
   </Provider>
