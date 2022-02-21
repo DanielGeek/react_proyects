b8eac70585a2bee7c92972a4e5912327bc93a981
DangelGlobant <daniel30081990@gmail.com>
2022-02-21 08:08:30 -0300

    161. Automatically document descriptions

--- 33-my-storybook/src/components/MyLabel.tsx
+++ 33-my-storybook/src/components/MyLabel.tsx
@@ -1,12 +1,20 @@
 import './mylabel.css';
 
 export interface MyLabelProps {
+  /**
+   * Is this the message to show in the label
+   */
   label: string;
+  /**
+   * Is this the size of the label
+   */
   size: 'normal'|'h1'|'h2'|'h3';
 }
 
 export const MyLabel = ( { label = 'No Label', size = 'normal' }: MyLabelProps ) => {
   return (
-    <span className={ `${ size }`}>{ label }</span>
+    <span className={`${ size }`}>
+      { label }
+    </span>
   )
 }
