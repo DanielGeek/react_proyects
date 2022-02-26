725740c28a7e8eecd9a05bd4c6f8549023869db2
DangelGlobant <daniel30081990@gmail.com>
2022-02-26 01:00:32 -0300

    38. Validate that the input to filter exists

--- 32-testing-store-app/src/components/github-search-page/github-search-page.test.js
+++ 32-testing-store-app/src/components/github-search-page/github-search-page.test.js
@@ -3,12 +3,21 @@ import { render, screen } from '@testing-library/react';
 
 import { GithubSearchPage } from './github-serach-page';
 
+const setup = () => render(<GithubSearchPage />);
+
 describe('When the GithubSearchPage is mounted', () => {
+
 	it('must be display the title', () => {
-		render(<GithubSearchPage />);
+    setup();
 
 		expect(
 			screen.getByRole('heading', { name: /github repositories list page/i })
 		).toBeInTheDocument();
 	});
+
+  it('must be an input text with label "filter by" field', () => {
+    setup();
+
+    expect(screen.getByLabelText(/filter by/i)).toBeInTheDocument();
+  });
 });
