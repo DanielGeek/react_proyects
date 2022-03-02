0c73a8534b5857b0f19e2b999e7482402cc2b2ab
DangelGlobant <daniel30081990@gmail.com>
2022-03-02 12:04:11 -0300

    41. Implement initial status message of the app

--- 32-testing-store-app/src/components/github-search-page/github-search-page.test.js
+++ 32-testing-store-app/src/components/github-search-page/github-search-page.test.js
@@ -6,25 +6,32 @@ import { GithubSearchPage } from './github-serach-page';
 const setup = () => render(<GithubSearchPage />);
 
 describe('When the GithubSearchPage is mounted', () => {
+	it('must be display the title', () => {
+		setup();
 
-  it('must be display the title', () => {
-    setup();
+		expect(
+			screen.getByRole('heading', { name: /github repositories list page/i })
+		).toBeInTheDocument();
+	});
 
+	it('must be an input text with label "filter by" field', () => {
+		setup();
+
+		expect(screen.getByLabelText(/filter by/i)).toBeInTheDocument();
+	});
+
+	it('must be a Search Button', () => {
+		setup();
+
+		expect(screen.getByRole('button', { name: /search/i })).toBeInTheDocument();
+	});
+
+	it('must be a initial message `Please provide a search option and click in the search button`.', () => {
+		setup();
     expect(
-      screen.getByRole('heading', { name: /github repositories list page/i })
-    ).toBeInTheDocument();
-  });
-
-  it('must be an input text with label "filter by" field', () => {
-    setup();
-
-    expect(screen.getByLabelText(/filter by/i)).toBeInTheDocument();
-  });
-
-  it('must be a Search Button', () => {
-    setup();
-
-    expect(screen.getByRole('button', {name: /search/i})).toBeInTheDocument()
-  })
-
+			screen.getByText(
+				/please provide a search option and click in the search button/i
+			)
+		).toBeInTheDocument();
+	});
 });
--- 32-testing-store-app/src/components/github-search-page/github-serach-page.js
+++ 32-testing-store-app/src/components/github-search-page/github-serach-page.js
@@ -4,23 +4,37 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 
 export const GithubSearchPage = () => (
-  <Container>
-    <Typography variant="h3" component="h1">
-      Github repositories list page
-    </Typography>
+	<Container>
+		<Typography variant='h3' component='h1'>
+			Github repositories list page
+		</Typography>
 
-    <Grid container spacing={2} justifyContent="space-between">
-      <Grid item md={6} xs={12}>
-        <TextField  fullWidth label="Filter by" id="filterBy" />
-      </Grid>
+		<Grid container spacing={2} justifyContent='space-between'>
+			<Grid item md={6} xs={12}>
+				<TextField fullWidth label='Filter by' id='filterBy' />
+			</Grid>
 
-      <Grid item md={3} xs={12}>
-        <Button fullWidth color="primary" variant="contained" >Search</Button>
-      </Grid>
-    </Grid>
-  </Container>
+			<Grid item md={3} xs={12}>
+				<Button fullWidth color='primary' variant='contained'>
+					Search
+				</Button>
+			</Grid>
+		</Grid>
+
+		<Box
+			display='flex'
+			alignItems='center'
+			justifyContent='center'
+			height={400}
+		>
+			<Typography>
+				Please provide a search option and click in the search button
+			</Typography>
+		</Box>
+	</Container>
 );
 
 export default GithubSearchPage;
