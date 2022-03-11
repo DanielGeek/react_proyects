ea5f65f2b25b86fce09995bce5d57926c48fc8ff
DangelGlobant <daniel30081990@gmail.com>
2022-03-11 17:21:55 -0300

    202. Workbox SW Manual

--- 34-calendar-appv2/react-calendar-frontend/workbox-config.js
+++ 34-calendar-appv2/react-calendar-frontend/workbox-config.js
@@ -4,8 +4,5 @@ module.exports = {
 		'**/*.{json,ico,html,png,txt,css,js}'
 	],
 	swDest: 'build/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+	swSrc: 'src/sw-template.js'
 };
