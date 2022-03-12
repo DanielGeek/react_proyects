f819b9f75c2c9510ef9cee53bbc7891b0e1539c9
DangelGlobant <daniel30081990@gmail.com>
2022-03-12 09:11:39 -0300

    58. Consume API endpoint and test more resilient to change

--- 32-testing-store-app/src/components/content/index.js
+++ 32-testing-store-app/src/components/content/index.js
@@ -1,60 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@material-ui/core/Box';
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Avatar from '@material-ui/core/Avatar'
-import Link from '@material-ui/core/Link'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Avatar from '@material-ui/core/Avatar';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
-import TablePagination from '@material-ui/core/TablePagination'
+import TablePagination from '@material-ui/core/TablePagination';
 
 const tableHeaders = [
-  'Repository',
-  'Stars',
-  'Forks',
-  'Open issues',
-  'Updated at',
-]
+	'Repository',
+	'Stars',
+	'Forks',
+	'Open issues',
+	'Updated at',
+];
 
-export const Content = ({ isSearchApplied }) =>
+export const Content = ({ isSearchApplied, reposList }) =>
 	isSearchApplied ? (
 		<>
 			<TableContainer>
 				<Table>
 					<TableHead>
 						<TableRow>
-							{tableHeaders.map(name => (
-								<TableCell key={name}>{ name }</TableCell>
+							{tableHeaders.map((name) => (
+								<TableCell key={name}>{name}</TableCell>
 							))}
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						<TableRow>
-							<TableCell>
-								<Avatar alt='test' src='/logo192.png' />
-								<Link href='http://localhost:3000/test'>Test</Link>
-							</TableCell>
-							<TableCell>10</TableCell>
-							<TableCell>5</TableCell>
-							<TableCell>2</TableCell>
-							<TableCell>2020-01-01</TableCell>
-						</TableRow>
+						{reposList.map(
+							({
+								name,
+								id,
+								stargazers_count: stargazersCount,
+								forks_count: forksCount,
+								open_issues_count: openIssuesCount,
+								updated_at: updatedAt,
+								html_url: htmlUrl,
+								owner: {avatar_url: avatarUrl}
+							}) => (
+								<TableRow key={id}>
+									<TableCell>
+										<Avatar alt={name} src={avatarUrl} />
+										<Link href={htmlUrl}>{name}</Link>
+									</TableCell>
+									<TableCell>{stargazersCount}</TableCell>
+									<TableCell>{forksCount}</TableCell>
+									<TableCell>{openIssuesCount}</TableCell>
+									<TableCell>{updatedAt}</TableCell>
+								</TableRow>
+							)
+						)}
 					</TableBody>
 				</Table>
 			</TableContainer>
 			<TablePagination
-        rowsPerPageOptions={[30, 50, 100]}
-        component="div"
-        count={1}
-        rowsPerPage={30}
-        page={0}
-        onPageChange={() => {}}
-        onRowsPerPageChange={() => {}}
-      />
+				rowsPerPageOptions={[30, 50, 100]}
+				component='div'
+				count={1}
+				rowsPerPage={30}
+				page={0}
+				onPageChange={() => {}}
+				onRowsPerPageChange={() => {}}
+			/>
 		</>
 	) : (
 		<Box
@@ -73,4 +86,5 @@ export default Content;
 
 Content.propTypes = {
 	isSearchApplied: PropTypes.bool.isRequired,
+	reposList: PropTypes.arrayOf(PropTypes.object.isRequired),
 };
