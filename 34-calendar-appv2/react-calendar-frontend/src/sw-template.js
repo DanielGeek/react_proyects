b01a7997db444b6773fc8a15ab4fc05d632daa1b
DangelGlobant <daniel30081990@gmail.com>
2022-03-16 00:19:20 -0300

    205. Background Sync - Posts offline

--- 34-calendar-appv2/react-calendar-frontend/src/sw-template.js
+++ 34-calendar-appv2/react-calendar-frontend/src/sw-template.js
@@ -2,10 +2,13 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
+workbox.loadModule('workbox-background-sync')
+
 workbox.precaching.precacheAndRoute( self.__WB_MANIFEST );
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst, } = workbox.strategies;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
 registerRoute(
   new RegExp('http://localhost:4000/api/auth/renew'),
@@ -25,4 +28,17 @@ registerRoute(
 registerRoute(
   new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
   new CacheFirst()
+)
+
+// Posteos Offline
+const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
+  maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugin: [bgSyncPlugin]
+  }),
+  'POST'
 )
