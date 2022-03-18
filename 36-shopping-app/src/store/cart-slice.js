b6101647e061db42aa684b66a73a17626ef3aa45
DangelGlobant <daniel30081990@gmail.com>
2022-03-18 00:34:10 -0300

    Incrementing and Decrementing Items From Cart

--- 36-shopping-app/src/store/cart-slice.js
+++ 36-shopping-app/src/store/cart-slice.js
@@ -17,7 +17,7 @@ const cartSlice = createSlice({
 
 			if (existingItem) {
 				existingItem.quantity++;
-				existingItem.price += newItem.price;
+				existingItem.totalPrice += newItem.price;
 			} else {
 				state.itemsList.push({
 					id: newItem.id,
@@ -26,11 +26,12 @@ const cartSlice = createSlice({
 					totalPrice: newItem.price,
 					name: newItem.name,
 				});
+        state.totalQuantity++;
 			}
 		},
 		removeFromCart() {},
 		setShowCart(state) {
-			state.showCart = true;
+			state.showCart = !state.showCart;
 		},
 	},
 });
