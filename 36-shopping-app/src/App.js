37fa119af56481cfa4c4c0d70c1582406210d9eb
DangelGlobant <daniel30081990@gmail.com>
2022-03-20 00:08:56 -0300

    Using Firebase with Redux

--- 36-shopping-app/src/App.js
+++ 36-shopping-app/src/App.js
@@ -1,20 +1,35 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import "./App.css";
-import Auth from "./components/Auth";
-import Layout from "./components/Layout";
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import './App.css';
+import Auth from './components/Auth';
+import Layout from './components/Layout';
+import Notification from './components/Notification';
 
 function App() {
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-  console.log(isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
-  return (
-    <div className="App">
-      { !isLoggedIn && <Auth /> }
-      { isLoggedIn && <Layout /> }
-    </div>
-  );
+	const cart = useSelector((state) => state.cart);
+	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+	useEffect(() => {
+		const sendRequest = async () => {
+			const res = await fetch(
+				'https://redux-http-1a18f-default-rtdb.firebaseio.com/cartItems.json',
+				{
+					method: 'PUT',
+					body: JSON.stringify(cart),
+				}
+			);
+			const data = await res.json();
+		};
+		sendRequest();
+	}, [cart]);
+
+	return (
+		<div className='App'>
+      <Notification type='success' message={'This is dummy message'} />
+			{!isLoggedIn && <Auth />}
+			{isLoggedIn && <Layout />}
+		</div>
+	);
 }
 
 export default App;
