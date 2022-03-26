f2921ff29d6e27b4b7663a072f2183a63109cc0f
DangelGlobant <daniel30081990@gmail.com>
2022-03-26 12:32:32 -0300

    215. Obtain the user's geolocation

--- 37-maps-app/src/index.tsx
+++ 37-maps-app/src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { MapsApp } from './MapsApp';
+
+if ( !navigator.geolocation ) {
+  alert('You browser not have geolocation option');
+  throw new Error('You browser not have geolocation option');
+}
 
 ReactDOM.render(
   <React.StrictMode>
