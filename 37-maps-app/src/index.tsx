ae6a65fb3d5d590b286d2129a267f0e2fdc020a1
DangelGlobant <daniel30081990@gmail.com>
2022-03-27 22:56:44 -0300

    217. Show Mapbox Map

--- 37-maps-app/src/index.tsx
+++ 37-maps-app/src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MapsApp } from './MapsApp';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsZ2VlayIsImEiOiJja291eHRlODQwMm5hMm5sOXM3YzlwajlxIn0.UORg5svptSrVMXj_FSENAg';
+
 if ( !navigator.geolocation ) {
   alert('You browser not have geolocation option');
   throw new Error('You browser not have geolocation option');
