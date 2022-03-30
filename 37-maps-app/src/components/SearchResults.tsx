da27e0998db9f6e0a2350fc0ae16260db50ce9ea
DangelGlobant <daniel30081990@gmail.com>
2022-03-30 17:29:12 -0300

    229. Test the API from React

--- 37-maps-app/src/components/SearchResults.tsx
+++ 37-maps-app/src/components/SearchResults.tsx
@@ -5,8 +5,8 @@ import { Feature } from "../interfaces/places";
 
 export const SearchResults = () => {
 
-  const { places, isLoadingPlaces } = useContext( PlacesContext );
-  const { map } = useContext( MapContext );
+  const { places, isLoadingPlaces, userLocation } = useContext( PlacesContext );
+  const { map, getRouteBetweenPoints } = useContext( MapContext );
 
   const [activeId, setActiveId] = useState('');
 
@@ -17,6 +17,13 @@ export const SearchResults = () => {
         zoom: 14,
         center: [ lng, lat ]
       })
+  }
+
+  const getRoute = ( place: Feature ) => {
+    if ( !userLocation ) return;
+    const [lng, lat] = place.center;
+
+    getRouteBetweenPoints(userLocation, [lng, lat]);
   }
 
   if( isLoadingPlaces ) {
@@ -45,7 +52,9 @@ export const SearchResults = () => {
               { place.place_name }
             </p>
 
-            <button className={`btn btn-sm ${ activeId === place.id ? 'btn-outline-light': 'btn-outline-primary' } `}>
+            <button
+                onClick={() => getRoute( place )}
+                className={`btn btn-sm ${ activeId === place.id ? 'btn-outline-light': 'btn-outline-primary' } `}>
                 Addresses
             </button>
           </li>
