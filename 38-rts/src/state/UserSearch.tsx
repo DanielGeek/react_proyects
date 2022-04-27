4b441198d2c99acd841e7912fa63fb93e421767f
DangelGlobant <daniel30081990@gmail.com>
2022-04-27 14:20:49 -0400

    14. Type Unions in State

--- 38-rts/src/state/UserSearch.tsx
+++ 38-rts/src/state/UserSearch.tsx
@@ -8,8 +8,14 @@ const users = [
 
 const UserSearch: React.FC = () => {
 	const [name, setName] = useState('');
+	const [user, setUser] = useState<{ name: string, age: number | undefined }>();
 
   const onClick = () => {
+		const foundUser = users.find((user) => {
+			return user.name === name;
+		});
+
+		setUser(foundUser);
 
   };
 
@@ -18,6 +24,10 @@ const UserSearch: React.FC = () => {
 			User Search
 			<input value={name} onChange={(e) => setName(e.target.value)} />
 			<button onClick={onClick}>Find User</button>
+			<div>
+				{user && user.name}
+				{user && user.age}
+			</div>
 		</div>
 	);
 };
