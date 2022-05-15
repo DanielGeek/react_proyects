fa580b0b5500e2c3029cf9bf87f3de603982934e
DangelGlobant <daniel30081990@gmail.com>
2022-05-15 00:12:36 -0400

    75. Integrate ErrorBoundary in the app

--- 32-testing-store-app/src/components/error-boundary/error-boundary.js
+++ 32-testing-store-app/src/components/error-boundary/error-boundary.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 export class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {hasError: false}
+    this.state = { hasError: false }
   }
 
   static getDerivedStateFromError() {
-    return {hasError: true}
+    return { hasError: true }
   }
 
   handleReloadClick = () => window.location.reload()
 
   render() {
-    const {children} = this.props
-    const {hasError} = this.state
+    const { children } = this.props
+    const { hasError } = this.state
 
     if (hasError) {
       return (
         <>
-          <p>There is an unexpected error</p>
-          <button type="button" onClick={this.handleReloadClick}>
+          <Typography variant="h4">There is an unexpected error</Typography>
+          <Button type="button" onClick={this.handleReloadClick} variant="contained" color="primary">
             Reload
-          </button>
+          </Button>
         </>
       )
     }
