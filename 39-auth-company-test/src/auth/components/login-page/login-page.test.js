fadef8b6675945186c92334ec1b05904cfeb9f69
DangelGlobant <daniel30081990@gmail.com>
2022-05-16 17:58:59 -0400

    86. Test for login form elements

--- 39-auth-company-test/src/auth/components/login-page/login-page.test.js
+++ 39-auth-company-test/src/auth/components/login-page/login-page.test.js
@@ -3,9 +3,18 @@ import {screen, render} from '@testing-library/react';
 
 import { LoginPage } from './login-page';
 
+// eslint-disable-next-line testing-library/no-render-in-setup
+beforeEach(() => render(<LoginPage />))
+
 describe('when login page is mounted', () => {
   it('must display the login title', () => {
-    render(<LoginPage />)
     expect(screen.getByText(/login page/i)).toBeInTheDocument()
+  })
+
+  it('must have a form with the following fields: email, password and a submit button.', () => {
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
+    // eslint-disable-next-line jest/valid-expect
+    expect(screen.getByRole('button', {name: /send/i}))
   })
 })
