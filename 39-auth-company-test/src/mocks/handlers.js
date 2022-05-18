ee1166c705f9107f9edb63655843fe8a4f66d402
DangelGlobant <daniel30081990@gmail.com>
2022-05-18 15:07:50 -0400

    104. Test refactor for invalid credentials

--- 39-auth-company-test/src/mocks/handlers.js
+++ 39-auth-company-test/src/mocks/handlers.js
@@ -1,5 +1,10 @@
 import { rest } from 'msw'
 
+import {
+  HTTP_INVALID_CREDENTIALS_STATUS,
+  HTTP_OK_STATUS,
+} from '../consts';
+
 export const handlers = [
   rest.post('/login', (req, res, ctx) => {
     sessionStorage.setItem('is-authenticated', true)
@@ -7,4 +12,18 @@ export const handlers = [
   }),
 ]
 
-export default {handlers}
+export const handlerInvalidCredentials = ({ wrongEmail, wrongPassword }) =>
+rest.post('/login', (req, res, ctx) => {
+  const { email, password } = req.body
+
+  if (email === wrongEmail && password === wrongPassword) {
+    return res(
+      ctx.status(HTTP_INVALID_CREDENTIALS_STATUS),
+      ctx.json({ message: 'The email or password are not correct' }),
+    )
+  }
+
+  return res(ctx.status(HTTP_OK_STATUS))
+})
+
+export default {handlers, handlerInvalidCredentials}
