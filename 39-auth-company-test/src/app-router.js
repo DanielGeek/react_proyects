fb40df523013b6bab399a99108519962528f2511
DangelGlobant <daniel30081990@gmail.com>
2022-05-20 13:15:20 -0400

    117. Update login page tests to use auth context provider

--- 39-auth-company-test/src/app-router.js
+++ 39-auth-company-test/src/app-router.js
@@ -1,29 +1,37 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import { LoginPage } from './auth/components/login-page';
 import { PrivateRoute } from './utils/components/private-route';
+import { AuthContext } from './utils/contexts/auth-context';
 import { AdminPage } from './admin/components/admin-page';
 import { EmployeePage } from './employee/components/employee-page';
 
-export const AppRouter = ({isAuth}) => {
+export const AppRouter = ({ isAuth }) => {
   const [isUserAuth, setIsUserAuth] = useState(isAuth)
 
   const handleSuccessLogin = () => setIsUserAuth(true)
 
+  const authProviderValue = {
+    isAuth: isUserAuth,
+    handleSuccessLogin
+  }
+
   return (
-    <Switch>
-      <Route path="/" exact>
-        <LoginPage onSuccessLogin={handleSuccessLogin} />
-      </Route>
-      <PrivateRoute path="/admin" isAuth={isUserAuth}>
-        <AdminPage />
-      </PrivateRoute>
-      <PrivateRoute path="/employee" isAuth={isUserAuth}>
-        <EmployeePage />
-      </PrivateRoute>
-    </Switch>
+    <AuthContext.Provider value={authProviderValue}>
+      <Switch>
+        <Route path="/" exact>
+          <LoginPage onSuccessLogin={handleSuccessLogin} />
+        </Route>
+        <PrivateRoute path="/admin" isAuth={isUserAuth}>
+          <AdminPage />
+        </PrivateRoute>
+        <PrivateRoute path="/employee" isAuth={isUserAuth}>
+          <EmployeePage />
+        </PrivateRoute>
+      </Switch>
+    </AuthContext.Provider>
   )
 }
 
