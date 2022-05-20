d019ce196240b70c2039786b970058082b97f596
DangelGlobant <daniel30081990@gmail.com>
2022-05-20 13:43:17 -0400

    119. Decouple authentication logic by creating AuthGuard component

--- 39-auth-company-test/src/utils/components/private-route.js
+++ 39-auth-company-test/src/utils/components/private-route.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import {Route, Redirect} from 'react-router-dom';
 
-export const PrivateRoute = ({children, path, isAuth}) => (
-  <Route path={path} exact>
-    {isAuth ? children : <Redirect to="/" />}
-  </Route>
-)
+import { AuthContext } from '../contexts/auth-context';
+
+export const PrivateRoute = ({children, path}) => {
+  const {isAuth} = useContext(AuthContext)
+
+  return (
+    <Route path={path} exact>
+      {isAuth ? children : <Redirect to="/" />}
+    </Route>
+  )
+}
 
 PrivateRoute.propTypes = {
   children: PropTypes.node.isRequired,
