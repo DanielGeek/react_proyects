ee4afc51fd6008e52006d970968f77df57efa877
DangelGlobant <daniel30081990@gmail.com>
2022-05-21 17:23:38 -0400

    131. Employee Page Refactor

--- 39-auth-company-test/src/employee/components/employee-page/employee-page.test.js
+++ 39-auth-company-test/src/employee/components/employee-page/employee-page.test.js
@@ -3,14 +3,19 @@ import { render, screen } from '@testing-library/react';
 
 import { EmployeePage } from './employee-page'
 import { AuthContext } from '../../../utils/contexts/auth-context'
+import { ADMIN_ROLE, EMPLOYEE_ROLE } from '../../../consts';
+
+const renderWith = ({role, username = 'John Doe'}) => (
+  render(
+    <AuthContext.Provider value={{ user: { username, role } }}>
+      <EmployeePage />
+    </AuthContext.Provider>,
+  )
+)
 
 describe('When the admin access to employee page', () => {
   it('must have access to delete the employee button', () => {
-    render(
-      <AuthContext.Provider value={{ user: { username: 'John Doe', role: 'admin' } }}>
-        <EmployeePage />
-      </AuthContext.Provider>,
-    )
+    renderWith({role: ADMIN_ROLE})
 
     expect(screen.getByRole('button', { name: /delete/i })).toBeInTheDocument()
   })
@@ -19,11 +24,7 @@ describe('When the admin access to employee page', () => {
 
 describe('When the employee access to employee page', () => {
   it('must not have access to delete the employee button', () => {
-    render(
-      <AuthContext.Provider value={{ user: { username: 'John Doe', role: 'employee' } }}>
-        <EmployeePage />
-      </AuthContext.Provider>,
-    )
+    renderWith({role: EMPLOYEE_ROLE})
 
     expect(screen.queryByRole('button', { name: /delete/i })).not.toBeInTheDocument()
   })
