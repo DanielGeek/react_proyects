5eb47af59689125d50b3953a12b6cac1316bd589
DangelGlobant <daniel30081990@gmail.com>
2022-05-25 13:03:24 -0400

    32. Adding Request Logic

--- 40-redux-ts/src/state/action-creators/index.ts
+++ 40-redux-ts/src/state/action-creators/index.ts
@@ -9,12 +9,25 @@ const searchRepositories = (term: string) => {
     });
 
     try {
-      
-    } catch (err) {
+      const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
+        params: {
+          text: term
+        }
+      });
+
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCESS,
+        payload: names
+      });
+    } catch (err: any) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
-        payload: err.message
-      })
+        payload: err.message,
+      });
     }
   };
 };
