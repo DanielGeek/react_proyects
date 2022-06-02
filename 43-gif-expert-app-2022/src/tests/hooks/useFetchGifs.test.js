827e1c67c0ec5ae72f6b9a10c3efeb731dcec213
DangelGlobant <daniel30081990@gmail.com>
2022-06-02 17:42:41 -0400

    101. Testing on customHooks

---
+++ 43-gif-expert-app-2022/src/tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,15 @@
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+import { renderHook } from "@testing-library/react";
+
+describe('Pruebas en el hook useFetchGifs', () => {
+
+  test('debe de retornar el estado inicial', () => {
+
+    const { result } = renderHook( () => useFetchGifs( 'One Punch' ));
+    const { data, loading } = result.current;
+
+    expect(data).toEqual([]);
+    expect(loading).toEqual(true);
+
+  })
+})
