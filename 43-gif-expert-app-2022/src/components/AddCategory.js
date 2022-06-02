63e0061eb506c75f4649d986962253d06b4258c1
DangelGlobant <daniel30081990@gmail.com>
2022-06-02 11:29:30 -0400

    95. * Simulate changes in an input

--- 43-gif-expert-app-2022/src/components/AddCategory.js
+++ 43-gif-expert-app-2022/src/components/AddCategory.js
@@ -7,6 +7,8 @@ export const AddCategory = ({ setCategories }) => {
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
+
+    console.log('llamado');
   }
 
   const handleSubmit = (e) => {
@@ -22,6 +24,7 @@ export const AddCategory = ({ setCategories }) => {
   return (
     <>
       <form onSubmit={handleSubmit}>
+      <p>{inputValue}</p>
         <input
           type="text"
           value={inputValue}
