23c96105316ef0a589bcfb0d64c6286e8d361cbe
DangelGlobant <daniel30081990@gmail.com>
2022-06-02 21:09:53 -0400

    09. useEffect - SimpleForm

--- 44-hook-app-2022/src/index.js
+++ 44-hook-app-2022/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CounterWithCustomHook />
+    <SimpleForm />
   </React.StrictMode>
 );
 
