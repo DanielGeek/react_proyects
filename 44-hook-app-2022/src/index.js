dfe63673a997978a6f1277cb4241e5f29eeed601
DangelGlobant <daniel30081990@gmail.com>
2022-06-02 20:21:26 -0400

    107. useState

--- 44-hook-app-2022/src/index.js
+++ 44-hook-app-2022/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HookApp } from './HookApp';
+
+// import { HookApp } from './HookApp';
+import { CounterApp } from './components/01-useState/CounterApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HookApp />
+    <CounterApp />
   </React.StrictMode>
 );
 
