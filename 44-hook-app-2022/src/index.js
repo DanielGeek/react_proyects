31d0829c35dfbd46802bc69f717b05729e7987a1
DangelGlobant <daniel30081990@gmail.com>
2022-06-02 20:01:48 -0400

    106. Project Start - HooksApp

---
+++ 44-hook-app-2022/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HookApp } from './HookApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HookApp />
+  </React.StrictMode>
+);
+
