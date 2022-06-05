db4beab957eb663dba537a6c316c5594aa8e319e
daniel-barreto <daniel30081990@gmail.com>
2022-06-05 13:58:14 -0400

    143. useContext

--- 44-hook-app-2022/src/components/07-useContext/AppRouter.js
+++ 44-hook-app-2022/src/components/07-useContext/AppRouter.js
@@ -19,15 +19,17 @@ export const AppRouter = () => {
 
         <NavBar />
 
-        <Switch>
-          <Route exact path="/" component={HomeScreen} />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
 
-          <Route exact path="/about" component={AboutScreen} />
-          <Route exact path="/login" component={LoginScreen} />
+            <Route exact path="/about" component={AboutScreen} />
+            <Route exact path="/login" component={LoginScreen} />
 
-          <Redirect to="/" />
+            <Redirect to="/" />
 
-        </Switch>
+          </Switch>
+        </div>
       </div>
     </Router>
   )
