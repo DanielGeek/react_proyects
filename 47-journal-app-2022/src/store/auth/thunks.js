3019e06fc991c3e8cdab09c8bf360999ce7c585f
daniel-barreto <daniel30081990@gmail.com>
2022-07-27 22:06:14 -0400

    274. Google SignIn - Firebase

--- 47-journal-app-2022/src/store/auth/thunks.js
+++ 47-journal-app-2022/src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { singInWithGoogle } from '../../firebase/providers';
 import { checkingCredentials } from './';
 
 export const checkingAuthentication = ( email, password ) => {
@@ -8,6 +9,10 @@ export const checkingAuthentication = ( email, password ) => {
 
 export const startGoogleSignIn = () => {
   return async( dispatch ) => {
+
     dispatch( checkingCredentials() );
+
+    const result = await singInWithGoogle();
+    console.log({ result });
   }
 }
