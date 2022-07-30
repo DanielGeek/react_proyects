862cf3c841360a3b3a3c251ed24b320e0d01ae2e
daniel-barreto <daniel30081990@gmail.com>
2022-07-30 02:09:01 -0400

    284. Task resolution - User login

--- 47-journal-app-2022/src/store/auth/thunks.js
+++ 47-journal-app-2022/src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { singInWithGoogle, registerUserWithEmailPassword } from '../../firebase/providers';
+import { singInWithGoogle, registerUserWithEmailPassword, loginWithEmailPassword } from '../../firebase/providers';
 import { checkingCredentials, logout, login } from './';
 
 export const checkingAuthentication = ( email, password ) => {
@@ -34,6 +34,15 @@ export const startCreatingUserWithEmailPassword = ({ email, password, displayNam
 
 }
 
-export const startLoginWithEmailPassword = () => {
-  
+export const startLoginWithEmailPassword = ({ email, password }) => {
+    return async( dispatch ) => {
+
+      dispatch( checkingCredentials() );
+
+      const result = await loginWithEmailPassword({ email, password });
+      console.log(result);
+
+      if ( !result.ok ) return dispatch( logout( result ));
+      dispatch( login( result ));
+    }
 }
