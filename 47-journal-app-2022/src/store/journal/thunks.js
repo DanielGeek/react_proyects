e6d8c8422d4a1c298467210885fef7776aff4794
daniel-barreto <daniel30081990@gmail.com>
2022-07-30 23:37:49 -0400

    296. Create a new note

--- 47-journal-app-2022/src/store/journal/thunks.js
+++ 47-journal-app-2022/src/store/journal/thunks.js
@@ -1,7 +1,10 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../firebase/config";
 
 export const startNewNote = () => {
-  return async( dispatch ) => {
+  return async( dispatch, getState ) => {
 
+    const { uid } = getState().auth;
     // uid
 
     const newNote = {
@@ -9,6 +12,9 @@ export const startNewNote = () => {
       body: '',
       date: new Date().getTime(),
     }
+
+    const newDoc = doc( collection( FirebaseDB, `${ uid }/journal/notes` ))
+    await setDoc( newDoc, newNote );
 
     // dispatch
     // dispatch( newNote )
