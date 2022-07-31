ef51a510b35a87d7328d4d0d6a122e4f12b83f47
daniel-barreto <daniel30081990@gmail.com>
2022-07-31 00:48:39 -0400

    298. Load Firestore Notes

--- 47-journal-app-2022/src/store/journal/thunks.js
+++ 47-journal-app-2022/src/store/journal/thunks.js
@@ -1,6 +1,7 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, setActiveNote, savingNewNote } from "./journalSlice";
+import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes } from "./journalSlice";
+import { loadNotes } from "../../helpers";
 
 export const startNewNote = () => {
   return async( dispatch, getState ) => {
@@ -24,5 +25,17 @@ export const startNewNote = () => {
     // dispatch
     dispatch( addNewEmptyNote( newNote ) );
     dispatch( setActiveNote( newNote ) );
+  }
+}
+
+export const startLoadingNotes = () => {
+  return async( dispatch, getState ) => {
+
+    const { uid } = getState().auth;
+    if ( !uid ) throw new Error('El UID del usuario no existe');
+
+    const notes = await loadNotes( uid );
+    dispatch( setNotes( notes ) );
+
   }
 }
