7e76fe3d244716acae8390ee52356f34454fedc0
daniel-barreto <daniel30081990@gmail.com>
2022-08-01 22:20:21 -0400

    304. SweetAlert 2

--- 47-journal-app-2022/src/journal/views/NoteView.jsx
+++ 47-journal-app-2022/src/journal/views/NoteView.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { SaveOutlined } from '@mui/icons-material';
 import { Button, Grid, TextField, Typography } from '@mui/material';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
 
 import { useForm } from '../../hooks/useForm';
 import { ImageGallery } from '../components'
@@ -12,7 +14,7 @@ import { setActiveNote, startSaveNote } from '../../store/journal';
 export const NoteView = () => {
 
     const dispatch = useDispatch();
-    const { active: note } = useSelector(state => state.journal);
+    const { active: note, messageSaved, isSaving } = useSelector(state => state.journal);
 
     const { body, title, date, onInputChange, formState } = useForm(note);
 
@@ -25,6 +27,12 @@ export const NoteView = () => {
         dispatch( setActiveNote(formState) );
 
     }, [formState])
+
+    useEffect(() => {
+        if ( messageSaved.length > 0) {
+            Swal.fire('Nota actualizada', messageSaved, 'success');
+        }
+    }, [messageSaved])
 
     const onSaveNote = () => {
         dispatch( startSaveNote() );
@@ -43,6 +51,7 @@ export const NoteView = () => {
             </Grid>
             <Grid item>
                 <Button
+                    disabled={ isSaving }
                     onClick={ onSaveNote }
                     color="primary"
                     sx={{ padding: 2 }}
--- 47-journal-app-2022/src/store/journal/journalSlice.js
+++ 47-journal-app-2022/src/store/journal/journalSlice.js
@@ -18,13 +18,14 @@ export const journalSlice = createSlice({
     },
     setActiveNote: ( state, action ) => {
       state.active = action.payload;
+      state.messageSaved = '';
     },
     setNotes: ( state, action ) => {
       state.notes = action.payload;
     },
     setSaving: ( state ) => {
       state.isSaving = true;
-      // TODO: mensaje de error...
+      state.messageSaved = '';
     },
     updateNote: ( state, action ) => {
       state.isSaving = false;
@@ -36,7 +37,7 @@ export const journalSlice = createSlice({
         return note;
       });
 
-      // TODO: mostrar mensaje de update
+      state.messageSaved = `${ action.payload.title }, actualizada correctamente`;
     },
     deleteNoteById: ( state, action ) => {
 
