abccb989944e86540c3fd13b3ac63e1546df0573
daniel-barreto <daniel30081990@gmail.com>
2022-08-03 19:25:42 -0400

    312. Delete a note

--- 47-journal-app-2022/src/store/journal/thunks.js
+++ 47-journal-app-2022/src/store/journal/thunks.js
@@ -1,6 +1,6 @@
-import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { collection, deleteDoc, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes, setSaving, updateNote, setPhotosToActiveNote } from "./journalSlice";
+import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes, setSaving, updateNote, setPhotosToActiveNote, deleteNoteById } from "./journalSlice";
 import { fileUpload, loadNotes } from "../../helpers";
 
 export const startNewNote = () => {
@@ -74,4 +74,17 @@ export const startUploadingFiles = ( files = [] ) => {
       dispatch( setPhotosToActiveNote( photosUrls ) );
 
     }
+}
+
+export const startDeletingNote = () => {
+  return async( dispatch, getState ) => {
+
+    const { uid } = getState().auth;
+    const { active: note } = getState().journal;
+
+    const docRef = doc( FirebaseDB, `${ uid }/journal/notes/${ note.id }`);
+    await deleteDoc( docRef );
+
+    dispatch( deleteNoteById(note.id) );
+  }
 }
