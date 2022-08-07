2e93ce10408fd750dbc5f9ba531b146aebc176e3
daniel-barreto <daniel30081990@gmail.com>
2022-08-06 20:21:50 -0400

    332. Testing on the LoginPage

---
+++ 47-journal-app-2022/tests/auth/pages/LoginPage.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import { LoginPage } from "../../../src/auth/pages/LoginPage";
+import { authSlice } from "../../../src/store/auth";
+import { MemoryRouter } from "react-router-dom";
+
+const store = configureStore({
+  reducer: {
+    auth: authSlice.reducer
+  },
+  // preloadedState: {
+
+  // }
+})
+
+describe('Prueba en <LoginPage />', () => {
+
+  test('debe de mostrar el componente correctamente', () => {
+
+    render(
+      <Provider store={ store }>
+        <MemoryRouter>
+          <LoginPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect( screen.getAllByText('Login').length ).toBeGreaterThanOrEqual(1);
+
+  });
+ });
