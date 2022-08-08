270057ee7541b2a384ff6e6092c3377b8873589b
daniel-barreto <daniel30081990@gmail.com>
2022-08-08 17:12:44 -0400

    355. CalendarSlice

--- 48-calendar-2022/src/calendar/pages/CalendarPage.jsx
+++ 48-calendar-2022/src/calendar/pages/CalendarPage.jsx
@@ -6,25 +6,12 @@ import { addHours } from 'date-fns'
 import { Navbar, CalendarEvent, CalendarModal } from "../"
 
 import { localizer, getMessagesEs } from '../../helpers';
-import { useUiStore } from '../../hooks';
-
-const events = [{
-  title: 'Cumpleaños del jefe',
-  notes: 'Hay que comprar el pastel',
-  start: new Date(),
-  end: addHours( new Date(), 2),
-  bgColor: '#fafafa',
-  user: {
-    _id: '123',
-    name: 'Daniel'
-  }
-}]
-
+import { useUiStore, useCalendarStore } from '../../hooks';
 
 export const CalendarPage = () => {
 
   const { openDateModal } = useUiStore();
-
+  const { events } = useCalendarStore();
   const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
 
   const eventStyleGetter = ( event, start, end, isSelected ) => {
