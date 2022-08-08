def2a1a059227c0e3edd2daa33ccfe9db123677c
daniel-barreto <daniel30081990@gmail.com>
2022-08-08 12:14:50 -0400

    347. Listen to calendar events

--- 48-calendar-2022/src/calendar/pages/CalendarPage.jsx
+++ 48-calendar-2022/src/calendar/pages/CalendarPage.jsx
@@ -5,6 +5,7 @@ import { addHours } from 'date-fns'
 import { Navbar, CalendarEvent } from "../"
 
 import { localizer, getMessagesEs } from '../../helpers';
+import { useState } from 'react';
 
 const events = [{
   title: 'Cumpleaños del jefe',
@@ -21,6 +22,8 @@ const events = [{
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
   const eventStyleGetter = ( event, start, end, isSelected ) => {
 
     const styles = {
@@ -35,6 +38,19 @@ export const CalendarPage = () => {
     }
   }
 
+  const onDoubleClick = ( event ) => {
+    console.log({ doubleClick: event })
+  }
+
+  const onSelect = ( event ) => {
+    console.log({ click: event })
+  }
+
+  const onViewChange = ( event ) => {
+    localStorage.setItem('lastView', event );
+    setLastView( event );
+  }
+
   return (
     <>
       <Navbar />
@@ -43,6 +59,7 @@ export const CalendarPage = () => {
         culture='es'
         localizer={localizer}
         events={events}
+        defaultView={ lastView }
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
@@ -51,6 +68,9 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent
         }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChange }
       />
     </>
   )
