9ac41e57bb8e03270070679b2586f7ceefabf65c
daniel-barreto <daniel30081990@gmail.com>
2022-08-08 20:22:00 -0400

    359. Edit the active event

--- 48-calendar-2022/src/hooks/useCalendarStore.js
+++ 48-calendar-2022/src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-import { onAddNewEvent, onSetActiveEvent } from "../store";
+import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from "../store";
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
@@ -13,7 +13,7 @@ export const useCalendarStore = () => {
     // TODO: llegar al backend
 
     if( calendarEvent._id) {
-      // Actualizando
+      dispatch( onUpdateEvent({ ...calendarEvent }) );
     } else {
       dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
     }
