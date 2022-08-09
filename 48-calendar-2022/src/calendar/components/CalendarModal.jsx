25513a2cf4ed34158af5944923fa819ea66bf769
daniel-barreto <daniel30081990@gmail.com>
2022-08-08 20:12:55 -0400

    358. Add a new event

--- 48-calendar-2022/src/calendar/components/CalendarModal.jsx
+++ 48-calendar-2022/src/calendar/components/CalendarModal.jsx
@@ -27,7 +27,7 @@ Modal.setAppElement('#root');
 export const CalendarModal = () => {
 
   const { isDateModalOpen, closeDateModal } = useUiStore();
-  const { activeEvent } = useCalendarStore();
+  const { activeEvent, startSavingEvent } = useCalendarStore();
 
   const [formSubmitted, setFormSubmitted] = useState(false);
 
@@ -71,7 +71,7 @@ export const CalendarModal = () => {
     closeDateModal();
   }
 
-  const onSubmit = ( event ) => {
+  const onSubmit = async( event ) => {
     event.preventDefault();
     setFormSubmitted(true);
 
@@ -86,9 +86,9 @@ export const CalendarModal = () => {
 
     console.log(formValues);
 
-    // TODO:
-    // Remover errores en pantalla
-    // cerrar modal
+    await startSavingEvent( formValues );
+    closeDateModal();
+    setFormSubmitted(false);
   }
 
   return (
--- 48-calendar-2022/src/hooks/useCalendarStore.js
+++ 48-calendar-2022/src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-import { onSetActiveEvent } from "../store";
+import { onAddNewEvent, onSetActiveEvent } from "../store";
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
@@ -9,9 +9,20 @@ export const useCalendarStore = () => {
     dispatch( onSetActiveEvent( calendarEvent ) );
   }
 
+  const startSavingEvent = async( calendarEvent ) => {
+    // TODO: llegar al backend
+
+    if( calendarEvent._id) {
+      // Actualizando
+    } else {
+      dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
+    }
+  }
+
   return {
     activeEvent,
     events,
     setActiveEvent,
+    startSavingEvent,
   }
 }
