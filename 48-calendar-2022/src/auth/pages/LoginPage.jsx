1813900474ed2da386511b0c637b998e70ceefb5
daniel-barreto <daniel30081990@gmail.com>
2022-08-11 10:36:05 -0400

    409. Creation of a new user

--- 48-calendar-2022/src/auth/pages/LoginPage.jsx
+++ 48-calendar-2022/src/auth/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ const registerFormFields = {
 
 export const LoginPage = () => {
 
-    const { startLogin, errorMessage } = useAuthStore();
+    const { startLogin, errorMessage, startRegister } = useAuthStore();
 
     const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm( loginFormFields );
     const { registerEmail, registerName , registerPassword, registerPassword2, onInputChange: onRegisterInputChange } = useForm( registerFormFields );
@@ -33,7 +33,11 @@ export const LoginPage = () => {
 
     const registerSubmit = ( event ) => {
         event.preventDefault();
-        console.log({ registerName, registerEmail, registerPassword, registerPassword2 })
+        if ( registerPassword !== registerPassword2 ) {
+            Swal.fire('Error en registro', 'Contraseñas no son iguales', 'error');
+            return;
+        }
+        startRegister({ name: registerName, email: registerEmail, password: registerPassword })
     }
 
     return (
