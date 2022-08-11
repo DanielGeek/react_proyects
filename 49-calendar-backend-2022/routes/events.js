2106359f7ec547d56b2555b2997a6d719efd3525
daniel-barreto <daniel30081990@gmail.com>
2022-08-11 04:23:56 -0400

    389. Validate required fields

--- 49-calendar-backend-2022/routes/events.js
+++ 49-calendar-backend-2022/routes/events.js
@@ -2,6 +2,10 @@
 // host + /api/events
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
 
@@ -11,7 +15,15 @@ router.use( validarJWT );
 
 router.get('/', getEventos);
 
-router.post('/', crearEvento);
+router.post(
+  '/',
+  [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+    check('start', 'Fecha de finalización es obligatoria').custom( isDate ),
+    validarCampos
+  ],
+  crearEvento);
 
 router.put('/:id', actualizarEvento);
 
