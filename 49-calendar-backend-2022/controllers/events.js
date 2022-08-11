ba6b5d4993ff7985b89455f8299f09a30d8d7da9
daniel-barreto <daniel30081990@gmail.com>
2022-08-11 07:07:32 -0400

    393. Delete Events

--- 49-calendar-backend-2022/controllers/events.js
+++ 49-calendar-backend-2022/controllers/events.js
@@ -46,7 +46,7 @@ const actualizarEvento = async(req, res = response) => {
     const evento = await Evento.findById( eventoId );
 
     if ( !evento ) {
-      res.status(400).json({
+      return res.status(400).json({
         ok: false,
         msg: 'Evento no existe por ese id'
       });
@@ -80,12 +80,42 @@ const actualizarEvento = async(req, res = response) => {
   }
 }
 
-const eliminarEvento = (req, res = response) => {
+const eliminarEvento = async(req, res = response) => {
 
-  res.json({
-    ok: true,
-    msg: 'eliminarEvento'
-  });
+  const eventoId = req.params.id;
+  const uid = req.uid;
+
+  try {
+
+    const evento = await Evento.findById( eventoId );
+
+    if ( !evento ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Evento no existe por ese id'
+      });
+    }
+
+    if ( evento.user.toString() !== uid ) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene privilegio de eliminar este evento'
+      });
+    }
+
+    await Evento.findByIdAndDelete( eventoId );
+
+    res.json({
+      ok: true
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    });
+  }
 }
 
 module.exports = {
