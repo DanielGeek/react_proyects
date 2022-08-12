95b9d6e0511fb9d1985c00ca1eb11ac43d90a2c6
daniel-barreto <daniel30081990@gmail.com>
2022-08-12 17:54:51 -0400

    433. Testing in uiSlice

---
+++ 48-calendar-2022/tests/store/ui/uiSlice.test.js
@@ -0,0 +1,20 @@
+import { onCloaseDateModal, onOpenDateModal, uiSlice } from "../../../src/store";
+
+describe('Pruebas en uiSlice', () => {
+
+  test('debe de regresar el estado por defecto', () => {
+
+    expect(uiSlice.getInitialState()).toEqual({ isDateModalOpen: false });
+
+  });
+
+  test('debe de cambiar el isDateModalOpen correctamente', () => {
+
+    let state = uiSlice.getInitialState();
+    state = uiSlice.reducer( state, onOpenDateModal() )
+    expect( state.isDateModalOpen ).toBeTruthy();
+
+    state = uiSlice.reducer( state, onCloaseDateModal() )
+    expect( state.isDateModalOpen ).toBeFalsy();
+  });
+});
