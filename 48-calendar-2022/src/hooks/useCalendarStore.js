06ab569a19f3a05afec756de94960cd89770ead8
daniel-barreto <daniel30081990@gmail.com>
2022-08-12 11:44:16 -0400

    419. Upload events to store

--- 48-calendar-2022/src/hooks/useCalendarStore.js
+++ 48-calendar-2022/src/hooks/useCalendarStore.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux"
 import { calendarApi } from "../api";
 import { convertEventsToDateEvents } from "../helpers";
-import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from "../store";
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
@@ -34,7 +34,7 @@ export const useCalendarStore = () => {
 
       const { data } = await calendarApi.get('/events');
       const events = convertEventsToDateEvents( data.eventos );
-      console.log(events);
+      dispatch( onLoadEvents( events ) );
 
     } catch (error) {
       console.log('Error cargando eventos');
