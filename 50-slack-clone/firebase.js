746c2ef5b1f65288a20be922ed364d026263aff6
daniel-barreto <daniel30081990@gmail.com>
2022-08-12 22:00:20 -0400

    setup firebase, react touter and styled components

---
+++ 50-slack-clone/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7vaailqFJ1ufW7X6hXH2Z5I06yy_VCCI",
+  authDomain: "react-cursos-cda62.firebaseapp.com",
+  projectId: "react-cursos-cda62",
+  storageBucket: "react-cursos-cda62.appspot.com",
+  messagingSenderId: "995663104854",
+  appId: "1:995663104854:web:9161952cb6f946279f64f7"
+};
+
+// Initialize Firebase
---
+++ 50-slack-clone/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2wWugxo-8TW2yzzlinfgLbLC9BAYs7cw",
+  authDomain: "slack-clone-5bb94.firebaseapp.com",
+  projectId: "slack-clone-5bb94",
+  storageBucket: "slack-clone-5bb94.appspot.com",
+  messagingSenderId: "480786996595",
+  appId: "1:480786996595:web:4dffb4ca3f358e13a83906"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
