38b98688cb4de0c10d83ae5249ad29e1b0cf0455
daniel-barreto <daniel30081990@gmail.com>
2022-08-12 23:37:53 -0400

    Building the Sidebar Section

--- 50-slack-clone/src/components/Header.js
+++ 50-slack-clone/src/components/Header.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Avatar } from '@material-ui/core';
 import AccessTimeIcon from '@material-ui/icons/AccessTime'
 import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
   return (
@@ -16,9 +17,13 @@ function Header() {
       {/* Header Search */}
       <HeaderSearch>
         <SearchIcon />
+        <input placeholder="Search papafam" />
       </HeaderSearch>
 
       {/* Header Right */}
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   )
 }
@@ -26,6 +31,24 @@ function Header() {
 export default Header;
 
 const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
 `;
 
 const HeaderContainer = styled.div`
@@ -48,6 +71,17 @@ const HeaderLeft = styled.div`
   > .MuiSvgIcon-root {
     margin-left: auto;
     margin-right: 30px;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
   }
 `;
 
