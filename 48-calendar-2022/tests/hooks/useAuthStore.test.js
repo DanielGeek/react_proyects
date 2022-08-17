6990e82bc0774603dc05e901ffbd65fc9fc1288e
daniel-barreto <daniel30081990@gmail.com>
2022-08-16 22:53:42 -0400

    444. startLogin must login correctly

--- 48-calendar-2022/tests/hooks/useAuthStore.test.js
+++ 48-calendar-2022/tests/hooks/useAuthStore.test.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { useAuthStore } from "../../src/hooks/useAuthStore";
 import { authSlice } from "../../src/store";
-import { initialState } from "../fixtures/authState";
+import { initialState, notAuthenticatedState } from "../fixtures/authState";
+import { testUserCredentials } from "../fixtures/testUser";
 
 const getMockStore = ( initialState ) => {
   return configureStore({
@@ -35,6 +36,30 @@ describe('Pruebas en useAuthStore', () => {
       startLogout: expect.any(Function),
       startRegister: expect.any(Function)
     })
+  });
+
+  test('startLogin debe de realizar el login correctamente', async() => {
+    localStorage.clear();
+    const mockStore = getMockStore({ ...notAuthenticatedState });
+
+    const { result } = renderHook( () => useAuthStore(), {
+      wrapper: ({ children }) => <Provider store={ mockStore }>{children}</Provider>
+    });
+
+    await act(async() => {
+      await result.current.startLogin( testUserCredentials )
+    });
+
+     const { errorMessage, status, user } = result.current;
+     expect({ errorMessage, status, user }).toEqual({
+      errorMessage: undefined,
+      status: 'authenticated',
+      user: { name: 'Daniel Angel Barreto', uid: '5f93ba08da830726b0b70a7e' }
+     });
+
+     expect(localStorage.getItem('token')).toEqual(expect.any(String));
+     expect(localStorage.getItem('token-init-date')).toEqual(expect.any(String));
+
   });
 
 });
