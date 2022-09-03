88ea434147f9401fc98c30dbe6000df6f7411c1d
daniel-barreto <daniel30081990@gmail.com>
2022-09-03 19:45:05 -0400

    config firebase

--- 51-tik-tok-clone/src/App.js
+++ 51-tik-tok-clone/src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import Video from './Video';
+import db from './firebase';
+import './App.css';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    db.collection("videos").onSnapshot((snapshot) =>
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    );
+    console.log({videos})
+  }, []);
+
   return (
     <div className="app">
       <div className='app__videos'>
-        <Video
-          url="https://v16-webapp.tiktok.com/8d1c5e4e73a574c478e782b1abe4f06c/6312dd65/video/tos/useast2a/tos-useast2a-pve-0068/a3c5a030477741af973b873a76dd32b1/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=4182&bt=2091&cs=0&ds=3&ft=gKSYZ88Uo0PD1aRnYyg9w.U2O5LiaQ2D~18&mime_type=video_mp4&qs=0&rc=aDRnZjk8aTZnOzs2OztmaEBpM2tqOGY6ZmlvZTMzNzczM0A0LmMuNDEwNi4xNmIwMy9iYSMwZnA2cjRvLm5gLS1kMTZzcw%3D%3D&l=20220902225035010189056034025A5754&btag=80000"
-          channel="daniel.angel.b"
-          description="WOW this works..."
-          song="The dani daniel"
-          likes={123}
-          messages={400}
-          shares={200}
-        />
-        <Video />
-        <Video />
-        <Video />
+        {videos.map(
+          ({ url, channel, description, song, likes, messages, shares }) => (
+            <Video
+              key={url}
+              url={url}
+              channel={channel}
+              description={description}
+              song={song}
+              likes={likes}
+              messages={messages}
+              shares={shares}
+            />
+          )
+        )}
       </div>
     </div>
   );
--- 51-tik-tok-clone/src/firebase.js
+++ 51-tik-tok-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCKHJFdry1XJaJPO_uvxM-mvNxrqQXbWWQ",
   authDomain: "tik-tok-clone-7513a.firebaseapp.com",
@@ -7,3 +9,9 @@ const firebaseConfig = {
   appId: "1:687117064514:web:1895cb44b26a1f9488d0db",
   measurementId: "G-S64DH7HVQ3"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
