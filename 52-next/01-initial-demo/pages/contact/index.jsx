1878d8c1aca4a98c5b40ef8065c647d9c0693363
daniel-barreto <daniel30081990@gmail.com>
2022-09-27 00:21:41 -0300

    11. Directory structure in a project

---
+++ 52-next/01-initial-demo/pages/contact/index.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
+
+export default function HomePage() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home - Daniel</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Contact</h1>
+      </main>
+    </div>
+  )
+}
