57c19b5f9bbc289ee987653ee90cf9cbeb6dca8e
daniel-barreto <daniel30081990@gmail.com>
2022-09-28 11:21:28 -0300

    20. Nested Multiple Layouts

--- 52-next/01-initial-demo/pages/about.jsx
+++ 52-next/01-initial-demo/pages/about.jsx
@@ -1,22 +1,31 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
-import Head from 'next/head'
 import Link from 'next/link'
+
+import { DarkLayout } from '../components/layouts/DarkLayout'
 import { MainLayout } from '../components/layouts/MainLayout'
-import { Navbar } from '../components/Navbar'
 
 export default function AboutPage() {
   return (
+    <>
+        <h1>About Page</h1>
+
+        <h1 className={'title'}>
+        Go to<Link href="/"> Home</Link>
+        </h1>
+
+        <p className={'description'}>
+          Get started by editing{' '}
+          <code className={'code'}>pages/about.jsx</code>
+        </p>
+    </>
+  )
+}
+
+AboutPage.getLayout = function getLayout( page ) {
+  return (
     <MainLayout>
-      <h1>About Page</h1>
-
-      <h1 className={'title'}>
-      Go to<Link href="/"> Home</Link>
-      </h1>
-
-      <p className={'description'}>
-        Get started by editing{' '}
-        <code className={'code'}>pages/about.jsx</code>
-      </p>
+      <DarkLayout>
+        { page }
+      </DarkLayout>
     </MainLayout>
   )
 }
