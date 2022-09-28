3b4cbd2d26ee2afa35901375402911e469d263ed
daniel-barreto <daniel30081990@gmail.com>
2022-09-28 19:15:36 -0300

    28. JavaScript Files to TypeScript

--- 52-next/01-initial-demo/components/layouts/DarkLayout.tsx
+++ 52-next/01-initial-demo/components/layouts/DarkLayout.tsx
@@ -1,5 +1,9 @@
+import React from 'react';
 
-export const DarkLayout = ({ children }) => {
+interface Props {
+  children?: React.ReactNode;
+}
+export const DarkLayout: React.FC<Props> = ({ children }) => {
   return (
     <div style={{
       backgroundColor: 'rgba(0,0,0,0.3)',
--- 52-next/01-initial-demo/components/layouts/MainLayout.tsx
+++ 52-next/01-initial-demo/components/layouts/MainLayout.tsx
@@ -1,9 +1,13 @@
+import React from 'react';
 import Head from 'next/head';
 import { Navbar } from '../Navbar';
 import styles from './MainLayout.module.css';
 
+interface Props {
+  children?: React.ReactNode;
+}
 
-export const MainLayout = ({ children }) => {
+export const MainLayout: React.FC<Props> = ({ children }) => {
   return (
     <div className={styles.container}>
       <Head>
