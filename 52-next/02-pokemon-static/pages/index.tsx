2ab396d4b41723a1fffa1b4e0b887a3f4e2d1e41
daniel-barreto <daniel30081990@gmail.com>
2022-09-30 21:16:40 -0300

    45. Next - GetStaticProps

--- 52-next/02-pokemon-static/pages/index.tsx
+++ 52-next/02-pokemon-static/pages/index.tsx
@@ -1,15 +1,35 @@
-import { Button } from '@nextui-org/react';
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 import { Layout } from '../components/layouts';
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+
+  console.log({props});
+
   return (
     <Layout title="Pokemons list">
-      <Button color="gradient">
-          Hello world
-      </Button>
+      <ul>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+      </ul>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  console.log('hello world');
+
+  return {
+    props: {
+      name: 'Daniel'
+    }
+  }
 }
 
 export default HomePage;
