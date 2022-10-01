8d43a79ddbd03fad985551997d649a7e8fe8a729
daniel-barreto <daniel30081990@gmail.com>
2022-09-30 22:26:02 -0300

    49. Show Pokémon cards

--- 52-next/02-pokemon-static/pages/index.tsx
+++ 52-next/02-pokemon-static/pages/index.tsx
@@ -1,6 +1,8 @@
 import { NextPage, GetStaticProps } from 'next'
-import { Layout } from '../components/layouts';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
+
 import { pokeApi } from '../api';
+import { Layout } from '../components/layouts';
 import { PokemonListResponse, SmallPokemon } from '../interfaces';
 
 interface Props {
@@ -11,15 +13,29 @@ const HomePage: NextPage<Props> = ({ pokemons }) => {
 
   return (
     <Layout title="Pokemons list">
-      <ul>
+      <Grid.Container gap={ 2 } justify='flex-start'>
         {
-          pokemons.map( ({ id, name }) => (
-            <li key={ id }>
-              #{ id } - { name }
-            </li>
+          pokemons.map( ({ id, name, img }) => (
+            <Grid xs={ 6 } sm={ 3 } md={ 2 } xl={ 1 } key={ id }>
+              <Card hoverable clickable>
+                <Card.Body css={{ p: 1 }}>
+                  <Card.Image
+                    src={ img }
+                    width="100%"
+                    height={ 140 }
+                  />
+                </Card.Body>
+                <Card.Footer>
+                  <Row justify='space-between'>
+                    <Text transform='capitalize'>{ name }</Text>
+                    <Text>#{ id }</Text>
+                  </Row>
+                </Card.Footer>
+              </Card>
+            </Grid>
           ))
         }
-      </ul>
+      </Grid.Container>
     </Layout>
   )
 }
