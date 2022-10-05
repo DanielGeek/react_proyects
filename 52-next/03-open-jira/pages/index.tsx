30428dc3c8068c25b7260968ca38d01a2b83d997
daniel-barreto <daniel30081990@gmail.com>
2022-10-05 14:37:32 -0300

    91. Layout y Navbar

--- 52-next/03-open-jira/pages/index.tsx
+++ 52-next/03-open-jira/pages/index.tsx
@@ -1,9 +1,12 @@
 import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { Layout } from '../components/layout';
 
 const HomePage: NextPage = () => {
   return (
-    <Typography variant='h1' color='primary'>Hello World</Typography>
+    <Layout>
+      <Typography variant='h1' color='primary'>Hello World</Typography>
+    </Layout>
   )
 }
 
