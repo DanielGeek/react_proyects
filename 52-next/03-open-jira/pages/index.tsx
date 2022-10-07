2cb57840b998569737b5dfb0cd657ef4d3e5fb03
daniel-barreto <daniel30081990@gmail.com>
2022-10-07 12:18:49 -0300

    105. Main Screen Layout - index.tsx

--- 52-next/03-open-jira/pages/index.tsx
+++ 52-next/03-open-jira/pages/index.tsx
@@ -1,11 +1,37 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
+
 import { Layout } from '../components/layout';
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant='h1' color='primary'>Hello World</Typography>
+    <Layout title='Home - OpenJira'>
+      <Grid container spacing={ 2 }>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="To do" />
+
+            <CardContent>
+
+            </CardContent>
+
+          </Card>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="Doing" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="Done" />
+          </Card>
+        </Grid>
+
+      </Grid>
     </Layout>
   )
 }
