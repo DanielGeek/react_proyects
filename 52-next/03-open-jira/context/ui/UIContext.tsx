d066d92be7c9e08a2b493aebef2fa29fdb66f4d0
daniel-barreto <daniel30081990@gmail.com>
2022-10-07 11:58:24 -0300

    99. Use the UI Context in our application

--- 52-next/03-open-jira/context/ui/UIContext.tsx
+++ 52-next/03-open-jira/context/ui/UIContext.tsx
@@ -3,6 +3,10 @@ import { createContext } from 'react';
 
 interface ContextProps {
    sidemenuOpen: boolean;
+
+   // Methods
+   closeSideMenu: () => void;
+   openSideMenu: () => void;
 }
 
 export const UIContext = createContext({} as ContextProps);
