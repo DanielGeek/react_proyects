bcd5400cf23ce2defe07412a498fdd6edfa16d48
daniel-barreto <daniel30081990@gmail.com>
2022-10-08 22:39:53 -0300

    116. Change the state of the Entry

--- 52-next/03-open-jira/context/entries/EntriesProvider.tsx
+++ 52-next/03-open-jira/context/entries/EntriesProvider.tsx
@@ -53,12 +53,19 @@ export const EntriesProvider:React.FC<Props> = ({ children }) => {
 
  }
 
+ const updateEntry = ( entry: Entry ) => {
+
+  dispatch({ type: '[Entry] Entry-Updated', payload: entry });
+
+ }
+
  return (
    <EntriesContext.Provider value={{
      ...state,
 
      // Methos
      addNewEntry,
+     updateEntry,
    }}>
      { children }
    </EntriesContext.Provider>
