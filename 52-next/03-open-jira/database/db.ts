16cc8707a881783e7b4bc91ff5c28fe778255483
daniel-barreto <daniel30081990@gmail.com>
2022-10-09 22:44:47 -0300

    124. Connect to MongoDB with Mongoose

---
+++ 52-next/03-open-jira/database/db.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+
+const mongooConnection = {
+  isConnected: 0
+}
+
+export const connect = async() => {
+
+  if ( mongooConnection.isConnected ) {
+    console.log('We are connected');
+    return;
+  }
+
+  if ( mongoose.connections.length > 0 ) {
+    mongooConnection.isConnected = mongoose.connections[0].readyState;
+
+    if ( mongooConnection.isConnected === 1 ) {
+      console.log('Using before connection');
+      return;
+    }
+
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect('....');
+  mongooConnection.isConnected = 1;
+  console.log('Connected to MongoDB', '....');
+}
+
+export const disconnect = async() => {
+
+  if ( mongooConnection.isConnected !== 0 ) return;
+
+  await mongoose.disconnect();
+  console.log('Disconnected from MongoDB');
+}
