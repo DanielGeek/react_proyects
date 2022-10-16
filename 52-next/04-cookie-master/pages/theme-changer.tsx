7e4ff61957e80fbf4927d3f01b81c7bdcf17a4ff
daniel-barreto <daniel30081990@gmail.com>
2022-10-15 21:26:18 -0300

    163. Page to change theme

--- 52-next/04-cookie-master/pages/theme-changer.tsx
+++ 52-next/04-cookie-master/pages/theme-changer.tsx
@@ -1,9 +1,41 @@
+import { ChangeEvent, useState } from "react";
+import { Button, Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
 import { Layout } from "../components/layouts";
 
 const ThemeChangerPage = () => {
+
+  const [currentTheme, setCurrentTheme] = useState('light');
+
+  const onThemeChange = ( event: ChangeEvent<HTMLInputElement>) => {
+    const selectedTheme = event.target.value;
+
+    console.log({ selectedTheme });
+    setCurrentTheme( selectedTheme );
+  }
+
   return (
     <Layout>
-      <h1>ThemeChangerPage</h1>
+        <Card>
+            <CardContent>
+                <FormControl>
+                    <FormLabel>Theme</FormLabel>
+                    <RadioGroup
+                        value={ currentTheme }
+                        onChange={ onThemeChange }
+                    >
+                        <FormControlLabel value='light' control={ <Radio /> } label="Light" />
+                        <FormControlLabel value='dark' control={ <Radio /> } label="Dark" />
+                        <FormControlLabel value='custom' control={ <Radio /> } label="Custom" />
+                    </RadioGroup>
+                </FormControl>
+
+                <Button
+                    // onClick={ onClick }
+                >
+                    Request
+                </Button>
+            </CardContent>
+        </Card>
     </Layout>
   )
 }
