62d40c041b9efc34a7a6fe3a9b5f25bb51a0afbe
daniel-barreto <daniel30081990@gmail.com>
2022-10-15 21:15:32 -0300

    162. Layout and Additional Pages

---
+++ 52-next/04-cookie-master/components/layouts/Layout.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import React from 'react'
+import { Navbar } from '../ui';
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export const Layout:React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Head>
+
+      </Head>
+      <nav>
+        <Navbar />
+      </nav>
+      <main style={{ padding: '20px 50px'}}>
+          { children }
+      </main>
+    </>
+  )
+}
