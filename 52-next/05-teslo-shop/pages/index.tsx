e68abc9afe767c229892accb9c7c324fc87c16fd
daniel-barreto <daniel30081990@gmail.com>
2022-10-19 21:20:33 -0300

    177. Directory Structure and ShopLayout

--- 52-next/05-teslo-shop/pages/index.tsx
+++ 52-next/05-teslo-shop/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
+import { Typography } from '@mui/material';
+
+import { ShopLayout } from '../components/layouts';
 
 const Home: NextPage = () => {
   return (
-    <h1>Teslo-shop</h1>
+    <ShopLayout title={'Teslo-Shop'} pageDescription={'Find the best teslo products here'}>
+      <Typography variant='h1' component='h1'>Store</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>All products</Typography>
+    </ShopLayout>
   )
 }
 
