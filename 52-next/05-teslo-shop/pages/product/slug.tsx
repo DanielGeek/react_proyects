d57d4b41a0be6cdff80a7a85043d2ef18b1708bb
daniel-barreto <daniel30081990@gmail.com>
2022-10-22 15:41:46 -0300

    185. Slideshow of images

--- 52-next/05-teslo-shop/pages/product/slug.tsx
+++ 52-next/05-teslo-shop/pages/product/slug.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import { ShopLayout } from "../../components/layouts"
+import { ProductSlideshow } from "../../components/products";
 import { initialData } from '../../database/products';
 
 const product = initialData.products[0];
@@ -10,7 +11,9 @@ const ProductPage = () => {
       <Grid container spacing={3}>
 
         <Grid item xs={12} sm={7}>
-
+          <ProductSlideshow
+            images={ product.images }
+          />
         </Grid>
 
         <Grid item xs={12} sm={5}>
