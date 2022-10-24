336c36393973a89092613078bbc7437c1281d381
daniel-barreto <daniel30081990@gmail.com>
2022-10-24 16:56:02 -0300

    197. Registration Page

--- 52-next/05-teslo-shop/pages/auth/register.tsx
+++ 52-next/05-teslo-shop/pages/auth/register.tsx
@@ -2,15 +2,18 @@ import NextLink from 'next/link';
 import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../../components/layouts'
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
-    <AuthLayout title={"Get into"}>
+    <AuthLayout title={'Create Account'}>
         <Box sx={{ width: 350, padding:'10px 20px' }}>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                    <Typography variant='h1' component="h1">Log in</Typography>
+                    <Typography variant='h1' component="h1">Create Account</Typography>
                 </Grid>
 
+                <Grid item xs={12}>
+                    <TextField label="Name" variant="filled" fullWidth />
+                </Grid>
                 <Grid item xs={12}>
                     <TextField label="Email" variant="filled" fullWidth />
                 </Grid>
@@ -20,14 +23,14 @@ const LoginPage = () => {
 
                 <Grid item xs={12}>
                     <Button color="secondary" className='circular-btn' size='large' fullWidth>
-                        Get into
+                        Register
                     </Button>
                 </Grid>
 
                 <Grid item xs={12} display='flex' justifyContent='end'>
-                    <NextLink href="/auth/register" passHref>
+                    <NextLink href="/auth/login" passHref>
                         <Link underline='always'>
-                            You do not have an account?
+                            Do you already have an account?
                         </Link>
                     </NextLink>
                 </Grid>
@@ -37,4 +40,4 @@ const LoginPage = () => {
   )
 }
 
-export default LoginPage
+export default RegisterPage
