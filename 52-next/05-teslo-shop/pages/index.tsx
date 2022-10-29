db9e3892e98ca83e9dbcf700180efa4d394ffd2c
daniel-barreto <daniel30081990@gmail.com>
2022-10-29 17:59:44 -0300

    216. SWR: React Hooks for Data Fetching

--- 52-next/05-teslo-shop/pages/index.tsx
+++ 52-next/05-teslo-shop/pages/index.tsx
@@ -5,17 +5,28 @@ import { ShopLayout } from '../components/layouts';
 import { initialData } from '../database/products';
 import { ProductList } from '../components/products';
 
-const Home: NextPage = () => {
+import useSWR from 'swr';
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+
+const HomePage: NextPage = () => {
+
+  const { data, error } = useSWR('/api/products', fetcher );
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  console.log({ data });
+
   return (
     <ShopLayout title={'Teslo-Shop'} pageDescription={'Find the best teslo products here'}>
       <Typography variant='h1' component='h1'>Store</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>All products</Typography>
 
       <ProductList
-        products={ initialData.products as any }
+        products={ data }
       />
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
