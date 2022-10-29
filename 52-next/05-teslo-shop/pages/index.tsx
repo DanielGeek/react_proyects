8ab326f06e0c8ec81c40f05203a761b59c00c832
daniel-barreto <daniel30081990@gmail.com>
2022-10-29 19:02:10 -0300

    218. Show a loading screen

--- 52-next/05-teslo-shop/pages/index.tsx
+++ 52-next/05-teslo-shop/pages/index.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from 'next'
 import { Typography } from '@mui/material';
 
 import { ShopLayout } from '../components/layouts';
-import { initialData } from '../database/products';
+
 import { ProductList } from '../components/products';
 import { useProducts } from '../hooks';
+
+import { FullScreenLoading } from '../components/ui';
 
 const HomePage: NextPage = () => {
 
@@ -14,10 +16,9 @@ const HomePage: NextPage = () => {
     <ShopLayout title={'Teslo-Shop'} pageDescription={'Find the best teslo products here'}>
       <Typography variant='h1' component='h1'>Store</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>All products</Typography>
-
       {
         isLoading
-          ? <h1>Loading...</h1>
+          ? <FullScreenLoading />
           : <ProductList products={ products } />
       }
     </ShopLayout>
