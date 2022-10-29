704ca6d7b218852b7173fd1b586e045b1ed3fa5e
daniel-barreto <daniel30081990@gmail.com>
2022-10-29 18:40:30 -0300

    217. Custom Hooks and Global SWR configuration

--- 52-next/05-teslo-shop/pages/index.tsx
+++ 52-next/05-teslo-shop/pages/index.tsx
@@ -4,27 +4,22 @@ import { Typography } from '@mui/material';
 import { ShopLayout } from '../components/layouts';
 import { initialData } from '../database/products';
 import { ProductList } from '../components/products';
-
-import useSWR from 'swr';
-const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+import { useProducts } from '../hooks';
 
 const HomePage: NextPage = () => {
 
-  const { data, error } = useSWR('/api/products', fetcher );
-
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-
-  console.log({ data });
+  const { products, isLoading } = useProducts('/products');
 
   return (
     <ShopLayout title={'Teslo-Shop'} pageDescription={'Find the best teslo products here'}>
       <Typography variant='h1' component='h1'>Store</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>All products</Typography>
 
-      <ProductList
-        products={ data }
-      />
+      {
+        isLoading
+          ? <h1>Loading...</h1>
+          : <ProductList products={ products } />
+      }
     </ShopLayout>
   )
 }
