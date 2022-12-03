d68918823cb9ab3a7647af9dc142ea2d6b12a791
DanielGeek <daniel30081990@gmail.com>
2022-12-03 01:14:05 -0300

    251. Calculate amounts to pay

--- 52-next/05-teslo-shop/context/cart/CartProvider.tsx
+++ 52-next/05-teslo-shop/context/cart/CartProvider.tsx
@@ -31,8 +31,21 @@ export const CartProvider:React.FC<Props> = ({ children }) => {
     useEffect(() => {
       Cookie.set('cart', JSON.stringify( state.cart ));
     }, [state.cart]);
+ 
+    useEffect(() => {
+      
+        const numberOfItems = state.cart.reduce( ( prev, current ) => current.quantity + prev, 0);
+        const subTotal = state.cart.reduce( ( prev, current ) => (current.price * current.quantity) + prev, 0);
+        const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
 
+        const orderSummary = {
+            numberOfItems,
+            subTotal,
+            tax: subTotal * taxRate,
+            total: subTotal * ( taxRate + 1)
+        }
 
+    }, [state.cart])
 
     const addProductToCart = ( product: ICartProduct ) => {
         //! Level 1
