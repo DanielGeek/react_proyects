79c51e33a1e37e6e581ebc1b05251aec4a8aaedb
DanielGeek <daniel30081990@gmail.com>
2022-12-03 21:53:18 -0300

    261. Create JWT - JsonWebToken

--- 52-next/05-teslo-shop/pages/api/user/login.ts
+++ 52-next/05-teslo-shop/pages/api/user/login.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { db } from '../../../database'
 import { User } from '../../../models'
 import bcrypt from 'bcryptjs';
+import { jwt } from '../../../utils';
 
 type Data = 
  | { message: string }
@@ -43,10 +44,12 @@ const loginUser = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
     return res.status(400).json({ message: 'Email or password not valid - PASSWORD'})
   }
 
-  const { role, name } = user;
+  const { role, name, _id } = user;
+
+  const token = jwt.signToken( _id, email );
 
   return res.status(200).json({
-    token: '',  // jwt
+    token,  // jwt
     user: {
       email, role, name
     }
---
+++ 52-next/05-teslo-shop/utils/jwt.ts
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+export const signToken = ( _id: string, email: string ) => {
+
+  if ( !process.env.JWT_SECRET_SEED ) {
+      throw new Error('No JWT Seed - Check Environment Variables');
+  }
+
+  return jwt.sign(
+      // payload
+      { _id, email },
+
+      // Seed
+      process.env.JWT_SECRET_SEED,
+
+      // Options
+      { expiresIn: '30d' }
+  )
+
+}
