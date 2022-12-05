d048037f73526949d41c4c51395e1ac2a1f91d0b
DanielGeek <daniel30081990@gmail.com>
2022-12-05 01:37:45 -0300

    271. Trigger Login actions

--- 52-next/05-teslo-shop/context/auth/AuthProvider.tsx
+++ 52-next/05-teslo-shop/context/auth/AuthProvider.tsx
@@ -1,6 +1,9 @@
 import { FC, useReducer } from 'react';
-import { IUser } from '../../interfaces';
 import { AuthContext, authReducer } from './';
+import Cookies from 'js-cookie';
+
+import { tesloApi } from '../../api';
+import { IUser } from '../../interfaces';
 
 interface Props {
     children?: React.ReactNode;
@@ -19,11 +22,25 @@ export const AuthProvider:FC<Props> = ({ children }) => {
 
  const [state, dispatch] = useReducer( authReducer, AUTH_INITIAL_STATE );
 
+ const loginUser = async( email: string, password: string ): Promise<boolean> => {
+
+    try {
+      const { data } = await tesloApi.post('/user/login', { email, password });
+      const { token, user } = data;
+      Cookies.set('token', token );
+      dispatch({ type: '[Auth] - Login', payload: user });
+      return true;
+    } catch (error) {
+        return false;
+    }
+ }
+
  return (
    <AuthContext.Provider value={{
        ...state,
 
        // Methods
+       loginUser
    }}>
      { children }
    </AuthContext.Provider>
--- 52-next/05-teslo-shop/pages/auth/login.tsx
+++ 52-next/05-teslo-shop/pages/auth/login.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+
 import { Box, Button, Chip, Grid, Link, TextField, Typography } from '@mui/material';
 import { ErrorOutline } from '@mui/icons-material';
-import { AuthLayout } from '../../components/layouts';
 import { useForm } from 'react-hook-form';
+
+import { AuthContext } from '../../context/auth/AuthContext';
+import { AuthLayout } from '../../components/layouts';
 import { validations } from '../../utils';
-import tesloApi from '../../api/tesloApi';
 
 type FormData = {
     email: string;
@@ -14,6 +17,9 @@ type FormData = {
 
 const LoginPage = () => {
 
+    const router = useRouter();
+    const { loginUser } = useContext( AuthContext );
+
     const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
     const [ showError, setShowError ] = useState(false);
 
@@ -21,16 +27,15 @@ const LoginPage = () => {
         
         setShowError(false);
 
-        try {
-            const { data } = await tesloApi.post('/user/login', { email, password });
-            const { token, user } = data;
-            console.log({ token, user });
+        const isValidLogin = await loginUser( email, password );
 
-        } catch (error) {
-            console.log('Credentials error');
+        if ( !isValidLogin ) {
             setShowError(true);
             setTimeout(() => setShowError(false), 3000);
+            return;
         }
+
+        router.replace('/');
     }
 
     return (
