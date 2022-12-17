38b220a9bde6eba3491d196749d3873df62906df
Daniel <daniel30081990@gmail.com>
2022-12-17 01:15:36 -0300

    306. NextJs Middlewares with Next Auth

--- 52-next/05-teslo-shop/middleware.ts
+++ 52-next/05-teslo-shop/middleware.ts
@@ -1,22 +1,38 @@
-import { NextRequest, NextResponse } from "next/server";
-import * as jose from "jose";
- 
-export async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith("/checkout")) {
-    const token = req.cookies.get("token") || "";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+// import * as jose from "jose";
+
+export async function middleware(req: NextRequest | any, ev: NextFetchEvent ) {
+
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  // console.log({ session });
+
+  if ( !session ) {
     const previousPage = req.nextUrl.pathname;
-    try {
-      await jose.jwtVerify(
-        token,
-        new TextEncoder().encode(process.env.JWT_SECRET_SEED)
-      );
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect(
-        new URL(`/auth/login?p=${previousPage}`, req.url)
-      );
-    }
+    return NextResponse.redirect(
+      new URL(`/auth/login?p=${previousPage}`, req.url)
+    );
+
   }
+
+  return NextResponse.next();
+
+  // if (req.nextUrl.pathname.startsWith("/checkout")) {
+  //   const token = req.cookies.get("token") || "";
+  //   const previousPage = req.nextUrl.pathname;
+  //   try {
+  //     await jose.jwtVerify(
+  //       token,
+  //       new TextEncoder().encode(process.env.JWT_SECRET_SEED)
+  //     );
+  //     return NextResponse.next();
+  //   } catch (error) {
+  //     return NextResponse.redirect(
+  //       new URL(`/auth/login?p=${previousPage}`, req.url)
+  //     );
+  //   }
+  // }
 }
 // Only the paths declared in here will run the middleware
 export const config = {
