1df9b12cd3d1e4089ffd5b1fade1efd9a6afc7c0
Daniel <daniel30081990@gmail.com>
2022-12-17 23:05:15 -0300

    317. Create Order - Backend

--- 52-next/05-teslo-shop/pages/api/orders/index.ts
+++ 52-next/05-teslo-shop/pages/api/orders/index.ts
@@ -1,4 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react';
+import { db } from '../../../database';
+import { IOrder } from '../../../interfaces';
+import { Product } from '../../../models';
 
 type Data = {
     message: string
@@ -17,8 +21,37 @@ export default function handler( req: NextApiRequest, res: NextApiResponse<Data>
 
 const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
-    const body = req.body;
+    const { orderItems, total } = req.body as IOrder;
 
-    return res.status(201).json( body );
+    // Verify that we have an user
+    const session: any = await getSession({ req });
+    if ( !session ) {
+        return res.status(401).json({ message: 'You must be authenticated to do this' });
+    }
+
+    // Create an array with the products that the person wants
+    const productsIds = orderItems.map( product => product._id );
+    await db.connect();
+
+    const dbProducts = await Product.find({ _id: { $in: productsIds } });
+
+    try {
+        
+        const subTotal = orderItems.reduce( ( prev, current ) => {
+            const currentPrice = dbProducts.find( prod => prod._id === current._id )?.price;
+            if ( !currentPrice ) {
+                throw new Error('Check the cart again, product does not exist');
+            }
+
+            return ( currentPrice * current.quantity ) + prev
+        }, 0 );
+    } catch (error) {
+        
+    }
+
+    console.log({ dbProducts });
+
+
+    return res.status(201).json( req.body );
 }
 
