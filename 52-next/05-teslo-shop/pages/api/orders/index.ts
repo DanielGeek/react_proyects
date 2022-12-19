f3d16155db7c0655889feb780a8e67832331beba
Daniel <daniel30081990@gmail.com>
2022-12-18 22:16:40 -0300

    318. Create Order - Backend Part 2

--- 52-next/05-teslo-shop/pages/api/orders/index.ts
+++ 52-next/05-teslo-shop/pages/api/orders/index.ts
@@ -2,11 +2,11 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react';
 import { db } from '../../../database';
 import { IOrder } from '../../../interfaces';
-import { Product } from '../../../models';
+import { Product, Order } from '../../../models';
 
-type Data = {
-    message: string
-}
+type Data = 
+    | { message: string }
+    | IOrder;
 
 export default function handler( req: NextApiRequest, res: NextApiResponse<Data>) {
     
@@ -38,20 +38,38 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     try {
         
         const subTotal = orderItems.reduce( ( prev, current ) => {
-            const currentPrice = dbProducts.find( prod => prod._id === current._id )?.price;
+            const currentPrice = dbProducts.find( prod => prod.id === current._id )?.price;
             if ( !currentPrice ) {
                 throw new Error('Check the cart again, product does not exist');
             }
 
             return ( currentPrice * current.quantity ) + prev
         }, 0 );
-    } catch (error) {
-        
+
+        const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+        const backendTotal = subTotal * ( taxRate + 1 );
+
+        if ( total !== backendTotal ) {
+            throw new Error('The total does not match the amount')
+        }
+
+        const userId = session.user._id;
+        const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+        await newOrder.save();
+
+        return res.status(201).json( newOrder );
+
+    } catch ( error: any) {
+        await db.disconnect();
+        console.log(error);
+        res.status(400).json({
+            message: error.message || 'Review server logs'
+        })
     }
 
     console.log({ dbProducts });
 
 
-    return res.status(201).json( req.body );
+    // return res.status(201).json( req.body );
 }
 
