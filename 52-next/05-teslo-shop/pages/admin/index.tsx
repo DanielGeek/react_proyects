1a72b2aaa8ac796234355f7cf8373f1ac1035a02
Daniel <daniel30081990@gmail.com>
2022-12-29 23:09:03 -0300

    352. Show statistics on the dashboard

--- 52-next/05-teslo-shop/pages/admin/index.tsx
+++ 52-next/05-teslo-shop/pages/admin/index.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import useSWR from 'swr';
+
 import { AttachMoneyOutlined, CreditCardOffOutlined, DashboardOutlined, GroupOutlined, CategoryOutlined, ProductionQuantityLimitsOutlined, CancelPresentationOutlined, AccessTimeOutlined } from '@mui/icons-material';
+
 import { AdminLayout } from '../../components/layouts';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import { SummaryTitle } from '../../components/admin';
+import { DashboardSummaryResponse } from '../../interfaces';
 
 const DashboardPage = () => {
-  return (
+
+    const { data, error } = useSWR<DashboardSummaryResponse>('/api/admin/dashboard', {
+        refreshInterval: 30 * 1000 // 30 seconds
+    });
+
+    const [refreshIn, setRefreshIn] = useState(30);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        console.log('Tick');
+        setRefreshIn( refreshIn => refreshIn > 0 ? refreshIn - 1: 30);
+        }, 1000 );
+
+        return () => clearInterval(interval);
+    }, []);
+    
+
+    if ( !error && !data ) {
+        return <></>
+    }
+
+    if ( error ) {
+        console.log(error);
+        return <Typography>Error loading information</Typography>
+    }
+
+    const {
+        numberOfOrders,
+        paidOrders,
+        numberOfClients,
+        numberOfProducts,
+        productsWithNoInventory,
+        lowInventory,
+        notPaidOrders,
+    } = data!;
+    
+    return (
     <AdminLayout
         title='Dashboard'
         subTitle='General statistics'
@@ -14,55 +54,55 @@ const DashboardPage = () => {
         <Grid container spacing={2}>
             
             <SummaryTitle
-                title={ 1 }
+                title={ numberOfOrders }
                 subTitle="Total orders"
                 icon={ <CreditCardOffOutlined color="secondary" sx={{ fontSize: 40 }} />}
             />
 
             <SummaryTitle
-                title={ 2 }
+                title={ paidOrders }
                 subTitle="Paid orders"
                 icon={ <AttachMoneyOutlined color="success" sx={{ fontSize: 40 }} />}
             />
 
             <SummaryTitle
-                title={ 3 }
+                title={ notPaidOrders }
                 subTitle="Pending orders"
                 icon={ <CreditCardOffOutlined color="error" sx={{ fontSize: 40 }} />}
             />
 
             <SummaryTitle
-                title={ 4 }
+                title={ numberOfClients }
                 subTitle="Clients"
                 icon={ <GroupOutlined color="primary" sx={{ fontSize: 40 }} />}
             />
 
             <SummaryTitle
-                title={ 5 }
+                title={ numberOfProducts }
                 subTitle="Products"
                 icon={ <CategoryOutlined color="warning" sx={{ fontSize: 40 }} />}
             />
             
             <SummaryTitle
-                title={ 6 }
+                title={ productsWithNoInventory }
                 subTitle="Without existence"
                 icon={ <CancelPresentationOutlined color="error" sx={{ fontSize: 40 }} />}
             />
 
             <SummaryTitle
-                title={ 7 }
+                title={ lowInventory }
                 subTitle="Low inventory"
                 icon={ <ProductionQuantityLimitsOutlined color="warning" sx={{ fontSize: 40 }} />}
             />
 
             <SummaryTitle
-                title={ 8 }
+                title={ refreshIn }
                 subTitle="Update on"
                 icon={ <AccessTimeOutlined color="secondary" sx={{ fontSize: 40 }} />}
             />
         </Grid>
     </AdminLayout>
-  )
+    )
 }
 
 export default DashboardPage
--- 52-next/05-teslo-shop/pages/api/admin/dashboard.ts
+++ 52-next/05-teslo-shop/pages/api/admin/dashboard.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database';
+import { Order, Product, User } from '../../../models';
 
 type Data = {
     numberOfOrders: number;
@@ -6,10 +8,47 @@ type Data = {
     notPaidOrders: number;
     numberOfClients: number;
     numberOfProducts: number;
-    productsWithInventory: number;
+    productsWithNoInventory: number;
     lowInventory: number;
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'Example' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  await db.connect();
+
+  // const numberOfOrders = await Order.count();
+  // const paidOrders = await Order.find({ isPaid: true }).count();
+  // const numberOfClients = await User.find({ role: 'client' }).count();
+  // const numberOfProducts = await Product.count();
+  // const productsWithNoInventory = await Product.find({ inStock: 0 }).count();
+  // const lowInventory = await Product.find({ inStock: { $lte: 10 }}).count();
+
+  const [
+      numberOfOrders,
+      paidOrders,
+      numberOfClients,
+      numberOfProducts,
+      productsWithNoInventory,
+      lowInventory,
+  ] = await Promise.all([
+    Order.count(),
+    Order.find({ isPaid: true }).count(),
+    User.find({ role: 'client' }).count(),
+    Product.count(),
+    Product.find({ inStock: 0 }).count(),
+    Product.find({ inStock: { $lte: 10 }}).count(),
+
+  ]);
+
+  await db.disconnect();
+
+  res.status(200).json({
+      numberOfOrders,
+      paidOrders,
+      numberOfClients,
+      numberOfProducts,
+      productsWithNoInventory,
+      lowInventory,
+      notPaidOrders: numberOfOrders - paidOrders
+  })
 }
