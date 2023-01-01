96e71bca3017958dca88e1d81b66fa671903bd75
Daniel <daniel30081990@gmail.com>
2023-01-01 02:04:34 -0300

    368. List of products

--- 52-next/05-teslo-shop/components/ui/SideMenu.tsx
+++ 52-next/05-teslo-shop/components/ui/SideMenu.tsx
@@ -162,7 +162,10 @@ export const SideMenu = () => {
                                 <ListItemText primary={'Dashboard'} />
                             </ListItem>
 
-                            <ListItem button>
+                            <ListItem 
+                                button
+                                onClick={ () => navigateTo('/admin/products') }
+                            >
                                 <ListItemIcon>
                                     <CategoryOutlined/>
                                 </ListItemIcon>
