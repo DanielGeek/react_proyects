d083e7119c2c44f0c23e9c1dc560e157b93bb93d
Daniel <daniel30081990@gmail.com>
2023-01-03 00:55:20 -0300

    375. REST - Update Product

--- 52-next/05-teslo-shop/pages/api/admin/products.ts
+++ 52-next/05-teslo-shop/pages/api/admin/products.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { isValidObjectId } from 'mongoose';
+
 import { IProduct } from '../../../interfaces';
 import { db } from '../../../database';
 import { Product } from '../../../models';
@@ -6,6 +8,7 @@ import { Product } from '../../../models';
 type Data = 
 | { message: string }
 | IProduct[]
+| IProduct;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     switch ( req.method ) {
@@ -13,6 +16,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
             return getProducts(req, res);
         
         case 'PUT':
+            return updateProduct(req, res);
+
         case 'POST':
 
         default:
@@ -33,3 +38,38 @@ const getProducts = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     res.status(200).json( products );
 }
+
+
+const updateProduct = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+    
+    const { _id = '', images = [] } = req.body as IProduct;
+
+    if ( !isValidObjectId( _id ) ) {
+        return res.status(400).json({ message: 'ID of product is not valid' });
+    }
+
+    if ( images.length < 2 ) {
+        return res.status(400).json({ message: 'You need at least 2 images' });
+    }
+
+    try {
+        
+        await db.connect();
+        const product = await Product.findById(_id);
+        if ( !product ) {
+            await db.disconnect();
+            return res.status(400).json({ message: 'There is no product with that ID' });
+        }
+
+        await product.update( req.body );
+        await db.disconnect();
+
+        return res.status(200).json( product );
+
+    } catch (error) {
+        console.log(error);
+        await db.disconnect();
+        return res.status(400).json({ message: 'Check the server console' });
+    }
+}
+
