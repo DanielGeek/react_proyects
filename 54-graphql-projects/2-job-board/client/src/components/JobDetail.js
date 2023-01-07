204c2bbb680feea1bc57651d85fd73cdf7759845
Daniel <daniel30081990@gmail.com>
2023-01-07 19:28:19 -0300

    19. Query Variables

--- 54-graphql-projects/2-job-board/client/src/components/JobDetail.js
+++ 54-graphql-projects/2-job-board/client/src/components/JobDetail.js
@@ -1,11 +1,20 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import { jobs } from '../fake-data';
+import { getJob } from '../graphql/queries';
 
 function JobDetail() {
+  const [job, setJob] = useState(null);
   const { jobId } = useParams();
+  
+  useEffect(() => {
+    getJob(jobId).then(setJob);
+  }, [jobId]);
+  
+  if (!job) {
+    return <p>Loading...</p>;
+  }
 
-  const job = jobs.find((job) => job.id === jobId);
   return (
     <div>
       <h1 className="title">
