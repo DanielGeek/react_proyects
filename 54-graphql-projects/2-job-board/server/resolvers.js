8228421ed8fa9b36811a9a01249ba2a7eb1db54f
Daniel <daniel30081990@gmail.com>
2023-01-12 02:04:19 -0300

    33. Exercise: Secure Update/Delete

--- 54-graphql-projects/2-job-board/server/resolvers.js
+++ 54-graphql-projects/2-job-board/server/resolvers.js
@@ -1,5 +1,11 @@
 import { Company, Job } from './db.js';
 
+const rejectIf = (condition) => {
+    if (condition) {
+        throw new Error('Unauthorized');
+    }
+}
+
 export const resolvers = {
     Query: {
         company: (_root, { id }) => Company.findById(id),
@@ -9,14 +15,23 @@ export const resolvers = {
 
     Mutation: {
         createJob: (_root, { input }, { user }) => {
-            console.log('[createJob] auth:', user);
-            if (!user) {
-                throw new Error('Unauthorized');
-            }
+            rejectIf(!user);
             return Job.create({ ...input, companyId: user.companyId });
         },
-        deleteJob: (_root, { id }) => Job.delete(id),
-        updateJob: (_root, { input }) => Job.update(input),
+
+        deleteJob: async (_root, { id }, { user }) => {
+            rejectIf(!user);
+            const job = await Job.findById(id);
+            rejectIf(job.companyId !== user.companyId);
+            return Job.delete(id);
+        },
+
+        updateJob: async (_root, { input }, { user }) => {
+            rejectIf(!user);
+            const job = await Job.findById(input.id);
+            rejectIf(job.companyId !== user.companyId);
+            return Job.update({ ...input, companyId: user.companyId });
+        }
     },  
 
     Company: {
