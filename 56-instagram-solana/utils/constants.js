b7f1a7cca57381c2a5e260f59e538445fa47ad3a
Daniel <daniel30081990@gmail.com>
2023-01-22 15:20:00 -0300

    public solana key

--- 56-instagram-solana/utils/constants.js
+++ 56-instagram-solana/utils/constants.js
@@ -2,4 +2,4 @@ import { PublicKey } from "@solana/web3.js";
 
 export const RPC_ENDPOINT = "https://restless-omniscient-choice.solana-devnet.discover.quiknode.pro/your-id-quiknode/";
 
-// export const PROGRAM_ID = new PublicKey("11111111111")
+export const PROGRAM_ID = new PublicKey("your-bet.solpg.io-public-key")
