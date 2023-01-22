d623928e0b989048588cb10dca94e17cc509e346
Daniel <daniel30081990@gmail.com>
2023-01-21 22:24:32 -0300

    template instagram solana clone

---
+++ 56-instagram-solana/README.md
@@ -0,0 +1,5 @@
+# App using Next.js 13 to build an Instagram clone with Solana blockchain
+
+## Tools links
+
+`https://www.quicknode.com/`
---
+++ 56-instagram-solana/utils/constants.js
@@ -0,0 +1,5 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const RPC_ENDPOINT = "https://restless-omniscient-choice.solana-devnet.discover.quiknode.pro/your-id-quiknode/";
+
+// export const PROGRAM_ID = new PublicKey("11111111111")
