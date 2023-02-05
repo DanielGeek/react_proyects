5ef893a4b58d118f7ef1bc78d7953d102886b068
Daniel <daniel30081990@gmail.com>
2023-02-05 19:27:36 -0300

    15. Async Resolve Functions

--- 58-users-graphql/tests/integration/graphql/schema.test.js
+++ 58-users-graphql/tests/integration/graphql/schema.test.js
@@ -1,8 +1,19 @@
+const axios = require('axios');
 const { graphql } = require('graphql');
 const schema = require('../../../schema/schema');
 
+jest.mock('axios');
+
 describe('GraphQL Schema', () => {
   it('should retrieve a user by id', async () => {
+    axios.get.mockResolvedValueOnce({
+      data: {
+        id: '23',
+        firstName: 'Bill',
+        age: 20,
+      },
+    });
+
     const query = `
       query ($id: String) {
         user(id: $id) {
@@ -19,6 +30,7 @@ describe('GraphQL Schema', () => {
 
     const result = await graphql(schema, query, null, null, variables);
 
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/users/23');
     expect(result).toHaveProperty('data.user.id', '23');
     expect(result).toHaveProperty('data.user.firstName', 'Bill');
     expect(result).toHaveProperty('data.user.age', 20);
