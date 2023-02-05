6a0320c19221afad79b353869fb7bf97c3359bf7
Daniel <daniel30081990@gmail.com>
2023-02-05 20:38:55 -0300

    19. More on Nested Queries

---
+++ 58-users-graphql/README.md
@@ -0,0 +1,11 @@
+# App with Express and GraphQL
+
+## run
+
+`npm i`
+`npm run dev`
+`npm json:server`
+
+## run tests
+
+`npm run test`
--- 58-users-graphql/tests/integration/graphql/schema.test.js
+++ 58-users-graphql/tests/integration/graphql/schema.test.js
@@ -5,34 +5,53 @@ const schema = require('../../../schema/schema');
 jest.mock('axios');
 
 describe('GraphQL Schema', () => {
-  it('should retrieve a user by id', async () => {
-    axios.get.mockResolvedValueOnce({
-      data: {
-        id: '23',
-        firstName: 'Bill',
-        age: 20,
-      },
-    });
+	it('should retrieve a user and company by id', async () => {
+		axios.get.mockResolvedValueOnce({
+			data: {
+				id: '23',
+				firstName: 'Bill',
+				age: 20,
+                companyId: '1',
+			},
+		});
 
-    const query = `
-      query ($id: String) {
-        user(id: $id) {
-          id
-          firstName
-          age
+        const responseCompany = {
+            data: {
+                id: '1',
+                name: 'Apple',
+                description: 'iphone',
+            },
+        };
+
+        axios.get.mockResolvedValueOnce(responseCompany);
+
+		const query = `
+        query ($id: String) {
+            user(id: $id) {
+                id
+                firstName
+                age,
+                company {
+                    id
+                    name
+                    description
+                }
+            }
         }
-      }
-    `;
+        `;
 
-    const variables = {
-      id: '23',
-    };
+		const variables = {
+			id: '23',
+		};
 
-    const result = await graphql(schema, query, null, null, variables);
+		const result = await graphql(schema, query, null, null, variables);
+        
+		expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/users/23');
+        expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/companies/1');
+		expect(result).toHaveProperty('data.user.id', '23');
+		expect(result).toHaveProperty('data.user.firstName', 'Bill');
+		expect(result).toHaveProperty('data.user.age', 20);
+        expect(result).toHaveProperty('data.user.company.id', '1');
 
-    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/users/23');
-    expect(result).toHaveProperty('data.user.id', '23');
-    expect(result).toHaveProperty('data.user.firstName', 'Bill');
-    expect(result).toHaveProperty('data.user.age', 20);
-  });
+	});
 });
