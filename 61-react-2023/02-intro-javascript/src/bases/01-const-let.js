965516f5a87d97fa677a708b32050a8d2281ef0f
Daniel <daniel30081990@gmail.com>
2023-05-03 23:49:23 -0400

    13 - vars and consts

---
+++ 61-react-2023/02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+// Vars and Consts
+const name = 'Daniel';
+const lastName = 'Angel';
+
+let givenValue = 5;
+givenValue = 4;
+
+console.log( name, lastName, givenValue );
+
+if ( true ) {
+    const name = 'Elias';
+
+    console.log(name);
+}
+
+console.log(givenValue);
