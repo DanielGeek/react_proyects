b3fd40af3c26a8389e14e9eac3d3698a34de6cfd
Daniel <daniel30081990@gmail.com>
2023-05-13 11:48:02 -0400

    38. Printing variables in HTML

--- 61-react-2023/03-counter-app-vite/src/FirstApp.jsx
+++ 61-react-2023/03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,10 @@
 
 export const FirstApp = () => {
+
     return (
         <>
             <h1>Daniel</h1>
+            {/* <code>{JSON.stringify( newMessage )}</code> */}
             <p>Spy a subtitle</p>
         </>
     )
