30504e4c681940d388cdc550a548958eaa8ef873
Daniel <daniel30081990@gmail.com>
2023-05-16 19:28:43 -0400

    42. DefaultProps

--- 61-react-2023/03-counter-app-vite/src/FirstApp.jsx
+++ 61-react-2023/03-counter-app-vite/src/FirstApp.jsx
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({ title, subTitle }) => {
+export const FirstApp = ({ title, subTitle, name }) => {
 
     return (
         <>
             <h1>{ title }</h1>
             {/* <code>{JSON.stringify( newMessage )}</code> */}
             <p>{ subTitle }</p>
+            <p>{ name }</p>
         </>
     )
 }
 
 FirstApp.propTypes = {
     title: PropTypes.string.isRequired,
-    subTitle: PropTypes.number.isRequired
+    subTitle: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+    name: 'Daniel Angel',
+    subTitle: 'There is not subtitle',
+    title: 'There is not title',
 }
--- 61-react-2023/03-counter-app-vite/src/main.jsx
+++ 61-react-2023/03-counter-app-vite/src/main.jsx
@@ -7,6 +7,6 @@ import './styles.css';
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         {/* <HelloWorldApp /> */}
-        <FirstApp title="Hello, I`m Goku" />
+        <FirstApp />
     </React.StrictMode>
 );
