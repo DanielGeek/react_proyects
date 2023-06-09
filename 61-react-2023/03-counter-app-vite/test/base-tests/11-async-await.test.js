1586be7df3404132cecc002d93f75653a2343980
Daniel <daniel30081990@gmail.com>
2023-06-08 23:25:54 -0400

    61. Evaluate the Catch in the async-await

--- 61-react-2023/03-counter-app-vite/test/base-tests/11-async-await.test.js
+++ 61-react-2023/03-counter-app-vite/test/base-tests/11-async-await.test.js
@@ -1,9 +1,10 @@
 import { getImage } from "../../src/base-tests/11-async-await";
 
 describe('Testing in 11-async-await.js', () => {
-    test('getImage should return an URL of the image', async() => {
+    test('getImage should return an error if we don`t have a api key', async() => {
 
-        const url = await getImage();
-        expect( typeof url ).toBe('string');
+        const resp = await getImage();
+        // expect( typeof url ).toBe('string');
+        expect( resp ).toBe('Image not found');
     });
 });
