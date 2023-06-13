a1cd1eae71daedb3a5f67aa2c69284abfc2ab264
Daniel <daniel30081990@gmail.com>
2023-06-12 22:35:49 -0400

    64. Testing FirstApp

--- 61-react-2023/03-counter-app-vite/test/FirstApp.test.jsx
+++ 61-react-2023/03-counter-app-vite/test/FirstApp.test.jsx
@@ -5,9 +5,19 @@ describe('Testing in <FirstApp />', () => {
 
     test('must match the snapshot ', () => {
 
-        const title = "Hello, I'm Gokue";
-        render( <FirstApp title={ title } /> );
+        const title = "Hello, I'm Goku";
+        const { container } = render( <FirstApp title={ title } /> );
+        expect( container ).toMatchSnapshot();
+    });
 
+    test('must show the title in h1 ', () => {
+
+        const title = "Hello, I'm Goku";
+        const { container, getByText } = render( <FirstApp title={ title } /> );
+        expect( getByText(title) ).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect( h1.innerHTML ).toContain( title );
     });
 
 });
