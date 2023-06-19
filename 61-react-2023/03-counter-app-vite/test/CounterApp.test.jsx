b8d3437851dae216523aca78a8fbe57795f66acf
Daniel <daniel30081990@gmail.com>
2023-06-18 22:34:44 -0400

    68. Simulate events - Click

--- 61-react-2023/03-counter-app-vite/test/CounterApp.test.jsx
+++ 61-react-2023/03-counter-app-vite/test/CounterApp.test.jsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 describe('Testing in <CounterApp />', () => {
 
-    const initialValue = 100;
+    const initialValue = 10;
 
     test('Must match snapshot', () => {
         
@@ -14,9 +14,41 @@ describe('Testing in <CounterApp />', () => {
 
     test("Must show the initial value 100 ", () => {
 
-        render( <CounterApp value={initialValue} /> );
+        render( <CounterApp value={100} /> );
         expect( screen.getByText(100) ).toBeTruthy();
 
     });
+
+    test('Must increase with the button +1', () => {
+        
+        render( <CounterApp value={ initialValue } /> );
+        fireEvent.click( screen.getByText('+1') );
+        expect( screen.getByText('11') ).toBeTruthy();
+
+    });
+    
+    test('Must decrease with the button -1', () => {
+        
+        render( <CounterApp value={ initialValue } /> );
+        fireEvent.click( screen.getByText('-1') );
+        // screen.debug();
+        expect( screen.getByText('9') ).toBeTruthy();
+
+    });
+    
+    test('Must work the reset button', () => {
+        
+        render( <CounterApp value={ 355 } /> );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        // fireEvent.click( screen.getByText('Reset') );
+        fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }));
+
+        expect( screen.getByText( 355 ) ).toBeTruthy();
+
+    });
+
+
 
 });
