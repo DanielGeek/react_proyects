d37b867ccf66385e78b3313a522c56f05fa1f4c5
Daniel <daniel30081990@gmail.com>
2023-06-25 22:47:05 -0400

    76. Add a new category

--- 61-react-2023/04-gif-expert-app/src/GifExpertApp.jsx
+++ 61-react-2023/04-gif-expert-app/src/GifExpertApp.jsx
@@ -4,12 +4,15 @@ export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch', 'Goku']);
 
-    console.log(categories);
+    const onAddCategory = () => {
+        setCategories([ 'Valorant', ...categories ]);
+        // setCategories( cat => [ ...cat, 'Valorant' ] );
+    }
 
     return (
         <>
             <h1>GifExpertApp</h1>
-
+            <button onClick={ onAddCategory }>Add</button>
             <ol>
                 { categories.map( category => {
                     return <li key={ category }>{ category }</li>
