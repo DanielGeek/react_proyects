76c5d6fc105aa8ff985bf883c2db1636c706da40
Daniel <daniel30081990@gmail.com>
2023-06-25 22:38:32 -0400

    75. Creating a list of categories

--- 61-react-2023/04-gif-expert-app/src/GifExpertApp.jsx
+++ 61-react-2023/04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Goku']);
+
+    console.log(categories);
+
     return (
         <>
             <h1>GifExpertApp</h1>
+
+            <ol>
+                { categories.map( category => {
+                    return <li key={ category }>{ category }</li>
+                }) }
+            </ol>
         </>
     );
 };
