c444b515b29e8b64179b7e6ef4522919db07221c
Daniel <daniel30081990@gmail.com>
2023-07-13 23:55:54 -0400

    83. useEffect

--- 61-react-2023/04-gif-expert-app/src/components/helpers/getGifs.js
+++ 61-react-2023/04-gif-expert-app/src/components/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=bqumbdLo8Lrb1pXLE0Sl8YM3WLw5hPrv&q=${ category }&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=bqumbdLo8Lrb1pXLE0Sl8YM3WLw5hPrv&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
