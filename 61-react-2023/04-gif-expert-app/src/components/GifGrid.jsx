40403dd31de51cfa3d0ee6ec1878a49ac97da430
Daniel <daniel30081990@gmail.com>
2023-07-26 00:50:18 -0400

    86. className - css classes

--- 61-react-2023/04-gif-expert-app/src/components/GifGrid.jsx
+++ 61-react-2023/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { getGifs } from "./helpers/getGifs";
+
+import { GifItem } from "./GifItem";
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
@@ -18,11 +20,14 @@ export const GifGrid = ({ category }) => {
         <>
             <h3>{ category }</h3>
 
-            <ol>
-                {images.map( ({ id, title }) => (
-                    <li key={ id }>{ title }</li>
+            <div className="card-grid">
+                {images.map( ( image ) => (
+                    <GifItem 
+                        key={ image.id }
+                        { ...image }
+                    />
                 ))}
-            </ol>
+            </div>
         </>
     )
 }
