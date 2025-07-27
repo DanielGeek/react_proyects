3c4153f93924e3f6d3d21a28c663a1f3088ec64f
Daniel Angel <daniel30081990@gmail.com>
2025-07-27 11:48:58 -0400

    Add Testimonial, Plan, and Footer components to Home
    Introduced new Testimonial, Plan, and Footer components and integrated them into the Home page. Updated AiTools to display tool titles and descriptions. These changes enhance the homepage with user testimonials, pricing plans, and a footer section for improved user experience and information.

--- 63-QuickAI/client/src/components/AiTools.jsx
+++ 63-QuickAI/client/src/components/AiTools.jsx
@@ -22,6 +22,8 @@ const AiTools = () => {
             shadow-lg border border-gray-100 hover:-translate-y-1 transition-all
             duration-300 cursor-pointer" onClick={() => user && navigate(tool.path)}>
             <tool.Icon className="w-12 h-12 p-3 text-white rounded-xl" style={{ background: `linear-gradient(to bottom, ${tool.bg.from}, ${tool.bg.to})` }} />
+            <h3 className="mt-6 mb-3 text-lg font-semibold">{tool.title}</h3>
+            <h3 className="text-gray-400 text-sm max-w-[95%]">{tool.description}</h3>
           </div>
         ))}
       </div>
