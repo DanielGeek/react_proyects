4d065c5c67a4cfe9c952a96959060540e334b6f2
DanielGeek <daniel30081990@gmail.com>
2025-10-24 00:02:37 -0400

    var and const

---
+++ 66-reinforcement/src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+
+const firstName: string = 'Daniel';
+let lastName = 'Ángel';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterE = lastName.includes('e');
+
+console.log({ containsLetterE, diceNumber, firstName, lastName });
