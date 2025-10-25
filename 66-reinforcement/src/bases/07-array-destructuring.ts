ec40d6cab9876cec53933d62f288fc670da6b11c
DanielGeek <daniel30081990@gmail.com>
2025-10-25 01:00:29 -0400

    task destructuring

--- 66-reinforcement/src/bases/07-array-destructuring.ts
+++ 66-reinforcement/src/bases/07-array-destructuring.ts
@@ -12,3 +12,17 @@ const returnsArrayFn = () => {
 const [letters, numbers] = returnsArrayFn();
 
 console.log(letters + numbers);
+
+// Task
+const useState = (value: string) => {
+    return [
+        value,
+        (newValue: string) => {
+            console.log(newValue);
+        },
+    ] as const;
+};
+
+const [name, setName] = useState('Goku');
+console.log(name);
+setName('Vegeta');
