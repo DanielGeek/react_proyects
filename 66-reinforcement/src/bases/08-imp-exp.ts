19d0479728c12defada958a2cff400fda3621878
DanielGeek <daniel30081990@gmail.com>
2025-10-25 15:25:26 -0400

    import and exports

---
+++ 66-reinforcement/src/bases/08-imp-exp.ts
@@ -0,0 +1,15 @@
+import { heroes, type Hero } from "../data/heroes.data"
+
+const getHeroById = (id: number): Hero|undefined => {
+    const hero = heroes.find((hero) => {
+        return hero.id === id;
+    });
+
+    // if (!hero) {
+    //     throw new Error(`Heroe with id ${id} not found`);
+    // }
+
+    return hero;
+};
+
+console.log(getHeroById(1));
