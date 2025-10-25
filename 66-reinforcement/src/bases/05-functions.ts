557ed67d83b7d192b4fa8a3c397c968fd274cd94
DanielGeek <daniel30081990@gmail.com>
2025-10-24 20:26:48 -0400

    Functions with multiple returns

--- 66-reinforcement/src/bases/05-functions.ts
+++ 66-reinforcement/src/bases/05-functions.ts
@@ -3,29 +3,44 @@ function greet(name: string): string {
     return `Hello ${name}`;
 }
 
-const greet2 = (name: string): string => {
-    return `Hello ${name}`;
-}
+const greet2 = (name: string) => `Hello ${name}`;
 
 const message = greet('Goku');
 const message2 = greet2('Vegeta');
 
 console.log(message, message2);
 
-function getUser() {
+interface User {
+    uid: string;
+    username: string;
+}
+
+function getUser(): User {
     return {
         uid: 'ABC-123',
         username: 'The_Father123'
     };
 }
 
-const getUser2 = () => {
-    return {
-        uid: 'ABC-123',
-        username: 'The_Father123'
-    };
-};
+const getUser2 = () => ({
+    uid: 'ABC-123',
+    username: 'The_Father123'
+});
 
 const user = getUser();
 const user2 = getUser2();
 console.log(user, user2);
+
+const myNumbers: number[] = [1, 2, 3, 4, 5];
+
+// myNumbers.forEach(function (value) {
+//     console.log({ value });
+// });
+
+// myNumbers.forEach((value) => {
+//     console.log({ value });
+// });
+
+myNumbers.forEach((value, index, arr) => {
+    console.log({ value, index, arr });
+});
