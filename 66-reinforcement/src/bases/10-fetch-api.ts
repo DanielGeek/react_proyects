0e27d0364019b836b496f075637bd975e42d716c
DanielGeek <daniel30081990@gmail.com>
2025-10-25 22:21:54 -0400

    Interfaces and optimization

--- 66-reinforcement/src/bases/10-fetch-api.ts
+++ 66-reinforcement/src/bases/10-fetch-api.ts
@@ -1,18 +1,21 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
 
 const API_KEY = 'U04NEsQa0g1R7kivoo1Z2PhijeVKm86O';
 
 const myRequest = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
 
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+
+    document.body.append(imgElement);
+}
+
 myRequest
     .then((response) => response.json())
-    .then((data) => {
-        const imageUrl = data.data.images.original.url;
-        console.log(imageUrl);
-
-        const imgElement = document.createElement('img');
-        imgElement.src = imageUrl;
-
-        document.body.append(imgElement);
+    .then(({ data }: GiphyRandomResponse) => {
+        const imageUrl = data.images.original.url;
+        createImageInsideDOM(imageUrl);
     })
     .catch((err) => {
         console.error(err);
