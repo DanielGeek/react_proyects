6cef9d58e5cb5502d9290754b54c3009ac865d92
DanielGeek <daniel30081990@gmail.com>
2025-10-26 18:42:41 -0400

    First component

---
+++ 67-first-steps-react/src/FirstStepsApp.tsx
@@ -0,0 +1,15 @@
+
+export function FirstStepsApp() {
+    return (
+        <>
+            <h1>Hello World!!</h1>
+            <p>This is a paragraph</p>
+
+            <button>Click me</button>
+
+            <div>
+                <h2>Hello from another div</h2>
+            </div>
+        </>
+    )
+}
