055c952af81f439f3fa58cc1a97a6c2296981322
DanielGeek <daniel30081990@gmail.com>
2025-10-26 19:11:54 -0400

    print variables

--- 67-first-steps-react/src/MyAwesomeApp.tsx
+++ 67-first-steps-react/src/MyAwesomeApp.tsx
@@ -1,9 +1,27 @@
 
-export function MyAwesomeApp() {
+export const MyAwesomeApp = () => {
+    const firstName = 'Daniel';
+    const lastName = 'Ángel';
+
+    const favoriteGames = ['Elden Ring', 'Smash', 'Metal Gear'];
+    const isActive = false;
+
+    const address = {
+        zipCode: 'ABC-123',
+        country: 'Venezuela',
+    };
+
     return (
         <>
-            <h1>Daniel</h1>
-            <h1>Ángel</h1>
+            <h1>{firstName}</h1>
+            <h1>{lastName}</h1>
+
+            <p>{favoriteGames}</p>
+            <p>{2 + 2}</p>
+
+            <p>{isActive ? 'Active' : 'Inactive'}</p>
+
+            <p>{JSON.stringify(address)}</p>
         </>
     )
 }
--- 67-first-steps-react/src/main.tsx
+++ 67-first-steps-react/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { FirstStepsApp } from './FirstStepsApp'
+// import { FirstStepsApp } from './FirstStepsApp'
 import { MyAwesomeApp } from './MyAwesomeApp'
 
 createRoot(document.getElementById('root')!).render(
