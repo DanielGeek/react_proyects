4e9cd3c396e01a80489493ab57bb752b4de5c7aa
DanielGeek <daniel30081990@gmail.com>
2025-10-29 18:13:25 -0400

    test: dispatch events

--- 67-first-steps-react/src/shopping-cart/ItemCounter.test.tsx
+++ 67-first-steps-react/src/shopping-cart/ItemCounter.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { ItemCounter } from "./ItemCounter";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 describe('ItemCounter', () => {
     test('should render with default values', () => {
@@ -11,7 +11,7 @@ describe('ItemCounter', () => {
         expect(screen.getByText(name)).toBeDefined();
         expect(screen.getByText(name)).not.toBeNull();
     });
-    
+
     test('should render with custom quantity', () => {
         const name = 'Nintendo Switch 2';
         const quantity = 10;
@@ -20,4 +20,37 @@ describe('ItemCounter', () => {
 
         expect(screen.getByText(quantity)).toBeDefined();
     });
+
+    test('should increase count when +1 button is pressed', () => {
+        render(<ItemCounter name="Test item" quantity={1} />);
+
+        const [buttonAdd] = screen.getAllByRole('button');
+
+        fireEvent.click(buttonAdd);
+
+        expect(screen.getByText('2')).toBeDefined();
+    });
+
+    test('should decrease count when -1 button is pressed', () => {
+        const quantity = 5;
+        render(<ItemCounter name="Test item" quantity={quantity} />);
+
+        const [, buttonSubtract] = screen.getAllByRole('button');
+
+        fireEvent.click(buttonSubtract);
+
+        expect(screen.getByText('4')).toBeDefined();
+    });
+
+    test('should not decrease count when -1 button is pressed and count is 1', () => {
+        const quantity = 1;
+        render(<ItemCounter name="Test item" quantity={quantity} />);
+
+        const [, buttonSubtract] = screen.getAllByRole('button');
+
+        fireEvent.click(buttonSubtract);
+
+        expect(screen.getByText('1')).toBeDefined();
+    });
+
 });
