af05e65bed39c0971f9a82a87c2dd7be166fa555
DanielGeek <daniel30081990@gmail.com>
2025-10-31 18:45:58 -0400

    feat: add prisma ORM and Neon db connection

--- 69-nodebase/src/app/page.tsx
+++ 69-nodebase/src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export const Page = () => {
 
   return (
-    <div>
-      Hello World
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button variant="outline">
+        Click me
+      </Button>
     </div>
   );
 };
