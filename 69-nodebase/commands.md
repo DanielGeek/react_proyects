4c4dcf341c431ee4402ec371084d04727e3d2003
DanielGeek <daniel30081990@gmail.com>
2025-11-01 22:13:17 -0400

    feat: add inngest
    - Setup Inngest
    - Create a background job
    - Add mprocs

---
+++ 69-nodebase/commands.md
@@ -0,0 +1,67 @@
+# Help Commands
+
+## [Prisma](https://www.prisma.io)
+
+## Reset your database
+
+```bash
+npx prisma migrate reset
+```
+
+## Migrate your database
+
+```bash
+npx prisma migrate dev
+```
+
+## Generate your Prisma Client
+
+```bash
+npx prisma generate
+```
+
+## [Coderabbit](https://www.coderabbit.ai)
+
+## Run your first review
+
+```bash
+coderabbit
+```
+
+## Use plain mode or prompt-only mode when using it with Claude Code, Codex CLI, Gemini CLI, etc
+
+```bash
+coderabbit review --plain
+```
+
+## Show minimal output optimized for AI agents
+
+```bash
+coderabbit --prompt-only
+```
+
+## Run your app
+
+```bash
+npm run dev
+```
+
+## [Inngest](https://www.inngest.com)
+
+## Run your Inngest agent
+
+```bash
+npx inngest-cli@1.12.1 dev
+```
+
+## Deploy your Inngest agent
+
+```bash
+npx inngest-cli@1.12.1 deploy
+```
+
+## Deploy your app
+
+```bash
+npm run deploy
+```
--- 69-nodebase/src/app/page.tsx
+++ 69-nodebase/src/app/page.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
-export const Page = async () => {
-  await requireAuth();
+export const Page = () => {
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers();
+  const create = useMutation(trpc.createWorkflow.
+    mutationOptions({
+      onSuccess: () => {
+        toast.success("Job queued");
+      },
+    }));
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
@@ -13,6 +26,9 @@ export const Page = async () => {
       <div>
         {JSON.stringify(data, null, 2)}
       </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
       <LogoutButton />
     </div>
   );
