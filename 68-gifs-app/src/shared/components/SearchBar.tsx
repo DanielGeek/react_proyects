d38b979854f304611edd1a0505804b80d40824a3
DanielGeek <daniel30081990@gmail.com>
2025-11-10 15:29:26 -0400

    feat: add tests and adjust debounce in SearchBar
    - Introduces unit tests and a snapshot for the SearchBar component.
    - changes the debounce timeout from 700ms to 600ms to improve responsiveness.

--- 68-gifs-app/src/shared/components/SearchBar.tsx
+++ 68-gifs-app/src/shared/components/SearchBar.tsx
@@ -12,7 +12,7 @@ export const SearchBar = ({ placeholder = 'Search', onQuery }: Props) => {
     useEffect(() => {
         const timeoutId = setTimeout(() => {
             onQuery(query);
-        }, 700);
+        }, 600);
 
         return () => {
             clearTimeout(timeoutId);
